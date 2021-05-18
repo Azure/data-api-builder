e1cbc5d42fa5823040067b566c4d5fa048c8d27c
Bhaskar Mallapragada <mbhaskar@gmail.com>
2021-05-18 14:54:59 -0700

    Initial commit with a basic controller

---
+++ Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/Controllers/GraphQLController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Cosmos.GraphQL.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class GraphQLController : ControllerBase
+    {
+        string JsonData = @"{'serviceName':'cosmos', 'endpointType':'graphQL'}";
+
+        private readonly ILogger<GraphQLController> _logger;
+
+        public GraphQLController(ILogger<GraphQLController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<JObject> Get()
+        {
+            JObject data =JObject.Parse(JsonData);
+            return Enumerable.Repeat(data, 1);
+        }
+    }
+}

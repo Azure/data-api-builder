0df758743a4045a5bdd31a9e2b567fc3b7a34c53
Bhaskar Mallapragada <mbhaskar@gmail.com>
2021-05-25 16:20:39 -0700

    Adding graphql service

---
+++ Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/Models/GraphQLRequest.cs
@@ -0,0 +1,15 @@
+﻿using GraphQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cosmos.GraphQL.Service.Models
+{
+    public class GraphQLRequest
+    {
+        internal string Query { get; set; }
+        internal string OperationName { get; set; }
+        internal Inputs Variables { get; set; }
+    }
+}

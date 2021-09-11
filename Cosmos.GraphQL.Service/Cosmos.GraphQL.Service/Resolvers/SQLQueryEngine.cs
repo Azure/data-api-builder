4c378115cd478368025e6982c86b3a3614c5e4b5
Aniruddh Munde <anmunde@microsoft.com>
2021-09-10 19:27:54 -0700

    Add ExecuteAsList separately

--- Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/Resolvers/SQLQueryEngine.cs
+++ Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/Resolvers/SQLQueryEngine.cs
@@ -24,7 +24,8 @@ namespace Cosmos.GraphQL.Services
         private IMetadataStoreProvider _metadataStoreProvider;
         private readonly IQueryExecutor _queryExecutor;
 
-        private const string x_ForJsonSuffix = " FOR JSON PATH, ROOT('data');";
+        private const string x_ForJsonSuffix = " FOR JSON PATH, INCLUDE_NULL_VALUES";
+        private const string x_WithoutArrayWrapperSuffix = "WITHOUT_ARRAY_WRAPPER";
 
         // <summary>
         // Constructor.
@@ -59,22 +60,21 @@ namespace Cosmos.GraphQL.Services
             {
                 // Edit query to add FOR JSON PATH
                 //
-                string queryText = resolver.parametrizedQuery + x_ForJsonSuffix;
+                string queryText = resolver.parametrizedQuery + x_ForJsonSuffix + "," + x_WithoutArrayWrapperSuffix + ";";
 
                 // Open connection and execute query using _queryExecutor
                 //
                 DbDataReader dbDataReader =  await _queryExecutor.ExecuteQueryAsync(queryText, resolver.databaseName);
 
                 // Parse Results into Json and return
-                // TODO: Is this sufficient for multiple rows in data ?
                 //
-                if (dbDataReader.FieldCount > 0)
+                if (await dbDataReader.ReadAsync())
                 {
                     jsonDocument = JsonDocument.Parse(dbDataReader.GetString(0));
                 }
                 else
                 {
-                    Console.WriteLine("Did not return enough columns in the JSON result.");
+                    Console.WriteLine("Did not return enough rows in the JSON result.");
                 }
             }
             catch (SystemException ex)
@@ -88,14 +88,40 @@ namespace Cosmos.GraphQL.Services
         // <summary>
         // Executes the given named graphql query on the backend and expecting a list of Jsons back.
         // </summary>
-        public IEnumerable<JsonDocument> ExecuteList(string graphQLQueryName, IDictionary<string, ArgumentValue> parameters)
+        public async Task<IEnumerable<JsonDocument>> ExecuteListAsync(string graphQLQueryName, IDictionary<string, ArgumentValue> parameters)
         {
             // TODO: add support for nesting
             // TODO: add support for join query against another container
             // TODO: add support for TOP and Order-by push-down
 
-            var resolver = _metadataStoreProvider.GetQueryResolver(graphQLQueryName);
+            GraphQLQueryResolver resolver = _metadataStoreProvider.GetQueryResolver(graphQLQueryName);
             List<JsonDocument> resultsAsList = new List<JsonDocument>();
+            try
+            {
+                // Edit query to add FOR JSON PATH
+                //
+                string queryText = resolver.parametrizedQuery + x_ForJsonSuffix + ";";
+
+                // Open connection and execute query using _queryExecutor
+                //
+                DbDataReader dbDataReader = await _queryExecutor.ExecuteQueryAsync(queryText, resolver.databaseName);
+
+                // Deserialize results into list of JsonDocuments and return
+                //
+                if (await dbDataReader.ReadAsync())
+                {
+                    resultsAsList = JsonSerializer.Deserialize<List<JsonDocument>>(dbDataReader.GetString(0));
+                }
+                else
+                {
+                    Console.WriteLine("Did not return enough rows in the JSON result.");
+                }
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine("Caught an exception: " + ex.Message);
+            }
+
             return resultsAsList;
         }
 

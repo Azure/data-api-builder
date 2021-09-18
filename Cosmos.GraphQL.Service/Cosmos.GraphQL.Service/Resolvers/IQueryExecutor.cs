40a1fd797ca775736dd6fed7179266aaa4877113
Aniruddh Munde <anmunde@microsoft.com>
2021-09-17 21:55:15 -0700

    Add IQueryExecutor

---
+++ Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/Resolvers/IQueryExecutor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Threading.Tasks;
+
+namespace Cosmos.GraphQL.Service.Resolvers
+{
+    /// <summary>
+    /// Interface for query execution.
+    /// </summary>
+    public interface IQueryExecutor
+    {
+        /// <summary>
+        /// Execute sql text with parameters and return result set.
+        /// </summary>
+        /// <param name="sqltext">SQL text to be executed.</param>
+        /// <param name="database">Database to execute the SQL text.</param>
+        /// <param name="parameters">The parameters used to execute the SQL text.</param>
+        /// <returns>DbDataReader object for reading the result set.</returns>
+        public Task<DbDataReader> ExecuteQueryAsync(string sqltext, string database, List<IDataParameter> parameters);
+    }
+}

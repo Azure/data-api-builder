87b26a97b4a5eada426b2a8b542094e0743eff4f
Jelte Fennema <jefennem@microsoft.com>
2021-09-20 13:59:15 +0200

    Remove unnecessary abstractions
    Now that we support 3 different databases, it became clearer which
    abstractions are necessary and which are not. This removes a bunch of
    unnecessary abstractions and interfaces. It removes the following
    abstractions:
    
    1. The `IDatabaseCredentials` credentials interface and all it's
       implementations. Instead we now use a `ConnectionString` internally
       for all databases.
    2. The `IDbConnectionService` interface and it's implementations. This
       is now included in the `QueryExecutor`.
    3. The `IClientProvider` interface. This was only really used by the
       Cosmos implementation. Instead the Cosmos implementation now calls
       `CosmosClientProvider` directly.

--- Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/Resolvers/CosmosClientProvider.cs
+++ Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/Resolvers/CosmosClientProvider.cs
@@ -1,19 +1,18 @@
-using System.Configuration;
 using Cosmos.GraphQL.Service.configurations;
 using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Cosmos.Fluent;
 
 namespace Cosmos.GraphQL.Service.Resolvers
 {
-    public class CosmosClientProvider : IClientProvider<CosmosClient>
+    public class CosmosClientProvider
     {
         private static CosmosClient _cosmosClient;
         private static readonly object syncLock = new object();
 
         private static void init()
         {
-            var cred = ConfigurationProvider.getInstance().Creds;
-            _cosmosClient = new CosmosClientBuilder(cred.ConnectionString).WithContentResponseOnWrite(true).Build();
+            var connectionString = ConfigurationProvider.getInstance().ConnectionString;
+            _cosmosClient = new CosmosClientBuilder(connectionString).WithContentResponseOnWrite(true).Build();
         }
 
         public CosmosClient GetClient()
--- Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/Resolvers/QueryExecutor.cs
+++ Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/Resolvers/QueryExecutor.cs
@@ -1,47 +1,38 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Threading.Tasks;
+using Cosmos.GraphQL.Service.configurations;
 
 namespace Cosmos.GraphQL.Service.Resolvers
 {
     /// <summary>
     /// Encapsulates query execution apis.
     /// </summary>
-    public class QueryExecutor : IQueryExecutor
+    public class QueryExecutor<ConnectionT> : IQueryExecutor
+        where ConnectionT : DbConnection, new()
     {
-        /// <summary>
-        /// The clientProvider useful for connecting to the backend.
-        /// </summary>
-        private readonly IDbConnectionService _clientProvider;
-
-        /// <summary>
-        /// Initializes a new instance of the <see cref="QueryExecutor"/> class.
-        /// Constructor for QueryExecutor.
-        /// </summary>
-        /// <param name="clientProvider">ClientProvider for setting up connection.</param>
-        public QueryExecutor(IDbConnectionService clientProvider)
-        {
-            _clientProvider = clientProvider;
-        }
-
         /// <summary>
         /// Executes sql text that return result set.
         /// </summary>
         /// <param name="sqltext">Sql text to be executed.</param>
-        /// <param name="databaseName">Database to execute the SQL text.</param>
+        /// <param name="parameters">The parameters used to execute the SQL text.</param>
         /// <returns>DbDataReader object for reading the result set.</returns>
-        public async Task<DbDataReader> ExecuteQueryAsync(string sqltext, string databaseName, List<IDataParameter> parameters)
+        public async Task<DbDataReader> ExecuteQueryAsync(string sqltext, IDictionary<string, object> parameters)
         {
-            DbConnection conn = await _clientProvider.GetOpenedConnectionAsync(databaseName);
+            var conn = new ConnectionT();
+            conn.ConnectionString = ConfigurationProvider.getInstance().ConnectionString;
+            await conn.OpenAsync();
             DbCommand cmd = conn.CreateCommand();
             cmd.CommandText = sqltext;
             cmd.CommandType = CommandType.Text;
-
             if (parameters != null)
             {
-                foreach (IDataParameter parameter in parameters)
+                foreach (var parameterEntry in parameters)
                 {
+                    var parameter = cmd.CreateParameter();
+                    parameter.ParameterName = "@" + parameterEntry.Key;
+                    parameter.Value = parameterEntry.Value;
                     cmd.Parameters.Add(parameter);
                 }
             }

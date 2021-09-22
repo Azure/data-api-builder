6b4b4ec623fecd863816c6540afba33bbf0d8bdd
Aniruddh Munde <3513779+Aniruddh25@users.noreply.github.com>
2021-09-21 22:23:06 -0700

    Remove unused using

--- Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/configurations/MSSQLCredentials.cs
+++ Cosmos.GraphQL.Service/Cosmos.GraphQL.Service/configurations/MSSQLCredentials.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Cosmos.GraphQL.Service.configurations
+﻿namespace Cosmos.GraphQL.Service.configurations
 {
     /// <summary>
     /// Representation of MsSql specific credential properties

677bcd7f680a518f6f98102f13620ca6da6d82b9
Sean Leonard <sean.leonard@microsoft.com>
2021-10-20 12:10:24 -0700

    remove unneccessary "using" directives in mssql integration test classes.

--- Cosmos.GraphQL.Service/Cosmos.GraphQL.Service.Tests/SqlTests/SqlTestHelper.cs
+++ Cosmos.GraphQL.Service/Cosmos.GraphQL.Service.Tests/SqlTests/SqlTestHelper.cs
@@ -1,10 +1,5 @@
 using Cosmos.GraphQL.Service.Models;
 using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cosmos.GraphQL.Service.Tests.Sql
 {

cfbcc451fae0a4fcf012223248241af823105dfb
Sean Leonard <sean.leonard@microsoft.com>
2021-10-27 09:46:14 -0700

    MsSql Integration Tests  (#62)
    * MsSql Passing Integration Tests + Integration Test Framework
    
    * Added named parameter for specific JSON serialization options in HotChocolate ToJson() method call.
    
    * remove unneccessary "using" directives in mssql integration test classes.
    
    * Revert "remove unneccessary "using" directives in mssql integration test classes."
    
    * Remove unneccessary "using" directives in mssqlintegration tests classes.
    
    * Added dedicated mssqlintegration test config file and added config setup in sqltesthelperclass to pull from new file.
    
    * Fixed build errors resulting from extra whitespaces. (analyzer issues)
    
    * Address PR comments. MsSql named classes. namedParams for nulls, updated summary comment for dropping tables
    
    * address whitespace issue in build pipeline format check
    
    * Updating per PR comments. Refactored Testclass to fit MSTest paradigm for initializing assets on class basis/test run, and not per test.
    
    * Updated config for test runs as Local test explorer cannot run both sets of tests simulataneously when appsettings.Test.json has singular engine connection string.
    
    * testing Pipeline run with outputting connectionstring to console.
    
    * More test of pipeline connection to sql db
    
    * Update Cosmos.GraphQL.Service/Cosmos.GraphQL.Service.Tests/MsSqlTests/MsSqlQueryTests.cs
    
    Co-authored-by: Aniruddh Munde <3513779+Aniruddh25@users.noreply.github.com>
    
    Co-authored-by: Sean Leonard <seleonar@microsoft.com>
    Co-authored-by: Aniruddh Munde <3513779+Aniruddh25@users.noreply.github.com>

---
+++ Cosmos.GraphQL.Service/Cosmos.GraphQL.Service.Tests/MsSqlTests/MsSqlTestHelper.cs
@@ -0,0 +1,69 @@
+using Cosmos.GraphQL.Service.configurations;
+using Cosmos.GraphQL.Service.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Cosmos.GraphQL.Service.Tests.MsSql
+{
+    /// <summary>
+    /// Helper functions for setting up test scenarios
+    /// </summary>
+    public class MsSqlTestHelper
+    {
+        public static readonly string GraphQLSchema = @"
+                type Query {
+                    characterList: [Character]
+                    characterById (id : ID!): Character
+                }
+                type Character {
+                    id : ID,
+                    name : String,
+                    type: String,
+                    homePlanet: Int,
+                    primaryFunction: String
+                }
+                ";
+
+        public static readonly string CharacterListResolver = "{\r\n \"id\": \"characterList\",\r\n \"parametrizedQuery\": \"SELECT id, name, type, homePlanet, primaryFunction FROM character\"\r\n }";
+        public static readonly string CharacterByIdResolver = "{\r\n \"id\": \"characterById\",\r\n \"parametrizedQuery\": \"SELECT id, name, type, homePlanet, primaryFunction FROM character WHERE id = @id\"\r\n}";
+        private static Lazy<IOptions<DataGatewayConfig>> _dataGatewayConfig = new Lazy<IOptions<DataGatewayConfig>>(() => MsSqlTestHelper.LoadConfig());
+
+        /// <summary>
+        /// Converts Raw JSON resolver to Resolver class object
+        /// </summary>
+        /// <param name="rawResolverText">escaped JSON string</param>
+        /// <returns>GraphQLQueryResolver object</returns>
+        public static GraphQLQueryResolver GetQueryResolverJson(string rawResolverText)
+        {
+            return JsonConvert.DeserializeObject<GraphQLQueryResolver>(rawResolverText);
+        }
+
+        /// <summary>
+        /// Sets up configuration object as defined by appsettings.ENV.json file
+        /// </summary>
+        /// <returns></returns>
+        private static IOptions<DataGatewayConfig> LoadConfig()
+        {
+            DataGatewayConfig datagatewayConfig = new DataGatewayConfig();
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.Test.json")
+                .Build();
+
+            config.Bind(nameof(DataGatewayConfig), datagatewayConfig);
+
+            return Options.Create(datagatewayConfig);
+        }
+
+        /// <summary>
+        /// Returns configuration value loaded from file.
+        /// </summary>
+        public static IOptions<DataGatewayConfig> DataGatewayConfig
+        {
+            get { return _dataGatewayConfig.Value; }
+        }
+    }
+}

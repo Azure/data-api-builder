97c97051bab7f4c75ed01d8534b02e8e1ab7c86c
aaron burtle <aaronburtle@microsoft.com>
2021-10-29 01:14:01 -0700

    reverted to working commit and reset namespace changes to Azure.DataGateway

---
+++ DataGateway.Service.Tests/QueryTests.cs
@@ -0,0 +1,24 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Azure.DataGateway.Service.Tests
+{
+    [TestClass, TestCategory(TestCategory.Cosmos)]
+    public class QueryTests : TestBase
+    {
+        [TestMethod]
+        public async Task TestSimpleQuery()
+        {
+            // Add query resolver
+            _metadataStoreProvider.StoreQueryResolver(TestHelper.SampleQueryResolver());
+
+            // Run query
+            _controller.ControllerContext.HttpContext = GetHttpContextWithBody(TestHelper.SampleQuery);
+            JsonDocument response = await _controller.PostAsync();
+
+            // Validate results
+            Assert.IsFalse(response.ToString().Contains("Error"));
+        }
+    }
+}

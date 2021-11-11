9df818eb5ed44b6fceaa9bee321c4c44c7626db0
Mathieu Tremblay <mathieu.tremblay1994@gmail.com>
2021-11-11 07:44:59 -0500

    Add comments on GraphQL class in functions project

--- DataGateway.Service.Functions/GraphQL.cs
+++ DataGateway.Service.Functions/GraphQL.cs
@@ -1,13 +1,16 @@
 using Azure.DataGateway.Services;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
-using Microsoft.Extensions.Logging;
 using System.IO;
 using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Azure.DataGateway.Service.Functions
 {
+    /// <summary>
+    /// This hosts the function that runs when we send a requerst to /graphql.
+    /// The equivalent of the GraphQLController in the DotNetCore project.
+    /// </summary>
     public class GraphQL
     {
         private readonly GraphQLService _schemaManager;
@@ -16,10 +19,16 @@ namespace Azure.DataGateway.Service.Functions
             _schemaManager = schemaManager;
         }
 
-        [Function("GraphQL")]
+        /// <summary>
+        /// This is where Post requests to /graphql are routed, it is the equivalent
+        /// of "GraphQLController.PostAsync" in the DotNetCore project.
+        /// It reads the body and calls into the GraphQLService to process the request.
+        /// </summary>
+        /// <param name="req">The request data.</param>
+        /// <returns>The result of the graphql request.</returns>
+        [Function("graphql")]
         public async Task<JsonDocument> Run(
-            [HttpTrigger(AuthorizationLevel.User, "post", Route = null)] HttpRequestData req,
-            ILogger log)
+            [HttpTrigger(AuthorizationLevel.User, "post", Route = null)] HttpRequestData req)
         {
             string requestBody;
             using (var reader = new StreamReader(req.Body))

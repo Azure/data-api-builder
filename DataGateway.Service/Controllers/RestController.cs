9f941247ec09f30e0756bbdcda9f3319b7a0c37f
Aniruddh Munde <anmunde@microsoft.com>
2021-11-16 19:52:15 -0800

    Add a RestController

---
+++ DataGateway.Service/Controllers/RestController.cs
@@ -0,0 +1,34 @@
+using Azure.DataGateway.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Azure.DataGateway.Service.Controllers
+{
+    [ApiController]
+    [Route("{entityName}")]
+    public class RestController : ControllerBase
+    {
+        private readonly RestService _restService;
+
+        public RestController(RestService restService)
+        {
+            _restService = restService;
+        }
+
+        [HttpGet]
+        [Route("{queryByPrimaryKey}")]
+        [Produces("application/json")]
+        public async Task<JsonDocument> FindById(
+            string entityName,
+            string queryByPrimaryKey)
+        {
+            string queryString = HttpContext.Request.QueryString.ToString();
+
+            /* TO DO: Stream the response instead of returning the Json Documents*/
+            JsonDocument resultJson = await _restService.ExecuteAsync(entityName, queryByPrimaryKey, queryString);
+            return resultJson;
+        }
+    }
+}

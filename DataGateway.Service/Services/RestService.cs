9e5bf898bec42bb5bcf49eb7d7b967fbd032c7b2
Aniruddh Munde <anmunde@microsoft.com>
2021-11-16 19:52:35 -0800

    Add a RestService

---
+++ DataGateway.Service/Services/RestService.cs
@@ -0,0 +1,27 @@
+using Azure.DataGateway.Service.Resolvers;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Azure.DataGateway.Services
+{
+    public class RestService
+    {
+        private readonly IQueryEngine _queryEngine;
+
+        public RestService(IQueryEngine queryEngine)
+        {
+            _queryEngine = queryEngine;
+        }
+
+        internal async Task<JsonDocument> ExecuteAsync(string entityName, string queryByPrimaryKey, string queryString)
+        {
+            var requestParser = new RequestParser();
+            QueryStructure queryStructure = new(entityName);
+            requestParser.ParsePrimayKey(queryByPrimaryKey, queryStructure);
+            requestParser.ParseQueryString(System.Web.HttpUtility.ParseQueryString(queryString), queryStructure);
+            return await _queryEngine.ExecuteAsync(queryStructure);
+        }
+
+    }
+}
+}

e24880d0b684c51f9034e9ef47c95df388507ee0
Aniruddh Munde <anmunde@microsoft.com>
2021-11-17 14:17:33 -0800

    Add comments to MsSqlQueryBuilder

--- DataGateway.Service/Resolvers/MsSqlQueryBuilder.cs
+++ DataGateway.Service/Resolvers/MsSqlQueryBuilder.cs
@@ -6,20 +6,36 @@ using System.Text;
 namespace Azure.DataGateway.Service.Resolvers
 {
     /// <summary>
-    /// Modifies a query that returns regular rows to return JSON for MSSQL.
+    /// Class for building MsSql queries.
     /// </summary>
     public class MsSqlQueryBuilder : IQueryBuilder
     {
         private const string FOR_JSON_SUFFIX = " FOR JSON PATH, INCLUDE_NULL_VALUES";
         private const string WITHOUT_ARRAY_WRAPPER_SUFFIX = "WITHOUT_ARRAY_WRAPPER";
+
+        /// <summary>
+        /// Wild Card for field selection.
+        /// </summary>
         private const string ALL_FIELDS = "*";
 
         private static DbCommandBuilder _builder = new SqlCommandBuilder();
+
+        /// <summary>
+        /// Enclose the given string within [] specific for MsSql.
+        /// </summary>
+        /// <param name="ident">The unquoted identifier to be enclosed.</param>
+        /// <returns>The quoted identifier.</returns>
         public string QuoteIdentifier(string ident)
         {
             return _builder.QuoteIdentifier(ident);
         }
 
+        /// <summary>
+        /// For the given query, append the correct JSON suffixes based on if the query is a list or not.
+        /// </summary>
+        /// <param name="inputQuery">The given query.</param>
+        /// <param name="isList">True if its a list query, false otherwise</param>
+        /// <returns>The JSON suffixed query.</returns>
         public string Build(string inputQuery, bool isList)
         {
             var queryText = new StringBuilder(inputQuery + FOR_JSON_SUFFIX);
@@ -32,10 +48,10 @@ namespace Azure.DataGateway.Service.Resolvers
         }
 
         /// <summary>
-        /// Build the MsSql query for the given FindQueryStructure object which
+        /// Builds the MsSql query for the given FindQueryStructure object which
         /// holds the major components of a query.
         /// </summary>
-        /// <param name="structure">The query structure holding the properties.</param>
+        /// <param name="structure">The query structure holding the components.</param>
         /// <returns>The formed query text.</returns>
         public string Build(FindQueryStructure structure)
         {

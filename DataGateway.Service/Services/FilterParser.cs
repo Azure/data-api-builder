45c3c42123086e7a5ab49425d3aec5684f8259dc
aaron burtle <aaronburtle@microsoft.com>
2022-01-28 16:33:15 -0800

    cleaned up comments, removed unneeded field

--- DataGateway.Service/Services/FilterParser.cs
+++ DataGateway.Service/Services/FilterParser.cs
@@ -7,22 +7,23 @@ using Microsoft.OData.Edm;
 namespace Azure.DataGateway.Service.Services
 {
     /// <summary>
-    /// FilterParser stores the model that represents customer data and returns
-    /// a list of RestPredicates representing the predicates required for the Query
-    /// that matches the provided $filter query string.
+    /// FilterParser stores the model that represents customer data and can
+    /// Parse the FilterClause generated from that model.
     /// </summary>
     public class FilterParser
     {
-        private DatabaseSchema _schema;
         private IEdmModel _model;
 
         public FilterParser(DatabaseSchema schema)
         {
-            _schema = schema;
             EdmModelBuilder builder = new();
-            _model = builder.BuildModel(_schema).GetModel();
+            _model = builder.BuildModel(schema).GetModel();
         }
 
+        /// <summary>
+        /// Parses the filter clause.
+        /// </summary>
+        /// <returns>A list of rest predicates to be used in query generation.</returns>
         public List<RestPredicate> Parse()
         {
             throw new NotImplementedException();

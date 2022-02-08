b932a935eb78daacea623a5d276933c3c94e0f8a
aaron burtle <aaronburtle@microsoft.com>
2022-02-07 21:22:42 -0800

    typo in comments

--- DataGateway.Service/Services/FilterParser.cs
+++ DataGateway.Service/Services/FilterParser.cs
@@ -27,7 +27,7 @@ namespace Azure.DataGateway.Service.Services
         /// </summary>
         /// <param name="filterQueryString">Represents the $filter part of the query string</param>
         /// <param name="resourcePath">Represents the resource path, in our case the entity name.</param>
-        /// <returns></returns>
+        /// <returns>A list of rest predicates to be used in query generation.</returns>
         public List<RestPredicate> Parse(string filterQueryString, string resourcePath)
         {
             Uri relativeUri = new(resourcePath + filterQueryString, UriKind.Relative);

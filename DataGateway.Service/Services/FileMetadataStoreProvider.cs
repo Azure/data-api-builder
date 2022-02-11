860048794c8c94120cce44d055dfa55f79ca0103
Aaron Powell <me@aaron-powell.com>
2022-02-11 16:27:44 +1100

    More records to force init and handling null from JSON deserialization
    The JSON stuff feels a bit icky, but the failure to deserialize is a problem to surface and we don't want a nullable config - would require constant null checks to use the config

--- DataGateway.Service/Services/FileMetadataStoreProvider.cs
+++ DataGateway.Service/Services/FileMetadataStoreProvider.cs
@@ -10,24 +10,14 @@ using Microsoft.Extensions.Options;
 
 namespace Azure.DataGateway.Service
 {
-
     /// <summary>
     /// A class describing the format of the JSON resolver configuration file.
     /// </summary>
-    public class ResolverConfig
+    /// <param name="GraphQLSchema">String Representation of graphQL schema, non escaped. This has higher priority than GraphQLSchemaFile, so if both are set this one will be used.</param>
+    /// <param name="GraphQLSchemaFile">Location of the graphQL schema file</param>
+    /// <param name="DatabaseSchema">A JSON encoded version of the information that resolvers need about schema of the schema of the database.</param>
+    public record ResolverConfig(string GraphQLSchema, string GraphQLSchemaFile, DatabaseSchema DatabaseSchema)
     {
-        /// <summary>
-        /// String Representation of graphQL schema, non escaped. This has
-        /// higher priority than GraphQLSchemaFile, so if both are set this one
-        /// will be used.
-        /// </summary>
-        public string GraphQLSchema { get; set; }
-
-        /// <summary>
-        /// Location of the graphQL schema file
-        /// </summary>
-        public string GraphQLSchemaFile { get; set; }
-
         /// <summary>
         /// A list containing metadata required to resolve the different
         /// queries in the GraphQL schema. See GraphQLQueryResolver for details.
@@ -45,12 +35,6 @@ namespace Azure.DataGateway.Service
         /// types in the GraphQL schema. See GraphqlType for details.
         /// </summary>
         public Dictionary<string, GraphQLType> GraphQLTypes { get; set; } = new();
-
-        /// <summary>
-        /// A JSON encoded version of the information that resolvers need about
-        /// schema of the schema of the database.
-        /// </summary>
-        public DatabaseSchema DatabaseSchema { get; set; }
     }
 
     /// <summary>
@@ -83,11 +67,21 @@ namespace Azure.DataGateway.Service
             };
             options.Converters.Add(new JsonStringEnumConverter());
 
-            _config = JsonSerializer.Deserialize<ResolverConfig>(jsonString, options);
+            // This feels dirty but it avoids having to make _config nullable - which would result in more
+            // down the line issues and null check requirements
+            ResolverConfig? deserializedConfig;
+            if ((deserializedConfig = JsonSerializer.Deserialize<ResolverConfig>(jsonString, options)) == null)
+            {
+                throw new JsonException("Failed to get a ResolverConfig from the provided config");
+            }
+            else
+            {
+                _config = deserializedConfig;
+            }
 
             if (string.IsNullOrEmpty(_config.GraphQLSchema))
             {
-                _config.GraphQLSchema = File.ReadAllText(_config.GraphQLSchemaFile ?? "schema.gql");
+                _config = _config with { GraphQLSchemaFile = File.ReadAllText(_config.GraphQLSchemaFile ?? "schema.gql") };
             }
 
             _queryResolvers = new();
@@ -115,7 +109,7 @@ namespace Azure.DataGateway.Service
 
         public MutationResolver GetMutationResolver(string name)
         {
-            if (!_mutationResolvers.TryGetValue(name, out MutationResolver resolver))
+            if (!_mutationResolvers.TryGetValue(name, out MutationResolver? resolver))
             {
                 throw new KeyNotFoundException("Mutation Resolver does not exist.");
             }
@@ -125,7 +119,7 @@ namespace Azure.DataGateway.Service
 
         public GraphQLQueryResolver GetQueryResolver(string name)
         {
-            if (!_queryResolvers.TryGetValue(name, out GraphQLQueryResolver resolver))
+            if (!_queryResolvers.TryGetValue(name, out GraphQLQueryResolver? resolver))
             {
                 throw new KeyNotFoundException("Query Resolver does not exist.");
             }
@@ -135,7 +129,7 @@ namespace Azure.DataGateway.Service
 
         public TableDefinition GetTableDefinition(string name)
         {
-            if (!_config.DatabaseSchema.Tables.TryGetValue(name, out TableDefinition metadata))
+            if (!_config.DatabaseSchema.Tables.TryGetValue(name, out TableDefinition? metadata))
             {
                 throw new KeyNotFoundException($"Table Definition for {name} does not exist.");
             }
@@ -145,7 +139,7 @@ namespace Azure.DataGateway.Service
 
         public GraphQLType GetGraphqlType(string name)
         {
-            if (!_config.GraphQLTypes.TryGetValue(name, out GraphQLType typeInfo))
+            if (!_config.GraphQLTypes.TryGetValue(name, out GraphQLType? typeInfo))
             {
                 throw new KeyNotFoundException($"Table Definition for {name} does not exist.");
             }

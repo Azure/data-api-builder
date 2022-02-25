b96666e904ed513f71c4864f892193173577574f
aaron burtle <aaronburtle@microsoft.com>
2022-02-25 01:50:45 -0800

    added unit testing to OdataASTVisitor to help gain 100% code coverage

--- DataGateway.Service.Tests/REST/ODataASTVisitorUnitTests.cs
+++ DataGateway.Service.Tests/REST/ODataASTVisitorUnitTests.cs
@@ -1,5 +1,5 @@
+using System;
 using System.IO;
-using System.Net;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Azure.DataGateway.Service.Exceptions;
@@ -7,6 +7,7 @@ using Azure.DataGateway.Service.Models;
 using Azure.DataGateway.Service.Resolvers;
 using Azure.DataGateway.Service.Services;
 using Azure.DataGateway.Services;
+using Microsoft.OData.Edm;
 using Microsoft.OData.UriParser;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
@@ -14,13 +15,14 @@ using Moq;
 namespace Azure.DataGateway.Service.Tests.REST
 {
     /// <summary>
-    /// Unit tests for RequestValidator.cs. Makes sure the proper primary key validation
-    /// occurs for REST requests for FindOne().
+    /// Unit tests for ODataASTVisitor.cs.
+    /// testing.
     /// </summary>
     [TestClass, TestCategory(TestCategory.MSSQL)]
     public class ODataASTVisitorUnitTests
     {
         private static FilterParser _filterParser;
+        private const string DEFAULT_ENTITY = "books";
 
         [ClassInitialize]
         public static void InitializeTestFixture(TestContext context)
@@ -39,153 +41,130 @@ namespace Azure.DataGateway.Service.Tests.REST
 
         #region Positive Tests
         /// <summary>
-        /// Simulated client request defines primary key column name and value
-        /// aligning to DB schema primary key.
+        /// Verify the correct string is parsed from the
+        /// provided filter with eq and null as value on right side.
         /// </summary>
         [TestMethod]
-        public void VisitorLeftNullRightValueFilterTest()
+        public void VisitorLeftFieldRightNullFilterTest()
         {
-            // (RestRequestContext context, IMetadataStoreProvider metadataStoreProvider)
-            Mock<IMetadataStoreProvider> metaDataStore = new();
-            Mock<RestRequestContext> context = new();
-            string entityName = "books";
-            context.SetupGet(x => x.EntityName).Returns(entityName);
-            string filterString = "?$filter=NULL eq id";
-            FilterClause ast = _filterParser.GetFilterClause(filterString, "books");
-            ODataASTVisitor visitor = new(new SqlQueryStructure(context.Object, metaDataStore.Object));
-            string filter = ast.Expression.Accept<string>(visitor);
+            PerformVisitorTest(
+                entityName: DEFAULT_ENTITY,
+                filterString: "?$filter=id eq null",
+                expected: "(id IS NULL)"
+                );
+        }
+
+        /// <summary>
+        /// Verify the correct string is parsed from the
+        /// provided filter with ne and null as value on right side.
+        /// </summary>
+        [TestMethod]
+        public void VisitorLeftFieldRightNotNullFilterTest()
+        {
+            PerformVisitorTest(
+                entityName: DEFAULT_ENTITY,
+                filterString: "?$filter=id ne null",
+                expected: "(id IS NOT NULL)"
+                );
+        }
+
+        /// <summary>
+        /// Verify the correct string is parsed from the
+        /// provided filter with > and null as value on right side.
+        /// </summary>
+        [TestMethod]
+        public void VisitorLeftFieldGreaterThanRightNullFilterTest()
+        {
+            PerformVisitorTest(
+                entityName: DEFAULT_ENTITY,
+                filterString: "?$filter=id gt null",
+                expected: "(id > NULL)"
+                );
         }
 
         #endregion
         #region Negative Tests
         /// <summary>
-        /// Simulated client request contains matching number of Primary Key columns,
-        /// but defines column that is NOT a primary key. We verify that the correct
-        /// status code and sub status code is a part of the DataGatewayException thrown.
+        /// Verifies that we are not able to parse unsupported Edm Types.
+        /// Create a constant node with invalid type and then invoke the visit
+        /// function from our visitor using that node.
         /// </summary>
         [TestMethod]
-        public void InvalidEdmTypeBoolTest()
+        public void InvalidEdmTypeReferenceTest()
         {
-            // (RestRequestContext context, IMetadataStoreProvider metadataStoreProvider)
             Mock<IMetadataStoreProvider> metaDataStore = new();
-            Mock<RestRequestContext> context = new();
-            string entityName = "books";
-            context.SetupGet(x => x.EntityName).Returns(entityName);
-            string filterString = "?$filter=id eq (publisher_id > 1)";
-            FilterClause ast = _filterParser.GetFilterClause(filterString, "books");
-            ODataASTVisitor visitor = new(new SqlQueryStructure(context.Object, metaDataStore.Object));
-            string filter = ast.Expression.Accept<string>(visitor);
+            TableDefinition tableDef = new()
+            {
+                Columns = new()
+            };
+            metaDataStore.Setup(x => x.GetTableDefinition(It.IsAny<string>())).Returns(tableDef);
+            FindRequestContext context = new(DEFAULT_ENTITY, false);
+            Mock<SqlQueryStructure> structure = new(context, metaDataStore.Object);
+            IEdmPrimitiveType primitiveType = EdmCoreModel.Instance.GetPrimitiveType(EdmPrimitiveTypeKind.Geography);
+            EdmPrimitiveTypeReference typeRef = new(primitiveType, false);
+            ConstantNode nodeIn = new(string.Empty, string.Empty, typeRef);
+            ODataASTVisitor visitor = new(structure.Object);
+            Assert.ThrowsException<NotSupportedException>(() => visitor.Visit(nodeIn));
+        }
+
+        /// <summary>
+        /// Tests that we throw an ArgumentException for comparison
+        /// of a field to a boolean value.
+        /// </summary>
+        [TestMethod]
+        public void InvalidBinaryOpTest()
+        {
+            Assert.ThrowsException<ArgumentException>(() => PerformVisitorTest(
+                entityName: DEFAULT_ENTITY,
+                filterString: "?$filter=id eq (publisher_id add 1)",
+                expected: string.Empty
+                ));
+
+        }
+
+        /// <summary>
+        /// Tests that we throw an exception when trying to use an invalid
+        /// operator in our filter. In those case an add operator.
+        /// </summary>
+        [TestMethod]
+        public void InvalidNullBinaryOpTest()
+        {
+            Assert.ThrowsException<DataGatewayException>(() => PerformVisitorTest(
+                entityName: DEFAULT_ENTITY,
+                filterString: "?$filter=null add 1",
+                expected: string.Empty
+                ));
         }
 
         #endregion
         #region Helper Methods
+
         /// <summary>
-        /// Runs the Validation method to show success/failure. Extracted to separate helper method
-        /// to avoid code duplication. Only attempt to catch DataGatewayException since
-        /// that exception determines whether we encounter an expected validation failure in case
-        /// of negative tests, vs downstream service failure.
+        /// Helper function performs the test by creating the Abstract Syntax Tree
+        /// and then traversing it with the ODataASTVisitor. We compare the resultant
+        /// filter to the expected.
         /// </summary>
-        /// <param name="findRequestContext">Client simulated request</param>
-        /// <param name="metadataStore">Mocked Config provider</param>
-        /// <param name="expectsException">True/False whether we expect validation to fail.</param>
-        /// <param name="statusCode">Integer which represents the http status code expected to return.</param>
-        /// <param name="subStatusCode">Represents the sub status code that we expect to return.</param>
-        public static void PerformTest(
-            FindRequestContext findRequestContext,
-            IMetadataStoreProvider metadataStore,
-            bool expectsException,
-            HttpStatusCode statusCode = HttpStatusCode.BadRequest,
-            DataGatewayException.SubStatusCodes subStatusCode = DataGatewayException.SubStatusCodes.BadRequest)
+        /// <param name="entityName">Represents the entity name in schema.</param>
+        /// <param name="filterString">Represents the filter to be applied.</param>
+        /// <param name="expected">The expected filter after parsing.</param>
+        private static void PerformVisitorTest(
+            string entityName,
+            string filterString,
+            string expected)
         {
-            //try
-            //{
-            //    RequestValidator.ValidatePrimaryKey(findRequestContext, _metadataStore.Object);
-
-            //    //If expecting an exception, the code should not reach this point.
-            //    if (expectsException)
-            //    {
-            //        Assert.Fail();
-            //    }
-            //}
-            //catch (DataGatewayException ex)
-            //{
-            //    //If we are not expecting an exception, fail the test. Completing test method without
-            //    //failure will pass the test, so no Assert.Pass() is necessary (nor exists).
-            //    if (!expectsException)
-            //    {
-            //        Console.Error.WriteLine(ex.Message);
-            //        throw;
-            //    }
-
-            //    // validates the status code and sub status code match the expected values.
-            //    Assert.AreEqual(statusCode, ex.StatusCode);
-            //    Assert.AreEqual(subStatusCode, ex.SubStatusCode);
-            //}
+            Mock<IMetadataStoreProvider> metaDataStore = new();
+            TableDefinition tableDef = new()
+            {
+                Columns = new()
+            };
+            metaDataStore.Setup(x => x.GetTableDefinition(It.IsAny<string>())).Returns(tableDef);
+            FindRequestContext context = new(entityName, false);
+            Mock<SqlQueryStructure> structure = new(context, metaDataStore.Object);
+            FilterClause ast = _filterParser.GetFilterClause(filterString, entityName);
+            ODataASTVisitor visitor = new(structure.Object);
+            string actual = ast.Expression.Accept<string>(visitor);
+            Assert.AreEqual(expected, actual);
         }
-
-        /// <summary>
-        /// Tries to parse a primary key route that contains duplicates.
-        /// Expectation is that this should always throw bad request exception.
-        /// </summary>
-        //private static void PerformDuplicatePrimaryKeysTest(string[] primaryKeys)
-        //{
-        //FindRequestContext findRequestContext = new(entityName: "entity", isList: false);
-        //StringBuilder primaryKeyRoute = new();
-
-        //foreach (string key in primaryKeys)
-        //{
-        //    primaryKeyRoute.Append($"{key}/1/{key}/1/");
-        //}
-
-        //// Remove the trailing slash
-        //primaryKeyRoute.Remove(primaryKeyRoute.Length - 1, 1);
-
-        //try
-        //{
-        //    RequestParser.ParsePrimaryKey(primaryKeyRoute.ToString(), findRequestContext);
-
-        //    Assert.Fail();
-        //}
-        //catch (DataGatewayException ex)
-        //{
-        //    // validates the status code and sub status code match the expected values.
-        //    Assert.AreEqual(HttpStatusCode.BadRequest, ex.StatusCode);
-        //    Assert.AreEqual(DataGatewayException.SubStatusCodes.BadRequest, ex.SubStatusCode);
-        //}
+        #endregion
     }
-
-    /// <summary>
-    /// Tests the ParsePrimaryKey method in the RequestParser class.
-    /// </summary>
-    //private static void PerformRequestParserPrimaryKeyTest(
-    //    FindRequestContext findRequestContext,
-    //    string primaryKeyRoute,
-    //    bool expectsException,
-    //    HttpStatusCode statusCode = HttpStatusCode.BadRequest,
-    //    DataGatewayException.SubStatusCodes subStatusCode = DataGatewayException.SubStatusCodes.BadRequest)
-    //{
-    //    try
-    //    {
-    //        RequestParser.ParsePrimaryKey(primaryKeyRoute.ToString(), findRequestContext);
-
-    //        //If expecting an exception, the code should not reach this point.
-    //        Assert.IsFalse(expectsException, "No exception thrown when exception expected.");
-    //    }
-    //    catch (DataGatewayException ex)
-    //    {
-    //        //If we are not expecting an exception, fail the test. Completing test method without
-    //        //failure will pass the test, so no Assert.Pass() is necessary (nor exists).
-    //        if (!expectsException)
-    //        {
-    //            Console.Error.WriteLine(ex.Message);
-    //            throw;
-    //        }
-
-    //        // validates the status code and sub status code match the expected values.
-    //        Assert.AreEqual(statusCode, ex.StatusCode);
-    //        Assert.AreEqual(subStatusCode, ex.SubStatusCode);
-    //    }
-    //}
-
-    #endregion
 }

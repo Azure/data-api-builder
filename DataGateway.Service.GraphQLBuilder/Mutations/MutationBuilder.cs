2d5703936a9145fb13b48eff3362d5d32ae0228b
Aaron Powell <me@aaron-powell.com>
2022-03-01 15:40:30 +1100

    more whitespace fixes

--- DataGateway.Service.GraphQLBuilder/Mutations/MutationBuilder.cs
+++ DataGateway.Service.GraphQLBuilder/Mutations/MutationBuilder.cs
@@ -22,7 +22,7 @@ namespace Azure.DataGateway.Service.GraphQLBuilder.Mutations
             }
 
             List<IDefinitionNode> definitionNodes = new() {
-                    new ObjectTypeDefinitionNode(null, new NameNode("Mutation"), null, new List<DirectiveNode>(), new List<NamedTypeNode>(), mutationFields),
+                new ObjectTypeDefinitionNode(null, new NameNode("Mutation"), null, new List<DirectiveNode>(), new List<NamedTypeNode>(), mutationFields),
             };
             definitionNodes.AddRange(inputs.Values);
             return new(definitionNodes);

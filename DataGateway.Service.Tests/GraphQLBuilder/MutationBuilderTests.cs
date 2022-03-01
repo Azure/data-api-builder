f852aa18cd69f69dc9c97405d4a1bd72f99a73b1
Aaron Powell <me@aaron-powell.com>
2022-03-01 16:14:27 +1100

    started some mutation tests

---
+++ DataGateway.Service.Tests/GraphQLBuilder/MutationBuilderTests.cs
@@ -0,0 +1,158 @@
+using System.Linq;
+using Azure.DataGateway.Service.GraphQLBuilder.Mutations;
+using HotChocolate.Language;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Azure.DataGateway.Service.Tests.GraphQLBuilder
+{
+    [TestClass]
+    public class MutationBuilderTests
+    {
+        [TestMethod]
+        [TestCategory("Mutation Builder - Create")]
+        [TestCategory("Schema Builder - Simple Type")]
+        public void CanGenerateCreateMutationWith_SimpleType()
+        {
+            string gql =
+                @"
+type Foo @model {
+    id: ID!
+}
+                ";
+
+            DocumentNode root = Utf8GraphQLParser.Parse(gql);
+
+            DocumentNode mutationRoot = MutationBuilder.Build(root);
+
+            ObjectTypeDefinitionNode query = GetMutationNode(mutationRoot);
+            Assert.AreEqual(1, query.Fields.Count(f => f.Name.Value == $"createFoo"));
+        }
+
+        [TestMethod]
+        [TestCategory("Mutation Builder - Create")]
+        [TestCategory("Schema Builder - Simple Type")]
+        public void CreateMutationExcludeIdFromInput_SimpleType()
+        {
+            string gql =
+                @"
+type Foo @model {
+    id: ID!
+}
+                ";
+
+            DocumentNode root = Utf8GraphQLParser.Parse(gql);
+
+            DocumentNode mutationRoot = MutationBuilder.Build(root);
+
+            ObjectTypeDefinitionNode query = GetMutationNode(mutationRoot);
+            FieldDefinitionNode field = query.Fields.First(f => f.Name.Value == $"createFoo");
+            Assert.AreEqual(0, field.Arguments.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("Mutation Builder - Create")]
+        [TestCategory("Schema Builder - Complex Type")]
+        public void CanGenerateCreateMutationWith_ComplexType()
+        {
+            string gql =
+                @"
+type Foo @model {
+    id: ID!
+    bar: String!
+    baz: Int
+}
+                ";
+
+            DocumentNode root = Utf8GraphQLParser.Parse(gql);
+
+            DocumentNode mutationRoot = MutationBuilder.Build(root);
+
+            ObjectTypeDefinitionNode query = GetMutationNode(mutationRoot);
+            Assert.AreEqual(1, query.Fields.Count(f => f.Name.Value == $"createFoo"));
+        }
+
+        [TestMethod]
+        [TestCategory("Mutation Builder - Create")]
+        [TestCategory("Schema Builder - Complex Type")]
+        public void CreateMutationExcludeIdFromInput_ComplexType()
+        {
+            string gql =
+                @"
+type Foo @model {
+    id: ID!
+    bar: String!
+    baz: Int
+}
+                ";
+
+            DocumentNode root = Utf8GraphQLParser.Parse(gql);
+
+            DocumentNode mutationRoot = MutationBuilder.Build(root);
+
+            ObjectTypeDefinitionNode query = GetMutationNode(mutationRoot);
+            FieldDefinitionNode field = query.Fields.First(f => f.Name.Value == $"createFoo");
+            InputValueDefinitionNode inputArg = field.Arguments[0];
+            InputObjectTypeDefinitionNode inputObj = (InputObjectTypeDefinitionNode)mutationRoot.Definitions.First(d => d is InputObjectTypeDefinitionNode node && node.Name == inputArg.Type.NamedType().Name);
+            Assert.AreEqual(2, inputObj.Fields.Count);
+        }
+
+        [TestMethod]
+        [TestCategory("Mutation Builder - Create")]
+        [TestCategory("Schema Builder - Nested Type")]
+        public void CanGenerateCreateMutationWith_NestedType()
+        {
+            string gql =
+                @"
+type Foo @model {
+    id: ID!
+    bar: Bar!
+}
+
+type Bar {
+    baz: Int
+}
+                ";
+
+            DocumentNode root = Utf8GraphQLParser.Parse(gql);
+
+            DocumentNode mutationRoot = MutationBuilder.Build(root);
+
+            ObjectTypeDefinitionNode query = GetMutationNode(mutationRoot);
+            Assert.AreEqual(1, query.Fields.Count(f => f.Name.Value == $"createFoo"));
+        }
+
+        [TestMethod]
+        [TestCategory("Mutation Builder - Create")]
+        [TestCategory("Schema Builder - Nested Type")]
+        public void CreateMutationExcludeIdFromInput_NestedType()
+        {
+            string gql =
+                @"
+type Foo @model {
+    id: ID!
+    bar: Bar!
+}
+
+type Bar {
+    baz: Int
+}
+                ";
+
+            DocumentNode root = Utf8GraphQLParser.Parse(gql);
+
+            DocumentNode mutationRoot = MutationBuilder.Build(root);
+
+            ObjectTypeDefinitionNode query = GetMutationNode(mutationRoot);
+            FieldDefinitionNode field = query.Fields.First(f => f.Name.Value == $"createFoo");
+            InputValueDefinitionNode inputArg = field.Arguments[0];
+            InputObjectTypeDefinitionNode inputObj = (InputObjectTypeDefinitionNode)mutationRoot.Definitions.First(d => d is InputObjectTypeDefinitionNode node && node.Name == inputArg.Type.NamedType().Name);
+            Assert.AreEqual(1, inputObj.Fields.Count);
+        }
+
+        private static ObjectTypeDefinitionNode GetMutationNode(DocumentNode mutationRoot)
+        {
+            return (ObjectTypeDefinitionNode)mutationRoot.Definitions.First(d => d is ObjectTypeDefinitionNode node && node.Name.Value == "Mutation");
+
+        }
+    }
+}

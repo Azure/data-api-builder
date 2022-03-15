0bd235231a1409bad0ceba6323d3ef9de0c98c05
Aniruddh Munde <anmunde@microsoft.com>
2022-03-14 20:09:32 -0700

    No need for virtual

--- DataGateway.Service.Tests/SqlTests/SqlMetadataProviderTests.cs
+++ DataGateway.Service.Tests/SqlTests/SqlMetadataProviderTests.cs
@@ -9,7 +9,7 @@ namespace Azure.DataGateway.Service.Tests.SqlTests
     public abstract class SqlMetadataProviderTests : SqlTestBase
     {
         [TestMethod]
-        public virtual async Task TestDerivedDatabaseSchemaIsValid()
+        public async Task TestDerivedDatabaseSchemaIsValid()
         {
             ResolverConfig runtimeConfig = _metadataStoreProvider.GetResolvedConfig();
             DatabaseSchema expectedSchema = runtimeConfig.DatabaseSchema;

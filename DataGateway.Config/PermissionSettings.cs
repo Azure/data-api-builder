d5056cd62f7cc73e131a4846c046bc2bd2ab441c
Aniruddh Munde <anmunde@microsoft.com>
2022-04-24 22:27:43 -0700

    Update DataGateway.Config/PermissionSettings.cs
    Co-authored-by: Aaron Powell <me@aaron-powell.com>

--- DataGateway.Config/PermissionSettings.cs
+++ DataGateway.Config/PermissionSettings.cs
@@ -4,7 +4,6 @@ namespace Azure.DataGateway.Config
     /// Defines who (in terms of roles) can access the entity and using which actions.
     /// </summary>
     /// <param name="Role">Name of the role to which defined permission applies.</param>
-
     /// <param name="Actions">Either a mixed-type array of a string or an object
     /// that details what actions are allowed to related roles.
     /// In a simple case, the array members are one of the following:

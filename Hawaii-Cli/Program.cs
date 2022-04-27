b0223dc8a4884835ccee85d4dbd20a73a08030d0
Abhishek Kumar <abhishekkuma@microsoft.com>
2022-04-27 23:23:20 +0530

    new console app

---
+++ Hawaii-Cli/Program.cs
@@ -0,0 +1,16 @@
+﻿using Abhicruiser.Cli.Classes;
+using System.CommandLine;
+using System.CommandLine.Invocation;
+
+var command = args.AsQueryable().FirstOrDefault();
+
+Console.WriteLine("Welcome to Abhicruiser.Cli");
+if (command == "init")
+{
+    CommandLineHelp.ParseArguments(args);
+}
+else
+{
+    Console.WriteLine($"Could not execute because the specified command was not found.");
+    Console.WriteLine("please do init to initialize the config file.");
+}

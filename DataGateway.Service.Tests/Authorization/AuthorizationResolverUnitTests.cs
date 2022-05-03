7fa9f545fa195b4e2e9fd2623af3656029b75a17
Sean Leonard <sean.leonard@microsoft.com>
2022-05-03 10:12:43 -0700

    Remove unnecessary usings.

--- DataGateway.Service.Tests/Authorization/AuthorizationResolverUnitTests.cs
+++ DataGateway.Service.Tests/Authorization/AuthorizationResolverUnitTests.cs
@@ -1,8 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Azure.DataGateway.Config;
 using Azure.DataGateway.Service.Authorization;
 using Azure.DataGateway.Service.Configurations;

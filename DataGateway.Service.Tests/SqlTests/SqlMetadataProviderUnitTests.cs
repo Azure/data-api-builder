254f1cdf201466e4cbc3aaf05b12ac9506adc71b
aaron burtle <aaronburtle@microsoft.com>
2022-05-04 16:23:29 -0700

    added connection string unit tests, updated design doc

---
+++ DataGateway.Service.Tests/SqlTests/SqlMetadataProviderUnitTests.cs
@@ -0,0 +1,93 @@
+using Azure.DataGateway.Service.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Azure.DataGateway.Service.Tests.SqlTests
+{
+    /// <summary>
+    /// Units testing for our connection string parser
+    /// to retreive schema.
+    /// </summary>
+    [TestClass, TestCategory(TestCategory.POSTGRESQL)]
+    public class SqlMetadataProviderUnitTests
+    {
+        /// <summary>
+        /// Verify we parse the connection string for the
+        /// schema correctly when it is at the end
+        /// of the connection string and is empty.
+        /// </summary>
+        [TestMethod]
+        public void CheckConnectionStringEmptySearchPathLastTest()
+        {
+            string expected = string.Empty;
+            string actual;
+            string connectionString = "Host = localhost;Database = graphql; SearchPath=";
+
+            MsSqlMetadataProvider.CheckConnectionStringForSchema(out actual, connectionString);
+            Assert.IsTrue(expected.Equals(actual));
+        }
+
+        /// <summary>
+        /// Verify we parse the connection string for the
+        /// schema correctly when it is at the end
+        /// of the connection string and is normal.
+        /// </summary>
+        [TestMethod]
+        public void CheckConnectionStringSchemaInSearchPathTest()
+        {
+            string expected = "foobar";
+            string actual;
+            string connectionString = "Host = localhost;Database = graphql; SearchPath=foobar";
+
+            MsSqlMetadataProvider.CheckConnectionStringForSchema(out actual, connectionString);
+            Assert.IsTrue(expected.Equals(actual));
+        }
+
+        /// <summary>
+        /// Verify we parse the connection string for the
+        /// schema correctly when it is first in the connection
+        /// string and is normal.
+        /// </summary>
+        [TestMethod]
+        public void CheckConnectionStringSchemaInSearchPathFirstTest()
+        {
+            string expected = "baz";
+            string actual;
+            string connectionString = "SearchPath=baz; Host = localhost;Database = graphql";
+
+            MsSqlMetadataProvider.CheckConnectionStringForSchema(out actual, connectionString);
+            Assert.IsTrue(expected.Equals(actual));
+        }
+
+        /// <summary>
+        /// Verify we parse the connection string for the schema
+        /// correctly when SearchPath does not exist in the connection
+        /// string.
+        /// </summary>
+        [TestMethod]
+        public void CheckConnectionStringNoSchemaInSearchPathTest()
+        {
+            string expected = string.Empty;
+            string actual;
+            string connectionString = "Host = localhost;Database = graphql";
+
+            MsSqlMetadataProvider.CheckConnectionStringForSchema(out actual, connectionString);
+            Assert.IsTrue(expected.Equals(actual));
+        }
+
+        /// <summary>
+        /// Verify we parse the connection string for the schema
+        /// correctly when SearchPath is first in the connection
+        /// string and empty.
+        /// </summary>
+        [TestMethod]
+        public void CheckConnectionStringEmptySearchPathFirstTest()
+        {
+            string expected = string.Empty;
+            string actual;
+            string connectionString = "SearchPath=;Host = localhost;Database = graphql";
+
+            MsSqlMetadataProvider.CheckConnectionStringForSchema(out actual, connectionString);
+            Assert.IsTrue(expected.Equals(actual));
+        }
+    }
+}

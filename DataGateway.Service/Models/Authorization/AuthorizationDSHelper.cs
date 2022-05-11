ceacce120a2f1102c548651d5d95c859f22f594e
Sean Leonard <sean.leonard@microsoft.com>
2022-05-11 15:41:47 -0700

    Added comments for the AuthZ DS Helper classes.

--- DataGateway.Service/Models/Authorization/AuthorizationDSHelper.cs
+++ DataGateway.Service/Models/Authorization/AuthorizationDSHelper.cs
@@ -2,24 +2,40 @@ using System.Collections.Generic;
 
 namespace Azure.DataGateway.Service.Models.Authorization
 {
-    // contains all the helper classes needed for creating the Authorization data structure.
+    /// <summary>
+    /// Represents the permission metadata of an entity.
+    /// An entity's top-level permission structure is a collection
+    /// of roles.
+    /// </summary>
     class EntityDS
     {
-        //key : roleName
+        /// <summary>
+        /// Given the key (roleName) returns the associated RoleDS object.
+        /// </summary>
         public Dictionary<string, RoleDS> RoleToActionMap = new();
     }
 
+    /// <summary>
+    /// Represents the permission metadata of a role
+    /// A role's top-level permission structure is a collection of
+    /// actions allowed for that role: Create, Read, Update, Delete, * (wildcard)
+    /// </summary>
     class RoleDS
     {
-        //key : actionName
+        /// <summary>
+        /// Given the key (actionName) returns the associated ActionDS object.
+        /// </summary>
         public Dictionary<string, ActionDS> ActionToColumnMap = new();
     }
 
+    /// <summary>
+    /// Represents the permission metadata of an action
+    /// An action lists both columns that are included and/or exluded
+    /// for that action.
+    /// </summary>
     class ActionDS
     {
         public HashSet<string> included = new();
         public HashSet<string> excluded = new();
     }
-
-    public record AuthorizationMetadata(string? RoleName, string? EntityName, string? ActionName, List<string>? Columns);
 }

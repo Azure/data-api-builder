652e26990861637665245a64884636aed217c619
aaron burtle <aaronburtle@microsoft.com>
2022-05-11 21:57:06 -0700

    updated some tests, new entity name, Ignore unused tests

--- DataGateway.Service.Tests/REST/RestUnitTests.cs
+++ DataGateway.Service.Tests/REST/RestUnitTests.cs
@@ -66,7 +66,7 @@ namespace Azure.DataGateway.Service.Tests.REST
 
             // Setup params to invoke function with
             // Must use valid entity name
-            string entityName = "Books";
+            string entityName = "Book";
             Operation operationType = Operation.None;
             string primaryKeyRoute = string.Empty;
 

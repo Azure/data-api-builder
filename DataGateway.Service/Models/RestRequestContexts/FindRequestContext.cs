046e1a223325aff3b38a0860249f1533a20e61b9
aaron burtle <aaronburtle@microsoft.com>
2022-05-16 11:19:31 -0700

    merging main into branch

--- DataGateway.Service/Models/RestRequestContexts/FindRequestContext.cs
+++ DataGateway.Service/Models/RestRequestContexts/FindRequestContext.cs
@@ -13,7 +13,7 @@ namespace Azure.DataGateway.Service.Models
         /// Constructor.
         /// </summary>
 
-        public FindRequestContext(string entityName, bool isList, DatabaseObject dbo, Dictionary<string, string>? mapping = null)
+        public FindRequestContext(string entityName, DatabaseObject dbo, bool isList, Dictionary<string, string>? mapping = null)
             : base(HttpRestVerbs.GET, entityName, dbo, mapping)
         {
             FieldsToBeReturned = new();

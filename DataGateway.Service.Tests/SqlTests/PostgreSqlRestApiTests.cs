55e384a63d026903e27a091bb499dd774782d034
aaron burtle <aaronburtle@microsoft.com>
2022-05-16 17:23:28 -0700

    syntax error in postgres, need quote to avoid toLowerCase

--- DataGateway.Service.Tests/SqlTests/PostgreSqlRestApiTests.cs
+++ DataGateway.Service.Tests/SqlTests/PostgreSqlRestApiTests.cs
@@ -513,7 +513,27 @@ namespace Azure.DataGateway.Service.Tests.SqlTests
                 @"
                     SELECT json_agg(to_jsonb(subq)) AS data
                     FROM (
-                        SELECT  treeId, ""species"" AS ""Scientific Name"", ""region"" AS ""United State's Region""
+                        SELECT  ""treeId"", ""species"" AS ""Scientific Name"", ""region"" AS ""United State's Region""
+                        FROM " + _integrationMappingTable + @"
+                    ) AS subq
+                "
+            },
+            {
+                "FindTestWithSingleMappedFieldsToBeReturned",
+                @"
+                    SELECT json_agg(to_jsonb(subq)) AS data
+                    FROM (
+                        SELECT  ""species"" AS ""Scientific Name""
+                        FROM " + _integrationMappingTable + @"
+                    ) AS subq
+                "
+            },
+            {
+                "FindTestWithUnMappedFieldsToBeReturned",
+                @"
+                    SELECT json_agg(to_jsonb(subq)) AS data
+                    FROM (
+                        SELECT  ""treeId""
                         FROM " + _integrationMappingTable + @"
                     ) AS subq
                 "

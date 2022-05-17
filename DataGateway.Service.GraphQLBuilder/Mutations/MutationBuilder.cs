4c9817feb9cd4d20caffb19c4b9e6fa33e8deba3
Aniruddh Munde <anmunde@microsoft.com>
2022-05-17 09:28:32 -0700

    IgnoreCase while determining MutationOperation

--- DataGateway.Service.GraphQLBuilder/Mutations/MutationBuilder.cs
+++ DataGateway.Service.GraphQLBuilder/Mutations/MutationBuilder.cs
@@ -38,8 +38,8 @@ namespace Azure.DataGateway.Service.GraphQLBuilder.Mutations
         {
             return inputTypeName switch
             {
-                string s when s.StartsWith(Operation.Create.ToString(), StringComparison.Ordinal) => Operation.Create,
-                string s when s.StartsWith(Operation.Update.ToString(), StringComparison.Ordinal) => Operation.UpdateGraphQL,
+                string s when s.StartsWith(Operation.Create.ToString(), StringComparison.OrdinalIgnoreCase) => Operation.Create,
+                string s when s.StartsWith(Operation.Update.ToString(), StringComparison.OrdinalIgnoreCase) => Operation.UpdateGraphQL,
                 _ => Operation.Delete
             };
         }

1f63522b3b0947d7e703e120a4c1bad0ab4f1f6a
Sean Leonard <sean.leonard@microsoft.com>
2022-05-25 18:22:29 -0700

    Remove unnecessary using.

--- DataGateway.Service.Tests/Authentication/EasyAuthAuthenticationUnitTests.cs
+++ DataGateway.Service.Tests/Authentication/EasyAuthAuthenticationUnitTests.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
-using System.Runtime.Serialization.Json;
 using System.Security.Claims;
 using System.Text;
 using System.Text.Json;

30d66824352b172681e38e2c3af51ee1f144f5d0
Sean Leonard <sean.leonard@microsoft.com>
2022-06-01 16:02:40 -0700

    remove unnecessary using statement

--- DataGateway.Service/Authorization/RestAuthorizationHandler.cs
+++ DataGateway.Service/Authorization/RestAuthorizationHandler.cs
@@ -1,7 +1,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Azure.DataGateway.Config;
 using Azure.DataGateway.Service.Exceptions;

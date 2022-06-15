4234986fada7240bc2e801a148fbf2484a23a40c
Ayush Agarwal <agarwalayush@microsoft.com>
2022-06-15 21:40:06 +0530

    Fixed formatting

--- DataGateway.Service/Authorization/AuthorizationResolver.cs
+++ DataGateway.Service/Authorization/AuthorizationResolver.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Security.Claims;
-using System.Text;
 using System.Text.Json;
 using System.Text.RegularExpressions;
 using Azure.DataGateway.Config;

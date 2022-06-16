00761f2eb209d83b1f4d0a7dd9fbd5fe7c8a2b99
VinnieKhanna <vinniek123@gmail.com>
2022-06-16 13:05:25 -0400

    replace == with `is`

--- DataGateway.Service.Tests/SqlTests/SqlTestBase.cs
+++ DataGateway.Service.Tests/SqlTests/SqlTestBase.cs
@@ -263,11 +263,11 @@ namespace Azure.DataGateway.Service.Tests.SqlTests
             // Initial DELETE request results in 204 no content, no exception thrown.
             // Subsequent DELETE requests result in 404, which result in an exception.
             string expected;
-            if ((operationType == Operation.Delete ||
-                 operationType == Operation.Upsert ||
-                 operationType == Operation.UpsertIncremental ||
-                 operationType == Operation.Update ||
-                 operationType == Operation.UpdateIncremental)
+            if ((operationType is Operation.Delete ||
+                 operationType is Operation.Upsert ||
+                 operationType is Operation.UpsertIncremental ||
+                 operationType is Operation.Update ||
+                 operationType is Operation.UpdateIncremental)
                 && actionResult is NoContentResult)
             {
                 expected = null;
@@ -291,7 +291,7 @@ namespace Azure.DataGateway.Service.Tests.SqlTests
                 {
                     string dbResult = await GetDatabaseResultAsync(sqlQuery);
                     // For FIND requests, null result signifies an empty result set
-                    dbResult = (operationType == Operation.Find && dbResult is null) ? "[]" : dbResult;
+                    dbResult = (operationType is Operation.Find && dbResult is null) ? "[]" : dbResult;
                     expected = $"{{\"value\":{FormatExpectedValue(dbResult)}{ExpectedNextLinkIfAny(paginated, EncodeQueryString(baseUrl), $"{expectedAfterQueryString}")}}}";
                 }
             }

0ec3d5daf6a70bab6385404b32299b53f971953b
Sean Leonard <sean.leonard@microsoft.com>
2022-06-22 09:55:58 -0700

    initial actionType relaction

--- DataGateway.Config/Action.cs
+++ DataGateway.Config/Action.cs
@@ -64,6 +64,18 @@ namespace Azure.DataGateway.Config
         string? Database);
 
     /// <summary>
+    /// Constants representing the CRUD operations utilized in DataGateway
+    /// These actions are defined in configuration and utilized in authorization.
+    /// </summary>
+    public static class ActionType
+    {
+        public const string CREATE = "create";
+        public const string READ = "read";
+        public const string UPDATE = "update";
+        public const string DELETE = "delete";
+    }
+
+    /// <summary>
     /// The REST HttpVerbs supported by the service
     /// expressed as authorization requirements.
     /// </summary>

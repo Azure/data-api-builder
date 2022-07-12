f4471873dafea12ce765d2c8c27aa661aea22069
aaron burtle <aaronburtle@microsoft.com>
2022-07-11 22:19:26 -0700

    typoe

--- DataGateway.Service/Resolvers/Sql Query Structures/SqlUpdateQueryStructure.cs
+++ DataGateway.Service/Resolvers/Sql Query Structures/SqlUpdateQueryStructure.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
-using System.Linq;using System.Net;
+using System.Linq;
+using System.Net;
 using Azure.DataGateway.Config;
 using Azure.DataGateway.Service.Exceptions;
 using Azure.DataGateway.Service.GraphQLBuilder.Mutations;

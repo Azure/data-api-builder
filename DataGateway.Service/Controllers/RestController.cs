1519f230fc8fe467383e052528d441419c87faa9
aaron burtle <aaronburtle@microsoft.com>
2022-07-11 17:42:19 -0700

    un used using directives

--- DataGateway.Service/Controllers/RestController.cs
+++ DataGateway.Service/Controllers/RestController.cs
@@ -1,8 +1,5 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Text.Json;
 using System.Threading.Tasks;
 using Azure.DataGateway.Config;
 using Azure.DataGateway.Service.Exceptions;

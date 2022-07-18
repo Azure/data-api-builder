f7ad8748824196baa0dc4d7b82b1df8370581eb8
Aaron Powell <me@aaron-powell.com>
2022-07-19 09:18:12 +1000

    removing mock token from tests

--- DataGateway.Service.Tests/GraphQLRequestExecutor.cs
+++ DataGateway.Service.Tests/GraphQLRequestExecutor.cs
@@ -28,7 +28,6 @@ namespace Azure.DataGateway.Service.Tests
                 .GetRuntimeConfiguration()
                 .GraphQLGlobalSettings.Path;
 
-            client.DefaultRequestHeaders.Add("X-MS-CLIENT-PRINCIPAL", "eyJ1c2VySWQiOiIyNTllM2JjNTE5NzU3Mzk3YTE2ZjdmMDBjMTI0NjQxYSIsInVzZXJSb2xlcyI6WyJhbm9ueW1vdXMiLCJhdXRoZW50aWNhdGVkIl0sImlkZW50aXR5UHJvdmlkZXIiOiJnaXRodWIiLCJ1c2VyRGV0YWlscyI6ImFhcm9ucG93ZWxsIiwiY2xhaW1zIjpbXX0=");
             HttpResponseMessage responseMessage = await client.PostAsJsonAsync(graphQLEndpoint, payload);
             string body = await responseMessage.Content.ReadAsStringAsync();
 

69b179fdfeabe3fe23dc2b30038921d5d2b50bf6
Ayush Agarwal <agarwalayush@microsoft.com>
2022-07-19 11:17:37 +0530

    Using Assert exception

--- DataGateway.Service.Tests/Unittests/BootStrapFailureTest.cs
+++ DataGateway.Service.Tests/Unittests/BootStrapFailureTest.cs
@@ -1,3 +1,4 @@
+using System;
 using System.IO;
 using System.Net;
 using System.Threading.Tasks;
@@ -10,6 +11,7 @@ namespace Azure.DataGateway.Service.Tests.Unittests
     [TestClass, TestCategory(TestCategory.MSSQL)]
     public class BootStrapFailureTest : SqlTestBase
     {
+        private static readonly string _compositeViewName = "books_authors";
         /// <summary>
         /// Test to validate that the runtime fails and throws an exception during bootstrap when the primary
         /// key cannot be determined for a database object.
@@ -26,25 +28,22 @@ namespace Azure.DataGateway.Service.Tests.Unittests
 
             // Add composite view whose primary key cannot be determined.
             string dbQuery = File.ReadAllText($"{_testCategory}Books.sql");
-            string compositeViewQuery = "EXEC('CREATE VIEW books_authors as SELECT books.title, authors.[name], " +
+            string compositeViewQuery = $"EXEC('CREATE VIEW {_compositeViewName} as SELECT books.title, authors.[name], " +
                 "authors.[birthdate], books.id as book_id, authors.id as author_id " +
                 "FROM dbo.books INNER JOIN dbo.book_author_link ON books.[id] = book_author_link.book_id " +
                 "INNER JOIN authors ON authors.[id] = book_author_link.author_id')";
 
-            // Execute the query to add it to the database.
+            // Execute the query to add view to the database.
             await _queryExecutor.ExecuteQueryAsync(dbQuery + compositeViewQuery, parameters: null);
             try
             {
-                await _sqlMetadataProvider.InitializeAsync();
-            }
-            catch (DataGatewayException ex)
-            {
+                DataGatewayException ex = await Assert.ThrowsExceptionAsync<DataGatewayException>(() => _sqlMetadataProvider.InitializeAsync());
                 Assert.AreEqual(HttpStatusCode.NotImplemented, ex.StatusCode);
-                Assert.AreEqual("Primary key not configured on the given database object books_authors", ex.Message);
+                Assert.AreEqual($"Primary key not configured on the given database object {_compositeViewName}", ex.Message);
             }
             finally
             {
-                string dropViewQuery = "DROP VIEW IF EXISTS books_authors";
+                string dropViewQuery = $"DROP VIEW IF EXISTS {_compositeViewName}";
                 await _queryExecutor.ExecuteQueryAsync(dropViewQuery, parameters: null);
             }
         }

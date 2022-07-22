7641dbc77e65d444b9cbcbb8a61be19af94754f0
Shyam Sundar J <shyamsundarj@microsoft.com>
2022-07-22 15:57:12 +0530

    adding one more test case

--- DataGateway.Service.Tests/GraphQLBuilder/MutationBuilderTests.cs
+++ DataGateway.Service.Tests/GraphQLBuilder/MutationBuilderTests.cs
@@ -639,7 +639,7 @@ type Foo @model {
             DisplayName = "Validates @authorize directive is NOT added.")]
         [DataRow(new string[] { "anonymous", "authenticated" }, false,
             DisplayName = "Validates @authorize directive is NOT added - multiple roles")]
-        public void CanGenerateDeleteMutationWith_SingularEntityName(IEnumerable<string> roles,
+        public void CanGenerateDeleteMutationWithSingularEntityName_ForDirectPluralEntityName(IEnumerable<string> roles,
             bool isAuthorizeDirectiveExpected)
         {
             string gql =
@@ -674,6 +674,55 @@ type Foos @model {
 
             FieldDefinitionNode deleteField =
                 query.Fields.Where(f => f.Name.Value == $"deleteFoo").First();
+            Assert.AreEqual(expected: isAuthorizeDirectiveExpected ? 1 : 0,
+                actual: deleteField.Directives.Count);
+
+        }
+
+        [DataTestMethod]
+        [TestCategory("Mutation Builder - Delete")]
+        [TestCategory("Schema Builder - Simple Type")]
+        [DataRow(new string[] { "authenticated" }, true,
+            DisplayName = "Validates @authorize directive is added.")]
+        [DataRow(new string[] { "anonymous" }, false,
+            DisplayName = "Validates @authorize directive is NOT added.")]
+        [DataRow(new string[] { "anonymous", "authenticated" }, false,
+            DisplayName = "Validates @authorize directive is NOT added - multiple roles")]
+        public void CanGenerateDeleteMutationWithSingularEntityName_ForInDirectPluralEntityName(IEnumerable<string> roles,
+            bool isAuthorizeDirectiveExpected)
+        {
+            string gql =
+    @"
+type Leaves @model {
+    id: ID!
+    bar: String!
+}
+                ";
+
+            DocumentNode root = Utf8GraphQLParser.Parse(gql);
+            Dictionary<string, EntityMetadata> entityPermissionsMap
+                = GraphQLTestHelpers.CreateStubEntityPermissionsMap(
+                    new string[] { "Leaves" },
+                    new string[] { ActionType.DELETE },
+                    roles);
+            DocumentNode mutationRoot = MutationBuilder.Build(root,
+                DatabaseType.cosmos,
+                new Dictionary<string, Entity> { { "Leaves", GenerateEmptyEntity() } },
+                entityPermissionsMap: entityPermissionsMap
+                );
+
+            ObjectTypeDefinitionNode query = GetMutationNode(mutationRoot);
+            FieldDefinitionNode field = query.Fields.First(f => f.Name.Value == $"deleteLeaf");
+            Assert.AreEqual(2, field.Arguments.Count);
+            Assert.AreEqual("id", field.Arguments[0].Name.Value);
+            Assert.AreEqual("ID", field.Arguments[0].Type.NamedType().Name.Value);
+            Assert.IsTrue(field.Arguments[0].Type.IsNonNullType());
+            Assert.AreEqual("_partitionKeyValue", field.Arguments[1].Name.Value);
+            Assert.AreEqual("String", field.Arguments[1].Type.NamedType().Name.Value);
+            Assert.IsTrue(field.Arguments[1].Type.IsNonNullType());
+
+            FieldDefinitionNode deleteField =
+                query.Fields.Where(f => f.Name.Value == $"deleteLeaf").First();
             Assert.AreEqual(expected: isAuthorizeDirectiveExpected ? 1 : 0,
                 actual: deleteField.Directives.Count);
 

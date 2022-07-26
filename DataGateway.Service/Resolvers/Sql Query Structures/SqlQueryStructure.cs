d8197177cf0f09244b645b27d2dd077cec5925f6
Sean Leonard <sean.leonard@microsoft.com>
2022-07-26 07:12:02 -0700

    update odata function call.

--- DataGateway.Service/Resolvers/Sql Query Structures/SqlQueryStructure.cs
+++ DataGateway.Service/Resolvers/Sql Query Structures/SqlQueryStructure.cs
@@ -206,10 +206,9 @@ namespace Azure.DataGateway.Service.Resolvers
             {
                 // Similar to how we have added FilterPredicates above,
                 // we will add DbPolicyPredicates here.
-                ODataASTVisitor visitor = new(this, sqlMetadataProvider);
                 try
                 {
-                    DbPolicyPredicates = GetFilterPredicatesFromOdataClause(context.DbPolicyClause, visitor);
+                    ProcessOdataClause(context.DbPolicyClause);
                 }
                 catch
                 {

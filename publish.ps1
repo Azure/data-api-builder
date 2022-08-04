db33aef770204615be400dbe64bb666b9453679b
Jarupat Jisarojito <guitarnana@gmail.com>
2022-08-03 19:44:09 -0700

    Update powershell script

--- publish.ps1
+++ publish.ps1
@@ -7,6 +7,6 @@ $BuildRoot = $PSScriptRoot
 $RIDs = "win-x64", "linux-x64", "osx-x64"
 
 foreach ($RID in $RIDs) {
-    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/engine --runtime $RID "DataGateway.Service\Azure.DataGateway.Service.csproj"
-    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/cli --runtime $RID "Hawaii-Cli\src\Hawaii.Cli.csproj"
+    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/engine --runtime $RID --no-self-contained $BuildRoot\DataGateway.Service\Azure.DataGateway.Service.csproj
+    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/cli --runtime $RID --no-self-contained $BuildRoot\Hawaii-Cli\src\Hawaii.Cli.csproj
 }

d41f63b9e47feae948317bb997e993a97e6c6ee5
Jarupat Jisarojito <guitarnana@gmail.com>
2022-08-03 17:45:42 -0700

    Add publish script and not making self-contained publish

---
+++ build.ps1
@@ -0,0 +1,12 @@
+param (
+    [Parameter (Mandatory=$true)][string] $BuildConfiguration,
+)
+
+$BuildRoot = $PSScriptRoot
+
+$RIDs = "win-x64", "linux-x64", "osx-x64"
+
+foreach ($RID in $RIDs) {
+    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/engine --runtime $RID "DataGateway.Service\Azure.DataGateway.Service.csproj"
+    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/cli --runtime $RID "Hawaii-Cli\src\Hawaii.Cli.csproj"
+}

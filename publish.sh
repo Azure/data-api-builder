c10a5d1df00a8d4a3833a0ad0159ab68cad6ec55
Jarupat Jisarojito <guitarnana@gmail.com>
2022-08-03 19:16:06 -0700

    Don't publish the project as self-contained

--- publish.sh
+++ publish.sh
@@ -6,12 +6,12 @@ RIDs=("win-x64" "linux-x64" "osx-x64")
 
 for RID in ${RIDs[@]}; do
     # Publish engine
-    cmd="dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/engine --runtime $RID $BuildRoot/DataGateway.Service/Azure.DataGateway.Service.csproj"
+    cmd="dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/engine --runtime $RID --no-self-contained $BuildRoot/DataGateway.Service/Azure.DataGateway.Service.csproj"
     echo "Running: $cmd"
     eval $cmd
 
     # Publish CLI
-    cmd="dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/cli --runtime $RID $BuildRoot/Hawaii-Cli/src/Hawaii.Cli.csproj"
+    cmd="dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/cli --runtime $RID --no-self-contained $BuildRoot/Hawaii-Cli/src/Hawaii.Cli.csproj"
     echo "Running: $cmd"
     eval $cmd
 done

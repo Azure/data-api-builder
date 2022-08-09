bc66d468f587f402aa078d1fac3f1db17bde88b8
Aniruddh Munde <anmunde@microsoft.com>
2022-08-08 20:53:15 -0700

    Rename from `DataGateway` to `DataApiBuilder` and move code under `src` (#673)
    ## What is the change?
    - Move all the source code under `src` folder like is done by HotChocolate and other open source repositories.
    - All the subcomponents under `src` do not prefix with `DataApiBuilder` since the repo name itself is `Azure/data-api-builder`
    - Namespace renames all the occurrences of `DataGateway` to `DataApiBuilder`. 
    - `DataGatewayException` renamed to `DataApiBuilderException`
    - Naming the `Hawaii-Cli` to `dab` is not included in this change.
    
    ## Testing
    Successful existing tests and engine startup

---
+++ src/Config/AuthorizationType.cs
@@ -0,0 +1,12 @@
+namespace Azure.DataApiBuilder.Config
+{
+    /// <summary>
+    /// Enumeration of Supported Authorization Types.
+    /// </summary>
+    public enum AuthorizationType
+    {
+        NoAccess,
+        Anonymous,
+        Authenticated
+    }
+}
---
+++ src/Service/Controllers/ConfigurationController.cs
@@ -0,0 +1,51 @@
+using Azure.DataApiBuilder.Service.Configurations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Azure.DataApiBuilder.Service.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ConfigurationController : Controller
+    {
+        RuntimeConfigProvider _configurationProvider;
+        public ConfigurationController(RuntimeConfigProvider configurationProvider)
+        {
+            _configurationProvider = configurationProvider;
+        }
+
+        /// <summary>
+        /// Takes in the runtime configuration, schema, connection string and optionally the
+        /// resolvers and configures the runtime. If the runtime is already configured, it will
+        /// return a conflict result.
+        /// </summary>
+        /// <param name="configuration">Runtime configuration, schema, resolvers and connection string.</param>
+        /// <returns>Ok in case of success or Conflict with the key:value.</returns>
+        [HttpPost]
+        public ActionResult Index([FromBody] ConfigurationPostParameters configuration)
+        {
+            if (_configurationProvider.TryGetRuntimeConfiguration(out _))
+            {
+                return new ConflictResult();
+            }
+
+            _configurationProvider.Initialize(
+                configuration.Configuration,
+                configuration.Schema,
+                configuration.ConnectionString);
+
+            return new OkResult();
+        }
+    }
+
+    /// <summary>
+    /// The required parameters required to configure the runtime.
+    /// </summary>
+    /// <param name="Configuration">The runtime configuration.</param>
+    /// <param name="Schema">The GraphQL schema. Can be left empty for SQL databases.</param>
+    /// <param name="ConnectionString">The database connection string.</param>
+    public record class ConfigurationPostParameters(
+        string Configuration,
+        string? Schema,
+        string ConnectionString)
+    { }
+}

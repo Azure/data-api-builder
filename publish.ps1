5271555de5c815365ddecad6a8b601dca7fa403e
Jarupat Jisarojito <guitarnana@gmail.com>
2022-08-09 12:03:28 -0700

    Update to self-contain binaries and create zip

--- publish.ps1
+++ publish.ps1
@@ -7,6 +7,9 @@ $BuildRoot = $PSScriptRoot
 $RIDs = "win-x64", "linux-x64", "osx-x64"
 
 foreach ($RID in $RIDs) {
-    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/engine --runtime $RID --no-self-contained $BuildRoot\DataGateway.Service\Azure.DataGateway.Service.csproj
-    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/cli --runtime $RID --no-self-contained $BuildRoot\Hawaii-Cli\src\Hawaii.Cli.csproj
+    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/engine --runtime $RID $BuildRoot\DataGateway.Service\Azure.DataGateway.Service.csproj
+    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/cli --runtime $RID $BuildRoot\Hawaii-Cli\src\Hawaii.Cli.csproj
+
+    Compress-Archive -Force -Path $BuildRoot/publish/$BuildConfiguration/$RID/engine/* -DestinationPath $BuildRoot/publish/$BuildConfiguration/$RID/engine.zip
+    Compress-Archive -Force -Path $BuildRoot/publish/$BuildConfiguration/$RID/cli/* -DestinationPath $BuildRoot/publish/$BuildConfiguration/$RID/cli.zip
 }

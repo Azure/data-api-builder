e608fda78bd16dcded40c93312fb6a6b6d055c65
Jarupat Jisarojito <guitarnana@gmail.com>
2022-08-09 23:10:50 -0700

    Update publish path

--- publish.ps1
+++ publish.ps1
@@ -7,7 +7,7 @@ $BuildRoot = $PSScriptRoot
 $RIDs = "win-x64", "linux-x64", "osx-x64"
 
 foreach ($RID in $RIDs) {
-    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true $BuildRoot\src\Hawaii-Cli\src\Hawaii.Cli.csproj
+    dotnet publish --configuration $BuildConfiguration --output $BuildRoot/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true $BuildRoot\src\Cli\src\Cli.csproj
 
     Compress-Archive -Force -Path $BuildRoot/publish/$BuildConfiguration/$RID/cli/* -DestinationPath $BuildRoot/publish/$BuildConfiguration/$RID/cli.zip
 }

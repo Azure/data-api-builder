ef5708c35fa37be386a8b4e5136f81cf719cfece
Jarupat Jisarojito <guitarnana@gmail.com>
2022-08-10 23:02:55 -0700

    Fix command error.

--- publish.ps1
+++ publish.ps1
@@ -9,7 +9,7 @@ $BuildRoot = $PSScriptRoot
 $RIDs = "win-x64", "linux-x64", "osx-x64"
 
 foreach ($RID in $RIDs) {
-    dotnet publish --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true -p:Version=$(DabVersion) $BuildRoot\src\Cli\src\Cli.csproj
+    dotnet publish --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true -p:Version=$DabVersion $BuildRoot\src\Cli\src\Cli.csproj
 
     Compress-Archive -Force -Path $BuildOutputDir/publish/$BuildConfiguration/$RID/cli/* -DestinationPath $BuildRoot/publish/$BuildConfiguration/$RID/cli_$DabVersion.zip
 }
--- publish.sh
+++ publish.sh
@@ -10,12 +10,12 @@ RIDs=("win-x64" "linux-x64" "osx-x64")
 
 for RID in ${RIDs[@]}; do
     # Publish CLI
-    cmd="dotnet publish --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true -p:Version=$(DabVersion) $BuildRoot/src/Cli/src/Cli.csproj"
+    cmd="dotnet publish --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true -p:Version=$DabVersion $BuildRoot/src/Cli/src/Cli.csproj"
     echo "Running: $cmd"
     eval $cmd
 
     pushd $BuildOutputDir/publish/$BuildConfiguration/$RID/cli
-    cmd="zip -q -r ../cli_$(DabVersion).zip *"
+    cmd="zip -q -r ../cli_$DabVersion.zip *"
     echo "Running: $cmd"
     eval $cmd
     popd

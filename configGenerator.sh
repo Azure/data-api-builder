d9ddfabbf7ac225f942bed32f770e236811d6911
Shyam Sundar J <shyamsundarj@microsoft.com>
2022-08-15 01:09:47 +0530

    removing empty lines in shell script

--- configGenerator.sh
+++ configGenerator.sh
@@ -3,10 +3,8 @@ databaseType=$1;
 commandsFileName="${databaseType}Commands.txt";
 absolutePath=$(pwd -P);
 commandsFileNameWithPath="$absolutePath/$commandsFileName";
-
 #Fetching the path to dab dll
 pathToDLL=$(find ./src/out/cli -name dab.dll)
-
 #Generating the config using dab commands
 echo "Generating config file using dab commands";
 while read -r command; do

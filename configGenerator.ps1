a4e78179622724ab03d686e49d4856820beb706d
Shyam Sundar J <shyamsundarj@microsoft.com>
2022-08-14 21:06:23 +0530

    modifying scripts to use full path to read from .txt files

--- configGenerator.ps1
+++ configGenerator.ps1
@@ -2,11 +2,12 @@ param(
     [Parameter (Mandatory=$true)][string] $databaseType
 )
 
-$fileName = $databaseType + "Commands.txt";
+$commandsFileName = $databaseType + "Commands.txt";
+$commandsFileWithPath = $PSScriptRoot + "\" + $commandsFileName;
 $pathToDabExe = Get-ChildItem -Path .\src\out\cli\ -Recurse -include "dab.exe" | %{$_.FullName} ;
 
 # Generating the config files using dab commands
-foreach($command in Get-Content $fileName){
+foreach($command in Get-Content $commandsFileWithPath){
     $commandToExecute = $pathToDabExe + " " + $command;
     Invoke-Expression $commandToExecute;
 }
--- configGenerator.sh
+++ configGenerator.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 databaseType=$1;
-fileName="${databaseType}Commands.txt";
+commandsFileName="${databaseType}Commands.txt";
+absolutePath=$(pwd -P);
+commandsFileNameWithPath="$absolutePath/$commandsFileName";
+
 #Fetching the path to dab dll
 pathToDLL=$(find ./src/out/cli -name dab.dll)
+
 #Generating the config using dab commands
 echo "Generating config file using dab commands";
 while read -r command; do
   cmd="dotnet ${pathToDLL} ${command}";
   eval $cmd;
-done <$fileName;
-echo "Successfully generated the config file."
+done <$commandsFileNameWithPath;

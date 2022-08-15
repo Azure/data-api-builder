8ef2aa97ac8bbe2363c6d929747e52c941ce701f
Shyam Sundar J <shyamsundarj@microsoft.com>
2022-08-15 11:33:32 +0530

    using absolute path to find dll path

--- configGenerator.sh
+++ configGenerator.sh
@@ -4,7 +4,7 @@ commandsFileName="${databaseType}Commands.txt";
 absolutePath="$( cd "$(dirname "$0")" ; pwd -P )";
 commandsFileNameWithPath="$absolutePath/$commandsFileName";
 #Fetching the path to dab dll
-pathToDLL=$(find ./src/out/cli -name dab.dll)
+pathToDLL="$(find "$absolutePath/src/out/cli" -name dab.dll)";
 #Generating the config using dab commands
 echo "Generating config file using dab commands";
 while read -r command; do

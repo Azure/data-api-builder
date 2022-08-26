9b54a1a670a90c34c3256f6c3b0650bcb52ed391
Shyam Sundar J <shyamsundarj@microsoft.com>
2022-08-26 17:59:41 +0530

    adding comments in sh script

--- configGenerator.sh
+++ configGenerator.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 commandFiles=("MsSqlCommands.txt" "MySqlCommands.txt" "PostgreSqlCommands.txt" "CosmosCommands.txt")
+#Fetching absolute path of this script
 absolutePath="$( cd "$(dirname "$0")" ; pwd -P )";
 cliOutputPath="$absolutePath/src/out/cli";
-#Fetching the path to dab dll
+#Fetching the path of dab dll
 pathToDLL=$(find $cliOutputPath -name dab.dll)
 #Generating the config using dab commands
 echo "Generating config file using dab commands";

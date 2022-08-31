34e08cc4492b96f31c21106def86a1afd46ae1fc
Jarupat Jisarojito <guitarnana@gmail.com>
2022-08-31 13:23:20 -0700

    Split package and zip into 2 tasks.

--- publish.ps1
+++ publish.ps1
@@ -2,14 +2,24 @@ param (
     [Parameter (Mandatory=$true)][string] $BuildConfiguration
     [Parameter (Mandatory=$true)][string] $BuildOutputDir
     [Parameter (Mandatory=$true)][string] $DabVersion
+    [Parameter (Mandatory=$false)][switch] $Package
+    [Parameter (Mandatory=$false)][switch] $CreateZip
 )
 
 $BuildRoot = $PSScriptRoot
 
 $RIDs = "win-x64", "linux-x64", "osx-x64"
 
-foreach ($RID in $RIDs) {
-    dotnet publish --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true -p:Version=$DabVersion $BuildRoot\src\Cli\src\Cli.csproj
+if ($Package)
+{
+    foreach ($RID in $RIDs) {
+        dotnet publish --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true -p:Version=$DabVersion $BuildRoot\src\Cli\src\Cli.csproj
+    }
+}
 
-    Compress-Archive -Force -Path $BuildOutputDir/publish/$BuildConfiguration/$RID/cli/* -DestinationPath $BuildRoot/publish/$BuildConfiguration/$RID/cli_$DabVersion.zip
+if ($CreateZip)
+{
+    foreach ($RID in $RIDs) {
+        Compress-Archive -Force -Path $BuildOutputDir/publish/$BuildConfiguration/$RID/cli/* -DestinationPath $BuildRoot/publish/$BuildConfiguration/$RID/cli_$DabVersion.zip
+    }
 }

3cbe84c951facbdd8a0db63feab8aaf4972e03b4
aaron burtle <aaronburtle@microsoft.com>
2022-08-31 21:33:16 -0700

    convert abstract class console to ILogger

--- src/Service.Tests/Unittests/RuntimeConfigPathUnitTests.cs
+++ src/Service.Tests/Unittests/RuntimeConfigPathUnitTests.cs
@@ -142,7 +142,6 @@ namespace Azure.DataApiBuilder.Service.Tests.UnitTests
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex.Message);
                 Assert.AreEqual(exceptionType, ex.GetType());
                 Assert.AreEqual(exceptionMessage, ex.Message);
                 Assert.AreEqual(2, configProviderLogger.Invocations.Count);

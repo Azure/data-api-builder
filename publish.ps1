063ac74d20d869a8669e624fe087e9e961864bd0
Jarupat Jisarojito <guitarnana@gmail.com>
2022-08-31 21:39:32 -0700

    Write powershell command

--- publish.ps1
+++ publish.ps1
@@ -13,13 +13,17 @@ $RIDs = "win-x64", "linux-x64", "osx-x64"
 if ($Package)
 {
     foreach ($RID in $RIDs) {
-        dotnet publish --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true -p:Version=$DabVersion $BuildRoot\src\Cli\src\Cli.csproj
+        $cmd = "dotnet publish --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$RID/cli --runtime $RID --self-contained true -p:Version=$DabVersion $BuildRoot/src/Cli/src/Cli.csproj"
+        Write-Host $cmd
+        Invoke-Expression $cmd
     }
 }
 
 if ($CreateZip)
 {
     foreach ($RID in $RIDs) {
-        Compress-Archive -Force -Path $BuildOutputDir/publish/$BuildConfiguration/$RID/cli/* -DestinationPath $BuildRoot/publish/$BuildConfiguration/$RID/cli_$DabVersion.zip
+        $cmd = "Compress-Archive -Force -Path $BuildOutputDir/publish/$BuildConfiguration/$RID/cli/* -DestinationPath $BuildOutputDir/publish/$BuildConfiguration/$RID/cli_$DabVersion.zip"
+        Write-Host $cmd
+        Invoke-Expression $cmd
     }
 }

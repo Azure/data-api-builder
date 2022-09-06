a361596b31b539daccee2125d8b38c73f3d80c76
Aniruddh Munde <anmunde@microsoft.com>
2022-09-06 00:14:56 -0700

    Cache the default access token

--- src/Service/Resolvers/MsSqlQueryExecutor.cs
+++ src/Service/Resolvers/MsSqlQueryExecutor.cs
@@ -12,7 +12,14 @@ namespace Azure.DataApiBuilder.Service.Resolvers
     {
         public const string DATABASE_SCOPE = @"https://database.windows.net/.default";
 
+        /// <summary>
+        /// The Managed Identity Access Token string obtained from the configuration controller.
+        /// </summary>
         private readonly string? _managedIdentityAccessToken;
+
+        public DefaultAzureCredential AzureCredential { get; set; } = new();
+
+        private AccessToken? _defaultAccessToken;
 
         public MsSqlQueryExecutor(
             RuntimeConfigProvider runtimeConfigProvider,
@@ -24,14 +31,23 @@ namespace Azure.DataApiBuilder.Service.Resolvers
         }
 
         /// <summary>
-        /// Modified the properties of the supplied connection to support managed identity access.
+        /// Modifies the properties of the supplied connection to support managed identity access.
         /// In the case of MsSql, gets access token if deemed necessary and sets it on the connection.
+        /// The supplied connection should already have a connection string.
         /// </summary>
         /// <param name="conn">The supplied connection to modify for managed identity access.</param>
         public override async Task HandleManagedIdentityAccessIfAnyAsync(DbConnection conn)
         {
             SqlConnection sqlConn = (SqlConnection)conn;
-            string? accessToken = _managedIdentityAccessToken ?? await TryGetAccessTokenAsync(ConnectionString);
+
+            // If the configuration controller provided a managed identity access token use that,
+            // else use the default saved access token if still valid.
+            // Get a new token only if the saved token is null or expired.
+            string? accessToken = _managedIdentityAccessToken ??
+                (IsDefaultAccessTokenValid() ?
+                    ((AccessToken)_defaultAccessToken!).Token :
+                    await TryGetAccessTokenAsync(conn.ConnectionString));
+
             if (accessToken is not null)
             {
                 QueryExecutorLogger.LogTrace("Using access token obtained from DefaultAzureCredential to connect to database.");
@@ -40,24 +56,36 @@ namespace Azure.DataApiBuilder.Service.Resolvers
         }
 
         /// <summary>
-        /// Determines if access token needs to be obtained or not based on the
-        /// properties specified in the connection string.
+        /// Determines if the saved default azure credential's access token is valid and not expired.
+        /// </summary>
+        /// <returns>True if valid, false otherwise.</returns>
+        private bool IsDefaultAccessTokenValid()
+        {
+            return _defaultAccessToken is not null &&
+                ((AccessToken)_defaultAccessToken!).ExpiresOn.CompareTo(System.DateTimeOffset.Now) > 0;
+        }
+
+        /// <summary>
+        /// Gets an access token using DefaultAzureCredentials
+        /// if none of UserID, Password or Authentication method are specified
+        /// in the connection string since they have higher precedence
+        /// and any attempt to use an access token in their presence would lead to
+        /// a System.InvalidOperationException.
         /// </summary>
         /// <param name="connString"></param>
         /// <returns>True when access token should be obtained, false otherwise.</returns>
-        private static async Task<string?> TryGetAccessTokenAsync(string connString)
+        private async Task<string?> TryGetAccessTokenAsync(string connString)
         {
             SqlConnectionStringBuilder connStringBuilder = new(connString);
             if (string.IsNullOrEmpty(connStringBuilder.UserID) &&
                string.IsNullOrEmpty(connStringBuilder.Password) &&
                connStringBuilder.Authentication == SqlAuthenticationMethod.NotSpecified)
             {
-                DefaultAzureCredential credential = new();
-                AccessToken defaultAccessToken =
-                    await credential.GetTokenAsync(
+                _defaultAccessToken =
+                    await AzureCredential.GetTokenAsync(
                         new TokenRequestContext(new[] { DATABASE_SCOPE }));
 
-                return defaultAccessToken.Token;
+                return ((AccessToken)_defaultAccessToken!).Token;
             }
 
             return null;

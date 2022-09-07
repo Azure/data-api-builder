b96c79e337e197055e37ae05ab5760ff7b491fdd
Shyam Sundar J <shyamsundarj@microsoft.com>
2022-09-07 18:07:39 +0530

    adding test for ParallelReadOperations

---
+++ src/Service.Tests/ConcurrentTests/SqlTests/ParallelReadTests.js
@@ -0,0 +1,102 @@
+import { check } from 'k6';
+import http from 'k6/http';
+
+let headers = {
+  'content-type': 'application/json'
+};
+
+const parameters = {
+  headers: headers
+}
+
+
+// The batch and batchPerHost options override the default number of parallel requests and connections
+// The thresholds property declares the condition to determine success or failure of the test.
+// As this test is intended to validate the API responses, all the checks must succeed to declare 
+// the test successful.
+export const options = {
+  batch: 5,
+  batchPerHost: 5,
+  thresholds: {
+    checks: ['rate>=1.00']
+  }
+}
+
+export default function () {
+
+  let bookQuery = `
+  query getBookById($id: Int!){
+    book_by_pk(id: $id) {
+      id
+      title
+    }
+  }
+  `;
+
+  let authorQuery = `
+      query getAuthorById($id : Int!){
+        author_by_pk(id: $id) {
+          id
+          name
+        }
+      }
+    `;
+
+  // Each REST or GraphQL request is created as a named request. Named requests are useful
+  // for validaing the responses.
+  const queryNames = ['bookQuery1', 'bookQuery2', 'notebookQuery', 'authorQuery1', 'authorQuery2'];
+
+  // Expected respone body for each request
+  const expectedResponses = {
+    'bookQuery1': { "data": { "book_by_pk": { "id": 1, "title": "Awesome book" } } },
+    'bookQuery2': { "data": { "book_by_pk": { "id": 2, "title": "Also Awesome book" } } },
+    'notebookQuery': { "value": [{ "id": 2, "notebookname": "Notebook2", "color": "green", "ownername": "Ani" }] },
+    'authorQuery1': { "data": { "author_by_pk": { "id": 124, "name": "Aniruddh" } } },
+    'authorQuery2': { "value": [{ "id": 125, "name": "Aniruddh", "birthdate": "2001-01-01" }] }
+  };
+
+  const requests = {
+    'bookQuery1': {
+      method: 'POST',
+      url: 'https://localhost:5001/graphql/',
+      body: JSON.stringify({ query: bookQuery, variables: { "id": 1 } }),
+      params: parameters
+    },
+    'bookQuery2': {
+      method: 'POST',
+      url: 'https://localhost:5001/graphql/',
+      body: JSON.stringify({ query: bookQuery, variables: { "id": 2 } }),
+      params: parameters
+    },
+    'notebookQuery': {
+      method: 'GET',
+      url: 'https://localhost:5001/api/Notebook/id/2',
+      body: null,
+      params: parameters
+    },
+    'authorQuery1': {
+      method: 'POST',
+      url: 'https://localhost:5001/graphql/',
+      body: JSON.stringify({ query: authorQuery, variables: { "id": 124 } }),
+      params: parameters
+    },
+    'authorQuery2': {
+      method: 'GET',
+      url: 'https://localhost:5001/api/Author/id/125',
+      body: null,
+      params: parameters
+    }
+  };
+
+  // Performs all the graphQL and REST requests in parallel
+  const responses = http.batch(requests);
+
+  // Validations for the API responses
+  queryNames.forEach(queryName => {
+    check(responses[queryName], {
+      'Validate no errors': responses[queryName].error.length == 0,
+      'Validate API response': JSON.stringify(expectedResponses[queryName]) == responses[queryName].body
+    });
+  });
+
+};

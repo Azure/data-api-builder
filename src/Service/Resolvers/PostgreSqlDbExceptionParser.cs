acc9fe4376631ec70c700dc4e56cd89f4b17db24
Ayush Agarwal <agarwalayush@microsoft.com>
2022-09-09 12:00:28 +0530

    Adding error code description for pg

--- src/Service/Resolvers/PostgreSqlDbExceptionParser.cs
+++ src/Service/Resolvers/PostgreSqlDbExceptionParser.cs
@@ -11,13 +11,29 @@ namespace Azure.DataApiBuilder.Service.Resolvers
         public PostgreSqlDbExceptionParser(RuntimeConfigProvider configProvider) : base(configProvider)
         {
             badRequestErrorCodes = new() {
-                "23000",    // integrity_constraint_violation
-                "23001",    // restrict_violation
-                "23502",    // not_null_violation
-                "23503",    // foreign_key_violation
-                "23505",    // unique_violation
-                "23514",    // check_violation
-                "23P01"     // exclusion_violation
+                // integrity_constraint_violation, occurs when an insert/update/delete statement violates
+                // a foreign key,primary key, check or unique constraint.
+                "23000",
+
+                // restrict_violation
+                "23001",
+
+                // not_null_violation, occurs when we try to null out a field in table which is declared as non-nullable.
+                "23502",
+
+                // foreign_key_violation, occurs when an insertion violates foreign key constraint.
+                "23503",
+
+                // unique_violation, occurs when an insertion on a table tries to insert a duplicate value for a unique field.
+                "23505",
+
+                // check_violation,The CHECK constraint ensures that all values in a column satisfy certain conditions.
+                // Check violation occurs when a check constraint fails. For eg. name like '.dab' when name = 'DAB' fails the check.
+                "23514",
+
+                // exclusion_violation, The EXCLUDE constraint ensures that if any two rows are compared on the specified column(s)
+                // or expression(s) using the specified operator(s), not all of these comparisons will return TRUE.
+                "23P01"
             };
         }
     }

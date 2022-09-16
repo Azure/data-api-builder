2d01eb2b0ab98a3ab91fe101ecb647e5dbe41191
Aniruddh Munde <anmunde@microsoft.com>
2022-09-15 19:11:45 -0700

    Use using for DbConnection and move DbDataReader usage into QueryExecutor

--- src/Service/Resolvers/IQueryExecutor.cs
+++ src/Service/Resolvers/IQueryExecutor.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
 using System.Data.Common;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 
 namespace Azure.DataApiBuilder.Service.Resolvers
@@ -10,12 +13,44 @@ namespace Azure.DataApiBuilder.Service.Resolvers
     public interface IQueryExecutor
     {
         /// <summary>
-        /// Execute sql text with parameters and return result set.
+        /// Executes sql text with the given parameters and
+        /// uses the function dataReaderHandler to process
+        /// the results from the DbDataReader and return into an object of type TResult.
         /// </summary>
         /// <param name="sqltext">SQL text to be executed.</param>
         /// <param name="parameters">The parameters used to execute the SQL text.</param>
-        /// <returns>DbDataReader object for reading the result set.</returns>
-        public Task<DbDataReader> ExecuteQueryAsync(string sqltext, IDictionary<string, object?> parameters);
+        /// <param name="onlyExtract">List of columns to extract. Extracts all if unspecified.</param>
+        ///<returns>An object formed using the results of the query as returned by the given handler.</returns>
+        public Task<TResult?> ExecuteQueryAsync<TResult>(
+            string sqltext,
+            IDictionary<string, object?> parameters,
+            Func<DbDataReader, List<string>?, Task<TResult?>> dataReaderHandler,
+            List<string>? onlyExtract = null);
+
+        /// <summary>
+        /// Extract the rows from the given db data reader to populate
+        /// the JsonArray to be returned.
+        /// </summary>
+        /// <param name="dbDataReader">A Db data reader.</param>
+        /// <param name="onlyExtract">List of columns to extract. Extracts all if unspecified.</param>
+        /// <returns>A JsonArray with each element corresponding to the row in the dbDataReader.</returns>
+        public Task<JsonArray?> GetJsonArrayAsync(DbDataReader dbDataReader, List<string>? onlyExtract = null);
+
+        /// <summary>
+        /// Extract the rows from the given db data reader to deserialize into
+        /// a Json object of type TResult to be returned.
+        /// </summary>
+        /// <param name="dbDataReader">A Db data reader.</param>
+        /// <returns>A Json object of type TResult.</returns>
+        public Task<TResult?> GetJsonResultAsync<TResult>(DbDataReader dbDataReader, List<string>? onlyExtract = null);
+
+        /// <summary>
+        /// Extracts a single row from DbDataReader and format it so it can be used as a parameter to a query execution
+        /// </summary>
+        /// <param name="dbDataReader">A Db data reader</param>
+        /// <param name="onlyExtract">List of columns to extract. Extracts all if unspecified.</param>
+        ///<returns>A dictionary representing the row in <c>ColumnName: Value</c> format, null if no row was found</returns>
+        public Task<Dictionary<string, object?>?> ExtractRowFromDbDataReader(DbDataReader dbDataReader, List<string>? onlyExtract = null);
 
         /// <summary>
         /// Wrapper for DbDataReader.ReadAsync.
@@ -23,13 +58,6 @@ namespace Azure.DataApiBuilder.Service.Resolvers
         /// be reported to the user
         /// </summary>
         public Task<bool> ReadAsync(DbDataReader reader);
-
-        /// <summary>
-        /// Extracts a single row from DbDataReader and format it so it can be used as a parameter to a query execution
-        /// </summary>
-        /// <param name="onlyExtract">List of columns to extract. Extracts all if unspecified.</param>
-        ///<returns>A dictionary representating the row in <c>ColumnName: Value</c> format, null if no row was found</returns>
-        public Task<Dictionary<string, object?>?> ExtractRowFromDbDataReader(DbDataReader dbDataReader, List<string>? onlyExtract = null);
 
         /// <summary>
         /// Modified the properties of the supplied connection to support managed identity access.
--- src/Service/Resolvers/QueryExecutor.cs
+++ src/Service/Resolvers/QueryExecutor.cs
@@ -2,6 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 using System.Threading.Tasks;
 using Azure.DataApiBuilder.Config;
 using Azure.DataApiBuilder.Service.Configurations;
@@ -30,15 +33,19 @@ namespace Azure.DataApiBuilder.Service.Resolvers
             QueryExecutorLogger = logger;
         }
 
-        /// <summary>
-        /// Executes sql text that return result set.
-        /// </summary>
-        /// <param name="sqltext">Sql text to be executed.</param>
-        /// <param name="parameters">The parameters used to execute the SQL text.</param>
-        /// <returns>DbDataReader object for reading the result set.</returns>
-        public virtual async Task<DbDataReader> ExecuteQueryAsync(string sqltext, IDictionary<string, object?> parameters)
+        /// <inheritdoc/>
+        public async Task<TResult?> ExecuteQueryAsync<TResult>(
+            string sqltext,
+            IDictionary<string, object?> parameters,
+            Func<DbDataReader, List<string>?, Task<TResult?>> dataReaderHandler,
+            List<string>? onlyExtract = null)
         {
-            TConnection conn = new()
+            if (dataReaderHandler is null)
+            {
+                throw new ArgumentNullException(nameof(dataReaderHandler));
+            }
+
+            using TConnection conn = new()
             {
                 ConnectionString = ConnectionString,
             };
@@ -61,7 +68,8 @@ namespace Azure.DataApiBuilder.Service.Resolvers
 
             try
             {
-                return await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+                using DbDataReader dbDataReader = await cmd.ExecuteReaderAsync(CommandBehavior.CloseConnection);
+                return await dataReaderHandler(dbDataReader, onlyExtract);
             }
             catch (DbException e)
             {
@@ -136,6 +144,68 @@ namespace Azure.DataApiBuilder.Service.Resolvers
             }
 
             return row;
+        }
+
+        /// <inheritdoc />
+        /// <Note>The parameter onlyExtract
+        /// is not used but is added to conform to the signature of the db data reader handler
+        /// function argument of ExecuteQueryAsync.</Note>
+        public async Task<JsonArray?> GetJsonArrayAsync(
+            DbDataReader dbDataReader,
+            List<string>? onlyExtract = null)
+        {
+            Dictionary<string, object?>? resultRecord;
+            JsonArray resultArray = new();
+
+            while ((resultRecord = await ExtractRowFromDbDataReader(dbDataReader)) is not null)
+            {
+                JsonElement result = JsonSerializer.Deserialize<JsonElement>(JsonSerializer.Serialize(resultRecord));
+                resultArray.Add(result);
+            }
+
+            return resultArray;
+        }
+
+        /// <inheritdoc />
+        public async Task<TResult?> GetJsonResultAsync<TResult>(
+            DbDataReader dbDataReader,
+            List<string>? onlyExtract = null)
+        {
+            TResult? jsonDocument = default;
+
+            // Parse Results into Json and return
+            //
+            if (dbDataReader.HasRows)
+            {
+                // Make sure to get the complete json string in case of large document.
+                jsonDocument =
+                    JsonSerializer.Deserialize<TResult>(
+                        await GetJsonStringFromDbReader(dbDataReader));
+            }
+            else
+            {
+                QueryExecutorLogger.LogInformation("Did not return enough rows in the JSON result.");
+            }
+
+            return jsonDocument;
+        }
+
+        private async Task<string> GetJsonStringFromDbReader(DbDataReader dbDataReader)
+        {
+            StringBuilder jsonString = new();
+            // Even though we only return a single cell, we need this loop for
+            // MS SQL. Sadly it splits FOR JSON PATH output across multiple
+            // cells if the JSON consists of more than 2033 bytes:
+            // Sources:
+            // 1. https://docs.microsoft.com/en-us/sql/relational-databases/json/format-query-results-as-json-with-for-json-sql-server?view=sql-server-2017#output-of-the-for-json-clause
+            // 2. https://stackoverflow.com/questions/54973536/for-json-path-results-in-ssms-truncated-to-2033-characters/54973676
+            // 3. https://docs.microsoft.com/en-us/sql/relational-databases/json/use-for-json-output-in-sql-server-and-in-client-apps-sql-server?view=sql-server-2017#use-for-json-output-in-a-c-client-app
+            while (await ReadAsync(dbDataReader))
+            {
+                jsonString.Append(dbDataReader.GetString(0));
+            }
+
+            return jsonString.ToString();
         }
     }
 }

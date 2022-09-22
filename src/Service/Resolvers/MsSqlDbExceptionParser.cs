ccec79bb523c0f3f6273ff487774a40b1637b3f4
Ayush Agarwal <34566234+ayush3797@users.noreply.github.com>
2022-09-22 11:42:09 +0530

    Handling Retry Logic in DataApiBuilder (#792)
    * Initial progress on retry logic
    
    * Attempting to fix tests
    
    * Adding policy as an instance variable
    
    * Initial progess on Unit tests for retry logic
    
    * Fixing formatting issues
    
    * Maybe an issue with async?
    
    * Adding testcategory
    
    * Moving unit tests to existing test classes
    
    * Adding test for successful query execution after n retries
    
    * Adding comments and summary
    
    * More comments for clarity
    
    * Moving createsqlexception function to sqltesthelper class
    
    * Finalising list of transient errors
    
    * Addressing nits
    
    * Adding constant for error code used in test
    
    * More nits
    
    * Addressing nits
    
    * Adding variable to buildprops for polly
    
    * Removing inappropriate error codes from mysql
    
    * Adding code comment for transienterrorcodes definition
    
    * Adding comments to CreateSqlException method
    
    * Removing inappropriate error codes
    
    * Removing error code due to client error
    
    Co-authored-by: Ayush Agarwal <agarwalayush@microsoft.com>

--- src/Service/Resolvers/MsSqlDbExceptionParser.cs
+++ src/Service/Resolvers/MsSqlDbExceptionParser.cs
@@ -38,13 +38,47 @@ namespace Azure.DataApiBuilder.Service.Resolvers
                 "548"
             })
         {
+            TransientErrorCodes = new(){
+                // Transient error codes compiled from:
+                // https://github.com/dotnet/efcore/blob/main/src/EFCore.SqlServer/Storage/Internal/SqlServerTransientExceptionDetector.cs
+                "20", "64", "121", "233", "601", "617", "669", "921", "997", "1203", "1204", "1205", "1221", "1807", "3935", "3960",
+                "3966", "4060", "4221", "8628", "8645", "8651", "9515", "10053", "10054", "10060", "10922", "10928", "10929", "10936",
+                "14355", "17197", "20041", "40197", "40501", "40613", "41301", "41302", "41305", "41325", "41839", "49918", "49919", "49920",
+
+                // Transient error codes compiled from:
+                // https://docs.microsoft.com/en-us/dotnet/api/microsoft.data.sqlclient.sqlconfigurableretryfactory?view=sqlclient-dotnet-standard-4.1
+                "1222", "40143", "40540",
+
+                // Transient error codes compiled from:
+                // https://docs.microsoft.com/en-us/azure/azure-sql/database/troubleshoot-common-errors-issues?view=azuresql
+                "615", "926",
+
+                // These errors mainly occur when the SQL Server client can't connect to the server.
+                // This may happen when the client cannot resolve the name of the server or the name of the server is incorrect.
+                "53", "11001",
+
+                // Transient error codes compiled from:
+                // https://docs.microsoft.com/en-us/sql/relational-databases/errors-events/database-engine-events-and-errors?view=sql-server-ver16
+                "18456"
+            };
         }
 
-        /// <inheritdoc/>
+        /// <summary>
+        /// Helper method to get the HttpStatusCode for the exception based on the 'Number' of the exception.
+        /// </summary>
+        /// <param name="e">The exception thrown as a result of execution of the request.</param>
+        /// <returns>status code to be returned in the response.</returns>
         public override HttpStatusCode GetHttpStatusCodeForException(DbException e)
         {
             string errorNumber = ((SqlException)e).Number.ToString();
             return BadRequestErrorCodes.Contains(errorNumber) ? HttpStatusCode.BadRequest : HttpStatusCode.InternalServerError;
+        }
+
+        /// <inheritdoc/>
+        public override bool IsTransientException(DbException e)
+        {
+            string errorNumber = ((SqlException)e).Number.ToString();
+            return TransientErrorCodes!.Contains(errorNumber);
         }
     }
 }

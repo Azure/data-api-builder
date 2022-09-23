5653ccc83084cdc36b4ba34f49a0be2ec3ef7347
Aniruddh Munde <anmunde@microsoft.com>
2022-09-23 15:39:15 -0700

    Allow nullable relationship fields in GraphQL (#808)
    ## Why make this change?
    
    Closes #711 
    Currently, we restrict all nested fields to be non-nullable. This translates to GraphQL errors when there are no relationships for some of the rows in the underlying database object. 
    We need to allow nullable nested( aka relationship) fields if the underlying foreign key is nullable.
    
    ## What is this change?
    
    - The SchemaConverter in GraphQLBuilder now looks at the ForeignKeyDefinition to find if any of the referencing(or referenced) columns are null and accordingly set the relationship field to be nullable or not. 
    - It looks at referencing columns if the source entity appears as the ReferencingDbObject in the RelationshipPair. Similarly it looks at referenced columns if it appears as the ReferencedDbObject.
    
    ## How was this tested?
    - [X] Unit Tests
    - [x] Integration Tests
    
    ## Sample Request(s)
    ```
    {
      comic_by_pk(id: 1) {
      title
      series {
        name
        }
      }
    }
    ```
    
    Response
    ```
    {
      "data": {
        "comic_by_pk": {
          "title": "Star Trek",
          "series": null
        }
      }
    }
    ```

--- src/Service/Services/MetadataProviders/CosmosSqlMetadataProvider.cs
+++ src/Service/Services/MetadataProviders/CosmosSqlMetadataProvider.cs
@@ -160,7 +160,7 @@ namespace Azure.DataApiBuilder.Service.Services.MetadataProviders
             throw new NotImplementedException();
         }
 
-        public IEnumerable<KeyValuePair<string, DatabaseObject>> GetEntityNamesAndDbObjects()
+        public IDictionary<string, DatabaseObject> GetEntityNamesAndDbObjects()
         {
             throw new NotImplementedException();
         }

0c3a5110b6452032ce58bea2cb661e7c1140c9dc
aaron burtle <aaronburtle@microsoft.com>
2022-09-23 15:41:07 -0700

    unused using directive

--- src/Service/Program.cs
+++ src/Service/Program.cs
@@ -8,7 +8,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 
 namespace Azure.DataApiBuilder.Service
 {

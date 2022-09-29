75ce77e345e64f96b579a49c4f08af3a6894978c
Abhishek Kumar <abhishekkuma@microsoft.com>
2022-09-29 21:40:43 +0530

    Revert "Fix bug for hostMode in CLI (#823)"
    This reverts commit 8978e56b5a5762118c6012984b65f43b5be0a337.

--- src/Cli/src/Program.cs
+++ src/Cli/src/Program.cs
@@ -14,8 +14,7 @@ namespace Cli
         /// <returns>0 on success, -1 on failure.</returns>
         public static int Main(string[] args)
         {
-            Parser parser = new(settings => settings.CaseInsensitiveEnumValues = true);
-            ParserResult<object>? result = parser.ParseArguments<InitOptions, AddOptions, UpdateOptions, StartOptions>(args)
+            ParserResult<object>? result = Parser.Default.ParseArguments<InitOptions, AddOptions, UpdateOptions, StartOptions>(args)
                 .WithParsed<InitOptions>(options =>
                 {
                     bool isSuccess = ConfigGenerator.TryGenerateConfig(options);

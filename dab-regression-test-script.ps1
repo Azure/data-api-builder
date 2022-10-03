a6fd1e019760a5016b9e4ba93a75c3de4412830c
Abhishek Kumar <abhishekkuma@microsoft.com>
2022-10-03 13:18:55 +0530

    adding more tests

--- dab-regression-test-script.ps1
+++ dab-regression-test-script.ps1
@@ -1,15 +1,36 @@
 param (
     [Parameter (Mandatory=$true)][string] $BuildConfiguration,
-    [Parameter (Mandatory=$true)][string] $BuildOutputDir,
     [Parameter (Mandatory=$true)][string] $DabVersion
 )
 
-# Check version
-$ver = Invoke-expression "./src/out/cli/$BuildConfiguration/net6.0/dab --version"
+# Dab executable
+$executableDAB = "./src/out/cli/$BuildConfiguration/net6.0/dab"
 
-describe MyTest {
-    it 'verifies something' {
+describe RegressionTest {
+    it 'Check Version' {
+        $ver = Invoke-expression "$executableDAB --version"
         $ver.Contains("dab $DabVersion") | Should -Be True
+    }
+
+    it 'Check Command Help Window' {
+        $helpTexts = Invoke-expression "$executableDAB --help"
+        $helpWritterOutput = ""
+        foreach ($helpText in $helpTexts)
+        {
+            $helpWritterOutput += $helpTexts
+        }
+
+        # Verifying all the supported commands are displayed on the help window
+        $helpWritterOutput.Contains("init") | Should -Be True
+        $helpWritterOutput.Contains("add") | Should -Be True
+        $helpWritterOutput.Contains("update") | Should -Be True
+        $helpWritterOutput.Contains("start") | Should -Be True
+    }
+
+    it 'Check Config File is generated' {
+        $configFileName = "dab-config-regression-test.json"
+        Invoke-expression "$executableDAB init -c $configFileName --database-type mssql --connection-string xxxx"
+        Test-Path -Path $configFileName | Should -Be True
     }
 }
 

7b86ab2b78dccafb3221e300686aec3201e4b40b
Abhishek Kumar <abhishekkuma@microsoft.com>
2022-10-05 19:43:53 +0530

    updating smoke test file

--- dab-smoke-test-script.ps1
+++ dab-smoke-test-script.ps1
@@ -26,44 +26,41 @@ $executableFileDirectory = "$BuildOutputDir/publish/$BuildConfiguration/$RID/dab
 $executableDAB = "$executableFileDirectory/dab"
 
 # Install dab nuget
-$installCommand = "dotnet tool install -g --add-source $BuildOutputDir/nupkg dab --version $DabVersion"
-Invoke-Expression $installCommand
-$nugetDAB = "dab"
+# $installCommand = "dotnet tool install -g --add-source $BuildOutputDir/nupkg dab --version $DabVersion"
+# Invoke-Expression $installCommand
+# $nugetDAB = "dab"
 
-$dabPackages = {$nugetDAB, $executableDAB}
-foreach ($dab in $dabPackages)
-{
-    describe SmokeTest {
-        it 'Check Version' {
-            $ver = Invoke-expression "$dab --version"
-            Write-Host("installed-version: {$ver}")
-            Write-Host("expected-version: {$DabVersion}")
-            $ver.Contains("dab $DabVersion") | Should -Be True
-        }
-
-        it 'Check Command Help Window' {
-            $helpTexts = Invoke-expression "$dab --help"
-
-            # Converting to object[] to string
-            $helpWritterOutput = ""
-            foreach ($helpText in $helpTexts)
-            {
-                $helpWritterOutput += $helpTexts
-            }
-
-            # Verifying all the supported commands are displayed on the help window
-            $helpWritterOutput.Contains("init") | Should -Be True
-            $helpWritterOutput.Contains("add") | Should -Be True
-            $helpWritterOutput.Contains("update") | Should -Be True
-            $helpWritterOutput.Contains("start") | Should -Be True
-        }
-
-        it 'Check Config File is generated' {
-            $configFileName = "dab-config-smoke-test.json"
-            Invoke-expression "$dab init -c $configFileName --database-type mssql --connection-string xxxx"
-            Test-Path -Path $configFileName | Should -Be True
-        }
-        
+describe SmokeTest {
+    it 'Check Version' {
+        $ver = Invoke-expression "$executableDAB --version"
+        Write-Host("installed-version: {$ver}")
+        Write-Host("expected-version: {$DabVersion}")
+        $ver.Contains("dab $DabVersion") | Should -Be True
     }
+
+    it 'Check Command Help Window' {
+        $helpTexts = Invoke-expression "$executableDAB --help"
+
+        # Converting to object[] to string
+        $helpWritterOutput = ""
+        foreach ($helpText in $helpTexts)
+        {
+            $helpWritterOutput += $helpTexts
+        }
+
+        # Verifying all the supported commands are displayed on the help window
+        $helpWritterOutput.Contains("init") | Should -Be True
+        $helpWritterOutput.Contains("add") | Should -Be True
+        $helpWritterOutput.Contains("update") | Should -Be True
+        $helpWritterOutput.Contains("start") | Should -Be True
+    }
+
+    it 'Check Config File is generated' {
+        $configFileName = "dab-config-smoke-test.json"
+        Invoke-expression "$executableDAB init -c $configFileName --database-type mssql --connection-string xxxx"
+        Test-Path -Path $configFileName | Should -Be True
+    }
+    
 }
+
 

67732c28eca5e42ee6e3de2975be33b1a5c5fe56
Abhishek Kumar <abhishekkuma@microsoft.com>
2022-10-06 11:35:16 +0530

    fix formatting

--- dab-smoke-test-script.ps1
+++ dab-smoke-test-script.ps1
@@ -55,7 +55,5 @@ describe SmokeTest {
         Invoke-expression "$executableDAB init -c $configFileName --database-type mssql --connection-string xxxx"
         Test-Path -Path $configFileName | Should -Be True
     }
-    
 }
-
 

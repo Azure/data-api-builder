7173e1789941f686a0e602864bccfc75073d5ffe
Aniruddh Munde <anmunde@microsoft.com>
2022-10-07 20:20:38 -0700

    Remove need for accept-encoding

--- src/Service.Tests/Configuration/ConfigurationTests.cs
+++ src/Service.Tests/Configuration/ConfigurationTests.cs
@@ -693,7 +693,6 @@ namespace Azure.DataApiBuilder.Service.Tests.Configuration
             // Adding the following headers simulates an interactive browser request.
             request.Headers.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36");
             request.Headers.Add("accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9");
-            request.Headers.Add("accept-encoding", "gzip, deflate, br");
 
             HttpResponseMessage response = await client.SendAsync(request);
             Assert.AreEqual(expectedStatusCode, response.StatusCode);

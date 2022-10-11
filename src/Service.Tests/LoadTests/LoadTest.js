8f4599b52ee9bd3445e77037748e68f94bf78772
Shyam Sundar J <shyamsundarj@microsoft.com>
2022-10-11 16:18:48 +0530

    adding load testing script

--- src/Service.Tests/LoadTests/LoadTest.js
+++ src/Service.Tests/LoadTests/LoadTest.js
@@ -2,19 +2,21 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-    stages: [
-      {duration: '10s', target: 50},
-      {duration: '10s', target: 5000},
-      {duration: '10s', target: 10000},
-      {duration: '300s', target: 10000},
-      {duration: '10s', target: 5000},
-      {duration: '10s', target: 0}
-    ]
-    // vus: 50,
-    // duration: '5m'
+  /*stages: [
+    {duration: '10s', target: 50},
+    {duration: '10s', target: 500},
+    {duration: '10s', target: 1000},
+    {duration: '300s', target: 1000},
+    {duration: '10s', target: 500},
+    {duration: '10s', target: 0}
+  ]*/
+    vus: 50,
+    duration: '3m'
 };
 
 let headers = {
+  'X-MS-CLIENT-PRINCIPAL': 'eyJJZGVudGl0eVByb3ZpZGVyIjoiZ2l0aHViIiwiVXNlcklkIjpudWxsLCJVc2VyRGV0YWlscyI6bnVsbCwiVXNlclJvbGVzIjpbImFub255bW91cyIsImF1dGhlbnRpY2F0ZWQiLCJwb2xpY3lfdGVzdGVyXzAxIiwicG9saWN5X3Rlc3Rlcl8wMiIsInBvbGljeV90ZXN0ZXJfMDMiLCJwb2xpY3lfdGVzdGVyXzA0IiwicG9saWN5X3Rlc3Rlcl8wNSIsInBvbGljeV90ZXN0ZXJfMDYiLCJwb2xpY3lfdGVzdGVyXzA3IiwicG9saWN5X3Rlc3Rlcl8wOCIsInBvbGljeV90ZXN0ZXJfMDkiLCJwb2xpY3lfdGVzdGVyX3VwZGF0ZV9ub3JlYWQiXSwiQ2xhaW1zIjpudWxsfQ==',
+  'X-MS-API-ROLE': 'authenticated',
   'content-type': 'application/json'
 };
 
@@ -24,37 +26,46 @@ const parameters = {
 
 export default function () {
     let bookQuery = `
-  query getBookById($id: Int!){
-    book_by_pk(id: $id) {
-      id
-      title
-      publishers {
-           id
-           name
-         }
-    }
-  }
+        query getBookById($id: Int!){
+            book_by_pk(id: $id) {
+              id
+              title
+            }
+        }
   `;
   // publishers {
   //   id
   //   name
   // }
 
+  let bookAllQuery = `{
+    books {
+      items{
+        id
+        title
+      }
+    }
+  
+  }`;
 
   let authorQuery = `
       query getAuthorById($id : Int!){
         author_by_pk(id: $id) {
           id
           name
-          books {
-               items{
-                 id
-                 title
-               }
-             }
         }
       }
     `;
+
+  let publisherQuery = `query getPublishers {
+    publishers {
+      items {
+        id
+        name
+      }
+    }
+  }
+  `;
 
     // books {
     //   items{
@@ -65,7 +76,7 @@ export default function () {
 
   // Each REST or GraphQL request is created as a named request. Named requests are useful
   // for validaing the responses.
-  const queryNames = ['bookQuery1', 'bookQuery2', 'notebookQuery', 'authorQuery1', 'authorQuery2'];
+  const queryNames = ['bookQuery1', 'bookQuery2', 'publisherQuery', 'authorQuery1', 'authorQuery2'];
   //const queryNames = ['bookQuery1', 'bookQuery2', 'authorQuery1'];
 
   const requests = {
@@ -78,13 +89,13 @@ export default function () {
     'bookQuery2': {
       method: 'POST',
       url: 'https://localhost:5001/graphql/',
-      body: JSON.stringify({ query: bookQuery, variables: { "id": 2 } }),
+      body: JSON.stringify({ query: bookAllQuery }),
       params: parameters
     },
-    'notebookQuery': {
-      method: 'GET',
-      url: 'https://localhost:5001/api/Notebook/id/2',
-      body: null,
+    'publisherQuery': {
+      method: 'POST',
+      url: 'https://localhost:5001/graphql/',
+      body: JSON.stringify({ query: publisherQuery }) ,
       params: parameters
     },
     'authorQuery1': {
@@ -94,9 +105,9 @@ export default function () {
       params: parameters
     },
     'authorQuery2': {
-      method: 'GET',
-      url: 'https://localhost:5001/api/Author/id/125',
-      body: null,
+      method: 'POST',
+      url: 'https://localhost:5001/graphql/',
+      body: JSON.stringify({ query: authorQuery, variables: { "id": 125 } }),
       params: parameters
     }
   };

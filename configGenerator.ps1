2edc47ec1c28e0f176ff3841df0c7ba49f5e84ef
Shyam Sundar J <shyamsundarj@microsoft.com>
2022-10-13 10:24:01 +0530

    generating config files using postbuild tasks

--- configGenerator.ps1
+++ configGenerator.ps1
@@ -1,9 +1,21 @@
 $PSDefaultParameterValues['*:Encoding'] = 'utf8';
 
 $commandFiles = "MsSqlCommands.txt", "MySqlCommands.txt", "PostgreSqlCommands.txt", "CosmosCommands.txt";
+
 $workingDirectory = $PSScriptRoot + "\src\Service\";
 # During start-up engine looks for config files inside /src/Service directory.
 Set-Location $workingDirectory;
+
+$configFiles = "dab-config.MsSql.json", "dab-config.MySql.json", "dab-config.PostgreSql.json", "dab-config.Cosmos.json";
+
+#Deleting files with same name if they exist
+foreach($configFile in $configFiles)
+{
+    if(Test-Path $configFile)
+    {
+        Remove-Item $configFile;
+    }
+}
 
 $cliBuildOutputPath = $PSScriptRoot + "\src\out\cli\";
 
--- configGenerator.sh
+++ configGenerator.sh
@@ -6,8 +6,20 @@ cliOutputPath="$absolutePath/src/out/cli";
 #Fetching the path of dab dll file
 pathToDLL=$(find $cliOutputPath -name dab.dll)
 workingDirectory="$absolutePath/src/Service/"
+
+configFiles=("dab-config.MsSql.json" "dab-config.MySql.json" "dab-config.PostgreSql.json" "dab-config.Cosmos.json")
+
 # During start-up engine looks for config files inside /src/Service directory.
 cd $workingDirectory;
+
+# Deleting existing config files
+for configFile in "${configFiles[@]}"
+do
+    if test -f "$configFile"; then
+        eval "rm ${configFile}"
+    fi
+done
+
 # Generating the config using dab commands
 echo "Generating config file using dab commands";
 for file in "${commandFiles[@]}"

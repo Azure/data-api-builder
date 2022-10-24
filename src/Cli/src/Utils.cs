c9410e80d2ff3a2ff5e65611da02ac6f998ef79e
Sean Leonard <sean.leonard@microsoft.com>
2022-10-24 15:17:37 -0700

    Remove Legacy devModeDefaultAuth config flag, replacing with new Authentication Handler for extensibility.

--- src/Cli/src/Utils.cs
+++ src/Cli/src/Utils.cs
@@ -311,7 +311,17 @@ namespace Cli
         {
             string[]? corsOriginArray = corsOrigin is null ? new string[] { } : corsOrigin.ToArray();
             Cors cors = new(Origins: corsOriginArray);
-            AuthenticationConfig authenticationConfig = new(Provider: EasyAuthType.StaticWebApps.ToString());
+
+            AuthenticationConfig authenticationConfig;
+            if (devModeDefaultAuth.GetValueOrDefault())
+            {
+                authenticationConfig = new(Provider: SimulatorType.Simulator.ToString());
+            }
+            else
+            {
+                authenticationConfig = new(Provider: EasyAuthType.StaticWebApps.ToString());
+            }
+
             return new HostGlobalSettings(
                 Mode: hostMode,
                 IsDevModeDefaultRequestAuthenticated: devModeDefaultAuth,

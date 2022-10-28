3f12459cc0db29b9c2e6af5e19f6a7cc29ab13c2
Sean Leonard <sean.leonard@microsoft.com>
2022-10-28 14:30:12 -0700

    Authentication Simulator - New Implementation to fix GraphQL Authorization Handler Behavior (#899)
    * Modifications to refactor dev-mode-authenticate mode.
    
    * Adding SimulatorAuthenticationHandler to enable extensible and straightforward simulation of authenticated users.
    
    * updated startup logic.
    
    * Add unit tests
    
    * remove commented code.
    
    * add integration tests and revert changes to launchSettings.json
    
    * remove spaces
    
    * Remove Legacy devModeDefaultAuth config flag, replacing with new Authentication Handler for extensibility.
    
    * remove legacy comments from simulatorauthenticationhandler.
    
    * Remove launchSettings changes
    
    * updated wording in comments
    
    * updated method signature and get rid of simulator type. Elaborated on comments and added datarow for tests.
    
    * Updated comment and authentication configuration conditional.
    
    * Fix references, conditionals, and added back dev-mode-authenticate unit tests for backwards compatibility
    
    * fix spacing.
    
    * fixed paren
    
    * add developermode gate on Simulator usage.
    
    * proper naming of header creation.
    
    * update simulator defaults constant to be consistent.
    Consolidate webhost builder for unit tests of simulator and easy auth.
    
    * Fixes comments and removes usings.
    
    * Updated comment for simulator restricted to devmode.
    
    * update comment for simulator authN handler aspnetcore ClaimsIdentity and AuthenticationTicket usage.
    
    * updated comparison from == to string.equals
    
    * simplified conditional
    
    * removed unused param from constructor
    
    * remove using
    
    * remove redundant role setting

---
+++ src/Service.Tests/Authorization/SimulatorIntegrationTests.cs
@@ -0,0 +1,126 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Azure.DataApiBuilder.Config;
+using Azure.DataApiBuilder.Service.Authorization;
+using Azure.DataApiBuilder.Service.Configurations;
+using Azure.DataApiBuilder.Service.Tests.SqlTests;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Azure.DataApiBuilder.Service.Tests.Authorization
+{
+    [TestClass]
+    public class SimulatorIntegrationTests
+    {
+        private const string MSSQL_ENVIRONMENT = TestCategory.MSSQL;
+        private const string SIMULATOR_CONFIG = "simulator-config.json";
+        private static TestServer _server;
+        private static HttpClient _client;
+
+        /// <summary>
+        /// Generate and write the custom simulator runtime config file.
+        /// Instantiate server and client one time for all tests because
+        /// the tests only validate how the engine responds to requests
+        /// and not variations in configuration settings.
+        /// </summary>
+        [ClassInitialize]
+        public static void SetupAsync(TestContext context)
+        {
+            SetupCustomRuntimeConfiguration();
+            string[] args = new[]
+            {
+                $"--ConfigFileName={SIMULATOR_CONFIG}"
+            };
+
+            _server = new(Program.CreateWebHostBuilder(args));
+            _client = _server.CreateClient();
+        }
+
+        /// <summary>
+        /// Tests REST and GraphQL requests against the engine when configured
+        /// with the authentication simulator.
+        /// - Ensures authentication errors (HTTP 401 Unauthorized) are not returned
+        /// because the Simulator provider guarantees that all requests are authenticated
+        /// and not necessarily authorized.
+        /// - Test entity: Journal
+        /// - The role AuthorizationHandlerTester has read permissions on Journal
+        /// and the roles Anonymous and Authenticated do not have read permissions on Journal.
+        /// - Validate that requests fails authorization (HTTP 403 Forbidden) if the clientRole
+        /// used does not match role permissions, and succeed (HTTP 200 OK) otherwise.
+        /// </summary>
+        /// <param name="clientRole">Role for which the engine should evaluate authorization.</param>
+        /// <param name="expectError">Whether an error is expected for the requests.</param>
+        /// <param name="expectedStatusCode">Response's Http StatusCode.</param>
+        /// <returns></returns>
+        [TestCategory(TestCategory.MSSQL)]
+        [DataTestMethod]
+        [DataRow("Anonymous", true, HttpStatusCode.Forbidden, DisplayName = "Simulator - Anonymous role does not have proper permissions.")]
+        [DataRow("Authenticated", true, HttpStatusCode.Forbidden, DisplayName = "Simulator - Authenticated but Authenticated role does not have proper permissions.")]
+        [DataRow("authorizationHandlerTester", false, HttpStatusCode.OK, DisplayName = "Simulator - Successful access with role: AuthorizationHandlerTester")]
+        public async Task TestSimulatorRequests(string clientRole, bool expectError, HttpStatusCode expectedStatusCode)
+        {
+            string graphQLQueryName = "journal_by_pk";
+            string graphQLQuery = @"{
+                journal_by_pk(id: 1) {
+                    id,
+                    journalname 
+                }
+                }";
+            string expectedResult = @"{ ""id"":1,""journalname"":""Journal1""}";
+
+            JsonElement actual = await GraphQLRequestExecutor.PostGraphQLRequestAsync(
+                _client,
+                configProvider: _server.Services.GetRequiredService<RuntimeConfigProvider>(),
+                query: graphQLQuery,
+                queryName: graphQLQueryName,
+                variables: null,
+                clientRoleHeader: clientRole
+                );
+
+            if (expectError)
+            {
+                SqlTestHelper.TestForErrorInGraphQLResponse(
+                    actual.ToString(),
+                    message: "The current user is not authorized to access this resource.",
+                    path: @"[""journal_by_pk""]"
+                );
+            }
+            else
+            {
+                SqlTestHelper.PerformTestEqualJsonStrings(expectedResult, actual.ToString());
+            }
+
+            HttpRequestMessage request = new(HttpMethod.Get, "api/Journal");
+            request.Headers.Add(AuthorizationResolver.CLIENT_ROLE_HEADER, clientRole);
+            HttpResponseMessage response = await _client.SendAsync(request);
+            Assert.AreEqual(expectedStatusCode, response.StatusCode);
+        }
+
+        private static void SetupCustomRuntimeConfiguration()
+        {
+            RuntimeConfigProvider configProvider = TestHelper.GetRuntimeConfigProvider(MSSQL_ENVIRONMENT);
+            RuntimeConfig config = configProvider.GetRuntimeConfiguration();
+
+            AuthenticationConfig authenticationConfig = new(Provider: AuthenticationConfig.SIMULATOR_AUTHENTICATION);
+            HostGlobalSettings customHostGlobalSettings = config.HostGlobalSettings with { Authentication = authenticationConfig };
+            JsonElement serializedCustomHostGlobalSettings =
+                JsonSerializer.SerializeToElement(customHostGlobalSettings, RuntimeConfig.SerializerOptions);
+
+            Dictionary<GlobalSettingsType, object> customRuntimeSettings = new(config.RuntimeSettings);
+            customRuntimeSettings.Remove(GlobalSettingsType.Host);
+            customRuntimeSettings.Add(GlobalSettingsType.Host, serializedCustomHostGlobalSettings);
+
+            RuntimeConfig configWithCustomHostMode =
+                config with { RuntimeSettings = customRuntimeSettings };
+
+            File.WriteAllText(
+                SIMULATOR_CONFIG,
+                JsonSerializer.Serialize(configWithCustomHostMode, RuntimeConfig.SerializerOptions));
+        }
+    }
+}
---
+++ src/Service/AuthenticationHelpers/AuthenticationSimulator/SimulatorAuthenticationDefaults.cs
@@ -0,0 +1,13 @@
+namespace Azure.DataApiBuilder.Service.AuthenticationHelpers.AuthenticationSimulator
+{
+    /// <summary>
+    /// Default values related to SimulatorAuthentication handler.
+    /// </summary>
+    public static class SimulatorAuthenticationDefaults
+    {
+        /// <summary>
+        /// The default value used for SimulatorAuthenticationOptions.AuthenticationScheme.
+        /// </summary>
+        public const string AUTHENTICATIONSCHEME = "SimulatorAuthentication";
+    }
+}

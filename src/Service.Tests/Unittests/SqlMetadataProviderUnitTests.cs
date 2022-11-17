ac064e0584426b96003421ba0a38fd36ec730e86
Shyam Sundar J <shyamsundarj@microsoft.com>
2022-11-17 19:02:17 +0530

    ignoring a test

--- src/Service.Tests/Unittests/SqlMetadataProviderUnitTests.cs
+++ src/Service.Tests/Unittests/SqlMetadataProviderUnitTests.cs
@@ -97,6 +97,7 @@ namespace Azure.DataApiBuilder.Service.Tests.UnitTests
         [DataRow("Servers=<>;Database=<>;Persist Security Info=False;Integrated Security=True;MultipleActiveResultSets=False;Connection Timeout=5;")]
         [DataRow("DO NOT EDIT, look at CONTRIBUTING.md on how to run tests")]
         [DataRow("")]
+        [Ignore]
         public async Task CheckExceptionForBadConnectionStringForMySql(string connectionString)
         {
             DatabaseEngine = TestCategory.MYSQL;

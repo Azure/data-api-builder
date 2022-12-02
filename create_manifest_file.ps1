f0150a0de02534ee8dac4fb320f4d35b4b5624e0
Abhishek Kumar <abhishekkuma@microsoft.com>
2022-12-02 17:48:24 +0530

    testing

--- create_manifest_file.ps1
+++ create_manifest_file.ps1
@@ -80,5 +80,5 @@ if($versionArray.Length -gt $maxVersionCount){
 
 # Updating the manifest file 
 # Keeping Depth as 4, as by default ConvertTo-Json only support conversion till depth 2.
-ConvertTo-Json -Depth 4 $versionArray | Out-File $BuildOutputDir/dab-manifest.json
+$versionArray | ConvertTo-Json -Depth 4 | Out-File $BuildOutputDir/dab-manifest.json
 

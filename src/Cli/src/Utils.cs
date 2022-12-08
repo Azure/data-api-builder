cb198b3a2f563e2af2b0b6d647edafcccd6b1bac
aaronburtle <93220300+aaronburtle@users.noreply.github.com>
2022-12-07 23:06:03 -0800

    Clarify the usage of config files on startup (#1026)
    * fix orderby for null int types
    
    * clarify config loading messages
    
    * Update MsSqlCommands.txt
    
    * Update MySqlCommands.txt
    
    * Update PostgreSqlCommands.txt
    
    * updated cli test
    
    * updated cli test
    
    * added clarifying comment
    
    Co-authored-by: abhishekkumams <102276754+abhishekkumams@users.noreply.github.com>

--- src/Cli/src/Utils.cs
+++ src/Cli/src/Utils.cs
@@ -465,7 +465,7 @@ namespace Cli
             if (!string.IsNullOrEmpty(userProvidedConfigFile))
             {
                 /// The existence of user provided config file is not checked here.
-                Console.WriteLine($"Using config file: {userProvidedConfigFile}");
+                Console.WriteLine($"User provided config file: {userProvidedConfigFile}");
                 RuntimeConfigPath.CheckPrecedenceForConfigInEngine = false;
                 runtimeConfigFile = userProvidedConfigFile;
                 return true;

ef49ad26237a3141733c28c08b4bdbb6a863de19
Aniruddh Munde <anmunde@microsoft.com>
2023-01-28 21:37:00 -0800

    Create cert directory

--- scripts/start-mssql-server.bash
+++ scripts/start-mssql-server.bash
@@ -1,24 +1,28 @@
 #!/bin/bash
 
-DockerSQLPass=$0
+DOCKER_SQL_PASS=$0
+CERT_DIR=/container/customerdb
+
+# Create directory to store certificate
+mkdir -p $CERT_DIR
 
 # Create self-signed certificate.
-openssl req -x509 -nodes -newkey rsa:2048 -subj '/CN=localhost' -keyout /container/customerdb/mssql.key -out /container/customerdb/mssql.pem -days 365
+openssl req -x509 -nodes -newkey rsa:2048 -subj '/CN=localhost' -keyout $CERT_DIR/mssql.key -out $CERT_DIR/mssql.pem -days 365
 
 # Assign read permissions to all.
-chmod 444 /container/customerdb/mssql.pem
-chmod 444 /container/customerdb/mssql.key
+chmod 444 $CERT_DIR/mssql.pem
+chmod 444 $CERT_DIR/mssql.key
 
 # Create mssql.conf file with the desired tlscert properties.
-echo "[network]" >> /container/customerdb/mssql.conf
-echo "tlscert = /var/opt/mssql/mssql.pem" >> /container/customerdb/mssql.conf
-echo "tlskey = /var/opt/mssql/mssql.key" >> /container/customerdb/mssql.conf
-echo "tlsprotocols = 1.2" >> /container/customerdb/mssql.conf
-echo "forceencryption = 1" >> /container/customerdb/mssql.conf
+echo "[network]" >> $CERT_DIR/mssql.conf
+echo "tlscert = /var/opt/mssql/mssql.pem" >> $CERT_DIR/mssql.conf
+echo "tlskey = /var/opt/mssql/mssql.key" >> $CERT_DIR/mssql.conf
+echo "tlsprotocols = 1.2" >> $CERT_DIR/mssql.conf
+echo "forceencryption = 1" >> $CERT_DIR/mssql.conf
 
 # Install certificate as trusted for the client connection to succeed.
-mv /container/customerdb/mssql.pem /container/customerdb/mssql.crt
-cp /container/customerdb/mssql.crt /usr/local/share/ca-certificates
+mv $CERT_DIR/mssql.pem $CERT_DIR/mssql.crt
+cp $CERT_DIR/mssql.crt /usr/local/share/ca-certificates
 
 # Start mssql-server by volume mounting the cert, key and conf files.
-docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=$DockerSQLPass" -p 1433:1433 --name customerdb -h customerdb -v /container/customerdb/mssql.conf:/var/opt/mssql/mssql.conf -v /container/customerdb/mssql.pem:/var/opt/mssql/mssql.pem -v /container/customerdb/mssql.key:/var/opt/mssql/mssql.key -d mcr.microsoft.com/mssql/server:2019-latest
+docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=$DOCKER_SQL_PASS" -p 1433:1433 --name customerdb -h customerdb -v $CERT_DIR/mssql.conf:/var/opt/mssql/mssql.conf -v $CERT_DIR/mssql.pem:/var/opt/mssql/mssql.pem -v $CERT_DIR/mssql.key:/var/opt/mssql/mssql.key -d mcr.microsoft.com/mssql/server:2019-latest

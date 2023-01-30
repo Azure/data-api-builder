6e62350a9ae074d8436b12209b6e3b7a7bfaa3ec
Abhishek Kumar <abhishekkuma@microsoft.com>
2023-01-30 14:25:41 +0530

    removing duplicate version print on console

--- dab-smoke-test-script.ps1
+++ dab-smoke-test-script.ps1
@@ -31,7 +31,7 @@ describe SmokeTest {
         $ver = Invoke-expression "$executableDAB --version"
         Write-Host("installed-version: {$ver}")
         Write-Host("expected-version: {$DabVersion}")
-        $ver.Contains("dab $DabVersion") | Should -Be True
+        $ver.Contains("Microsoft.DataApiBuilder $DabVersion") | Should -Be True
     }
 
     it 'Check Command Help Window' {

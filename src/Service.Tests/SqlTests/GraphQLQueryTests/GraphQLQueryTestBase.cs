51ea502e468728dca696dc3cf937698ca9950335
Sean Leonard <sean.leonard@microsoft.com>
2023-01-29 22:08:20 -0800

    Updated test with comments.

--- src/Service.Tests/SqlTests/GraphQLQueryTests/GraphQLQueryTestBase.cs
+++ src/Service.Tests/SqlTests/GraphQLQueryTests/GraphQLQueryTestBase.cs
@@ -54,12 +54,20 @@ namespace Azure.DataApiBuilder.Service.Tests.SqlTests.GraphQLQueryTests
             SqlTestHelper.PerformTestEqualJsonStrings(expected, actual.GetProperty("items").ToString());
         }
 
+        /// <summary>
+        /// Tests that the following "Find Many" query is properly handled by the engine given that it references
+        /// mapped column names "column1" and "column2" and NOT "__column1" nor "__column2"
+        /// and that the two mapped names are present in the result set.
+        /// </summary>
+        /// <param name="dbQuery"></param>
         [TestMethod]
         public async Task MultipleResultQueryWithMappings(string dbQuery)
         {
             string graphQLQueryName = "gQLmappings";
+
+            // "4" references the number of records in the GQLmappings table.
             string graphQLQuery = @"{
-                gQLmappings(first: 3) {
+                gQLmappings(first: 4) {
                     items {
                         column1
                         column2

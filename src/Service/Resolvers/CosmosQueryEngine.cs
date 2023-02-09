c7c5bac4022a657343b7f56a6c65487df195b1ea
Ayush Agarwal <agarwalayush@microsoft.com>
2023-02-09 23:07:28 +0530

    attempting to fix cosmos tests

--- src/Service/Resolvers/CosmosQueryEngine.cs
+++ src/Service/Resolvers/CosmosQueryEngine.cs
@@ -73,7 +73,7 @@ namespace Azure.DataApiBuilder.Service.Resolvers
 
             foreach (KeyValuePair<string, object> parameterEntry in structure.Parameters)
             {
-                querySpec = querySpec.WithParameter("@" + parameterEntry.Key, parameterEntry.Value);
+                querySpec = querySpec.WithParameter(parameterEntry.Key, parameterEntry.Value);
             }
 
             if (!string.IsNullOrEmpty(partitionKeyValue))

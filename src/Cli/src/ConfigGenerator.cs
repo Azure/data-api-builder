ab6af93897144fb3ac0b4c12a69425b80c9dcbd1
Ayush Agarwal <agarwalayush@microsoft.com>
2023-02-23 06:16:30 +0530

    good to go

--- src/Cli/src/ConfigGenerator.cs
+++ src/Cli/src/ConfigGenerator.cs
@@ -84,7 +84,7 @@ namespace Cli
 
                     // If the option --rest.path is specified for cosmosdb_nosql, log a warning because
                     // rest is not supported for cosmosdb_nosql yet.
-                    if (!restPath.Equals(GlobalSettings.REST_DEFAULT_PATH))
+                    if (!GlobalSettings.REST_DEFAULT_PATH.Equals(restPath))
                     {
                         _logger.LogWarning("Configuration option --rest.path is not honored for cosmosdb_nosql since " +
                             "it does not support REST yet.");

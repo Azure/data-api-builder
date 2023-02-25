530e4ff8a500d2101557befb777729d60b3107c5
Abhishek Kumar <abhishekkuma@microsoft.com>
2023-02-25 21:45:57 +0530

    fix formatting

--- src/Service/Services/PathRewriteMiddleware.cs
+++ src/Service/Services/PathRewriteMiddleware.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using Azure.DataApiBuilder.Config;
 using Azure.DataApiBuilder.Service.Configurations;

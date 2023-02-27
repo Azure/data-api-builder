acfcd3a94c7d343a059465830e229366b1769b73
Sean Leonard <sean.leonard@microsoft.com>
2023-02-27 13:31:07 -0800

    Add Powershell script for build pipeline to concatenate license to NOTICE file. Also adds build task to execute the script.

---
+++ NoticeGeneration.ps1
@@ -0,0 +1,18 @@
+# Copyright (c) Microsoft Corporation.
+# Licensed under the MIT License.
+
+param (
+    [Parameter (Mandatory=$true)][string] $BuildArtifactStagingDir,
+    [Parameter (Mandatory=$true)][string] $BuildSourcesDir
+)
+
+# Download and save the ChilliCream License 1.0 from ChilliCream/graphql-platform GitHub repo
+$chiliCreamLicenseSavePath = "$BuildArtifactStagingDir/chillicreamlicense.txt"
+$chiliCreamLicenseMetadataURL = "https://raw.githubusercontent.com/ChilliCream/graphql-platform/main/website/src/basic/licensing/chillicream-license.md"
+Invoke-WebRequest $chiliCreamLicenseMetadataURL `
+| Select-Object -ExpandProperty Content `
+| Out-File $chiliCreamLicenseSavePath
+
+# Concatenate existing NOTICE.txt file with Chilicream license.
+$noticeFilePath = $BuildSourcesDir
+Add-Content $noticeFilePath -Value (Get-Content $chiliCreamLicenseSavePath)

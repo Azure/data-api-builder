9e46b641c2b148ecf41b4a703740c382223e0abe
Sean Leonard <sean.leonard@microsoft.com>
2023-02-27 22:16:09 -0800

    add copyright per BCP webpage.

--- scripts/notice-generation.ps1
+++ scripts/notice-generation.ps1
@@ -16,4 +16,5 @@ Invoke-WebRequest $chiliCreamLicenseMetadataURL `
 # Concatenate existing NOTICE.txt file with Chilicream license.
 $noticeFilePath = "$BuildSourcesDir/NOTICE.txt"
 Add-Content $noticeFilePath -Value "Banana Cake Pop"
+Add-Content $noticeFilePath -Value "©2023 ChilliCream, Inc."
 Add-Content $noticeFilePath -Value (Get-Content $chiliCreamLicenseSavePath)

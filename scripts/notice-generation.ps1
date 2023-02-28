5f5f183ff5a1c486b6fb87aa596cc91fab7aca41
Sean Leonard <sean.leonard@microsoft.com>
2023-02-27 22:07:19 -0800

    add Component Name to the concatenated license.

--- scripts/notice-generation.ps1
+++ scripts/notice-generation.ps1
@@ -15,4 +15,5 @@ Invoke-WebRequest $chiliCreamLicenseMetadataURL `
 
 # Concatenate existing NOTICE.txt file with Chilicream license.
 $noticeFilePath = "$BuildSourcesDir/NOTICE.txt"
+Add-Content $noticeFilePath -Value "Banana Cake Pop"
 Add-Content $noticeFilePath -Value (Get-Content $chiliCreamLicenseSavePath)

65baec95bfe2e41dc5af6d48e0e947eea3bb9f4f
aaron burtle <aaronburtle@microsoft.com>
2023-03-01 11:23:34 -0800

    remove unneeded using dir

--- src/Service/Services/ResolverMiddleware.cs
+++ src/Service/Services/ResolverMiddleware.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Azure.DataApiBuilder.Service.Authorization;
 using Azure.DataApiBuilder.Service.GraphQLBuilder.CustomScalars;

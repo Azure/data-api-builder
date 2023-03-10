96cbe5865fd523d93c5d35c94360a5ea532665da
Ayush Agarwal <34566234+ayush3797@users.noreply.github.com>
2023-03-09 22:00:54 -0800

    Centralizing database policy addition (#1273)
    * Centralising db policy addition
    
    * Fixing tests
    
    * adding comment for clarity
    
    * updating comment
    
    * addressing comments
    
    * removing redundant change
    
    * removing redundant change
    
    * Passing http context to sqlexistsstructure
    
    * moving operationType one step back
    
    ---------
    
    Co-authored-by: Ayush Agarwal <agarwalayush@microsoft.com>
    Co-authored-by: abhishekkumams <102276754+abhishekkumams@users.noreply.github.com>

--- src/Service/Resolvers/Sql Query Structures/SqlUpdateQueryStructure.cs
+++ src/Service/Resolvers/Sql Query Structures/SqlUpdateQueryStructure.cs
@@ -11,6 +11,7 @@ using Azure.DataApiBuilder.Service.GraphQLBuilder.Mutations;
 using Azure.DataApiBuilder.Service.Models;
 using Azure.DataApiBuilder.Service.Services;
 using HotChocolate.Resolvers;
+using Microsoft.AspNetCore.Http;
 
 namespace Azure.DataApiBuilder.Service.Resolvers
 {
@@ -35,8 +36,15 @@ namespace Azure.DataApiBuilder.Service.Resolvers
             IAuthorizationResolver authorizationResolver,
             GQLFilterParser gQLFilterParser,
             IDictionary<string, object?> mutationParams,
+            HttpContext httpContext,
             bool isIncrementalUpdate)
-        : base(sqlMetadataProvider, authorizationResolver, gQLFilterParser, entityName: entityName)
+        : base(
+              metadataProvider: sqlMetadataProvider,
+              authorizationResolver: authorizationResolver,
+              gQLFilterParser: gQLFilterParser,
+              entityName: entityName,
+              httpContext: httpContext,
+              operationType: Config.Operation.Update)
         {
             UpdateOperations = new();
             OutputColumns = GenerateOutputColumns();
@@ -87,8 +95,15 @@ namespace Azure.DataApiBuilder.Service.Resolvers
             ISqlMetadataProvider sqlMetadataProvider,
             IAuthorizationResolver authorizationResolver,
             GQLFilterParser gQLFilterParser,
-            IDictionary<string, object?> mutationParams)
-            : base(sqlMetadataProvider, authorizationResolver, gQLFilterParser, entityName: entityName)
+            IDictionary<string, object?> mutationParams,
+            HttpContext httpContext)
+            : base(
+                  metadataProvider: sqlMetadataProvider,
+                  authorizationResolver: authorizationResolver,
+                  gQLFilterParser: gQLFilterParser,
+                  entityName: entityName,
+                  httpContext: httpContext,
+                  operationType: Config.Operation.Update)
         {
             UpdateOperations = new();
             SourceDefinition sourceDefinition = GetUnderlyingSourceDefinition();

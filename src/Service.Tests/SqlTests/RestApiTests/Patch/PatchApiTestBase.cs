d645d1866828eb610acf905d1d8c0abcfdae1547
Ayush Agarwal <ayusha083@gmail.com>
2023-03-23 11:06:14 +0530

    removing unnecessary import

--- src/Service.Tests/SqlTests/RestApiTests/Patch/PatchApiTestBase.cs
+++ src/Service.Tests/SqlTests/RestApiTests/Patch/PatchApiTestBase.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Net;
 using System.Threading.Tasks;
 using Azure.DataApiBuilder.Service.Exceptions;

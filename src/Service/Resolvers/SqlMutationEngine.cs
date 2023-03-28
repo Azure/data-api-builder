dbb76570c7f3d2a7728c2e420e303f12ece138eb
Ayush Agarwal <ayusha083@gmail.com>
2023-03-29 01:46:42 +0530

    updating known issues

--- src/Service/Resolvers/SqlMutationEngine.cs
+++ src/Service/Resolvers/SqlMutationEngine.cs
@@ -537,6 +537,7 @@ namespace Azure.DataApiBuilder.Service.Resolvers
                     (dbResultSet.Rows.FirstOrDefault() ?? new DbResultSetRow()) : null;
                 if (dbResultSetRow is not null && dbResultSetRow.Columns.Count == 0)
                 {
+                    // For GraphQL, insert operation corresponds to Create action.
                     if (operationType is Config.Operation.Create)
                     {
                         throw new DataApiBuilderException(

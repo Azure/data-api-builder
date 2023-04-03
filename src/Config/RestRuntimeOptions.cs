a7baa6acca747eb07214917667f96d0342c4c63a
Aaron Powell <me@aaron-powell.com>
2023-04-03 14:21:24 +1000

    Adding readable static for default REST path prefix

--- src/Config/RestRuntimeOptions.cs
+++ src/Config/RestRuntimeOptions.cs
@@ -1,3 +1,6 @@
 namespace Azure.DataApiBuilder.Config;
 
-public record RestRuntimeOptions(bool Enabled = true, string? Path = "/api");
+public record RestRuntimeOptions(bool Enabled = true, string Path = RestRuntimeOptions.DEFAULT_REST_PATH)
+{
+    public const string DEFAULT_REST_PATH = "/api";
+};

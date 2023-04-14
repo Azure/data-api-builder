55a78e16c315dbe4a560084b016464e2e5d35035
Sean Leonard <sean.leonard@microsoft.com>
2023-04-14 14:59:47 -0700

    Automate Component Schema creation for Full Entity (with primary key).

--- src/Service/Services/OpenApiDocumentor.cs
+++ src/Service/Services/OpenApiDocumentor.cs
@@ -1,10 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
 using Azure.DataApiBuilder.Auth;
 using Azure.DataApiBuilder.Config;
 using Azure.DataApiBuilder.Service.Configurations;
@@ -139,55 +143,99 @@ namespace Azure.DataApiBuilder.Service.Services
                         }
                     }
                 },
-                Components = new OpenApiComponents()
-                {
-                    Schemas = new Dictionary<string, OpenApiSchema>()
-                    {
-                        {
-                            "BookResponse",
-                            new OpenApiSchema()
-                            {
-                                Type = "object",
-                                Properties = new Dictionary<string, OpenApiSchema>()
-                                {
-                                    {
-                                        "id",
-                                        new OpenApiSchema()
-                                        {
-                                            Type = "integer",
-                                            Format = "int32"
-                                        }
-                                    },
-                                    {
-                                        "title",
-                                        new OpenApiSchema()
-                                        {
-                                            Type = "string"
-                                        }
-                                    },
-                                    {
-                                        "publisher_id",
-                                        new OpenApiSchema()
-                                        {
-                                            Type = "integer",
-                                            Format = "int32"
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-
-                }
+                Components = BuildComponents()
             };
             _openApiDocument = doc;
         }
 
-        public Dictionary<string, OpenApiSchema> BuildComponents()
+        public OpenApiComponents BuildComponents()
         {
-            Dictionary<string, OpenApiSchema> components = new();
-            Dictionary<string, DatabaseObject> entityMetadata = _metadataProvider.EntityToDatabaseObject;
-            return components;
+            return new OpenApiComponents()
+            {
+                Schemas = BuildComponentSchemas()
+            };
+        }
+
+        public Dictionary<string, OpenApiSchema> BuildComponentSchemas()
+        {
+            Dictionary<string, OpenApiSchema> schemas = new();
+
+            foreach (string entityName in _metadataProvider.EntityToDatabaseObject.Keys.ToList())
+            {
+                SourceDefinition? sourceDefinition = _metadataProvider.GetSourceDefinition(entityName);
+                List<string> columns = sourceDefinition is null ? new List<string>() : sourceDefinition.Columns.Keys.ToList();
+
+                // create component for FULL entity with PK.
+                schemas.Add(entityName, CreateComponentSchema(entityName, fields: columns));
+
+                // create component for entity with no PK
+                //schea
+            }
+
+            return schemas;
+        }
+
+        /// <summary>
+        /// Input needs entity fields and field data type metadata
+        /// Should this have conditional for creating component with PK field? or should that be handled before
+        /// and only pass in a field list here to generalize?
+        /// ex
+        /// </summary>
+        /// <param name="entityName">Name of the entity.</param>
+        /// <param name="fields">List of mapped (alias) field names.</param>
+        /// <returns></returns>
+        public OpenApiSchema CreateComponentSchema(string entityName, List<string> fields)
+        {
+            if (!_metadataProvider.EntityToDatabaseObject.TryGetValue(entityName, out DatabaseObject? dbObject) || dbObject is null)
+            {
+                throw new DataApiBuilderException(message: "oops bad entity", statusCode: System.Net.HttpStatusCode.InternalServerError, subStatusCode: DataApiBuilderException.SubStatusCodes.OpenApiDocumentAlreadyExists);
+            }
+
+            Dictionary<string, OpenApiSchema> properties = new();
+
+            foreach (string field in fields)
+            {
+                if(_metadataProvider.TryGetBackingColumn(entityName, field, out string? backingColumnValue) && !string.IsNullOrEmpty(backingColumnValue))
+                {
+                    properties.Add(field, new OpenApiSchema()
+                    {
+                        Type = $"JSON DATA TYPE for {backingColumnValue}",
+                        Format = $"OAS DATA TYPE for {backingColumnValue}"
+                    });
+                }
+            }
+
+            OpenApiSchema schema = new()
+            {
+                Type = "object",
+                Properties = properties
+            };
+
+            return schema;
+        }
+
+        public JsonValueKind SystemTypeToJsonValueKind(Type type)
+        {
+            return type.Name switch
+            {
+                "String" => JsonValueKind.String,
+                "Guid" => JsonValueKind.String,
+                "Byte" => JsonValueKind.String,
+                "Int16" => JsonValueKind.Number,
+                "Int32" => JsonValueKind.Number,
+                "Int64" => JsonValueKind.Number,
+                "Single" => JsonValueKind.Number,
+                "Double" => JsonValueKind.Number,
+                "Decimal" => JsonValueKind.Number,
+                "Boolean" => JsonValueKind.True,
+                "DateTime" => JsonValueKind.String,
+                "DateTimeOffset" => JsonValueKind.String,
+                "Byte[]" => JsonValueKind.String,
+                _ => throw new DataApiBuilderException(
+                        message: $"Column type {type} not handled by case. Please add a case resolving {type} to the appropriate GraphQL type",
+                        statusCode: HttpStatusCode.InternalServerError,
+                        subStatusCode: DataApiBuilderException.SubStatusCodes.GraphQLMapping)
+            };
         }
 
         /// <summary>

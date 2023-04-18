741727ba0496ba63c3eb5a80edd46865f8b8b0d4
Mathieu Tremblay <mathieu.tremblay1994@gmail.com>
2023-04-18 11:27:14 -0400

    Add v2 config controller

--- src/Service/Controllers/ConfigurationController.cs
+++ src/Service/Controllers/ConfigurationController.cs
@@ -2,7 +2,10 @@
 // Licensed under the MIT License.
 
 using System;
+using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
+using Azure.DataApiBuilder.Config;
 using Azure.DataApiBuilder.Service.Configurations;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -23,13 +26,55 @@ namespace Azure.DataApiBuilder.Service.Controllers
         }
 
         /// <summary>
-        /// Takes in the runtime configuration, schema, connection string and optionally the
-        /// resolvers and configures the runtime. If the runtime is already configured, it will
-        /// return a conflict result.
+        /// Takes in the runtime configuration and the configuration overrides and configures the runtime.
+        /// If the runtime is already configured, it will return a conflict result.
         /// </summary>
         /// <param name="configuration">Runtime configuration, schema, resolvers and connection string.</param>
-        /// <returns>Ok in case of success or Conflict with the key:value.</returns>
-        [HttpPost]
+        /// <returns>Ok in case of success, Bad request on bad config
+        /// or Conflict if the runtime is already configured </returns>
+        [HttpPost("v2")]
+        public async Task<ActionResult> Index([FromBody] ConfigurationPostParametersV2 configuration)
+        {
+            if (_configurationProvider.TryGetRuntimeConfiguration(out _))
+            {
+                return new ConflictResult();
+            }
+
+            try
+            {
+                // TODO: Once the configuration merging logic is implemented, merge the 2
+                // configuration files instead of only taking the connection string from
+                // the configuration overrides.
+                RuntimeConfig? configurationOverrides = JsonSerializer.Deserialize<RuntimeConfig>(configuration.ConfigurationOverrides);
+                if (configurationOverrides == null)
+                {
+                    _logger.LogError($"Failed to read ConfigurationOverrides.");
+                    return BadRequest();
+                }
+
+                bool initResult = await _configurationProvider.Initialize(
+                    configuration.Configuration,
+                    configuration.Schema,
+                    configurationOverrides.ConnectionString,
+                    configuration.AccessToken);
+
+                if (initResult && _configurationProvider.TryGetRuntimeConfiguration(out _))
+                {
+                    return Ok();
+                }
+                else
+                {
+                    _logger.LogError($"Failed to initialize configuration.");
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError($"Exception during configuration initialization. {e}");
+            }
+
+            return BadRequest();
+        }
+
         public async Task<ActionResult> Index([FromBody] ConfigurationPostParameters configuration)
         {
             if (_configurationProvider.TryGetRuntimeConfiguration(out _))
@@ -62,7 +107,6 @@ namespace Azure.DataApiBuilder.Service.Controllers
             return BadRequest();
         }
     }
-
     /// <summary>
     /// The required parameters required to configure the runtime.
     /// </summary>
@@ -75,7 +119,20 @@ namespace Azure.DataApiBuilder.Service.Controllers
         string Configuration,
         string? Schema,
         string ConnectionString,
-        string? AccessToken,
-        string? Database = null)
+        string? AccessToken)
+    { }
+
+    /// <summary>
+    /// The required parameters required to configure the runtime.
+    /// </summary>
+    /// <param name="Configuration">The runtime configuration.</param>
+    /// <param name="ConfigurationOverrides">Configuration parameters that override the options from the Configuration file.</param>
+    /// <param name="Schema">The GraphQL schema. Can be left empty for SQL databases.</param>
+    /// <param name="AccessToken">The managed identity access token (if any) used to connect to the database.</param>
+    public record class ConfigurationPostParametersV2(
+        string Configuration,
+        string ConfigurationOverrides,
+        string? Schema,
+        string? AccessToken)
     { }
 }

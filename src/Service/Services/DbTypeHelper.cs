fa7d67e18f9f107ecc727f8a5ce6f795a6e8c30b
Ayush Agarwal <ayusha083@gmail.com>
2023-04-19 11:31:37 +0530

    Adding helper class for type resolution

---
+++ src/Service/Services/DbTypeHelper.cs
@@ -0,0 +1,65 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Data;
+using System;
+
+namespace Azure.DataApiBuilder.Service.Services
+{
+    /// <summary>
+    /// Helper class used to resolve the underlying DbType for the parameter for its given SystemType. 
+    /// </summary>
+    public static class DbTypeHelper
+    {
+        private static Dictionary<Type, DbType> _systemTypeToDbTypeMap = new()
+        {
+            [typeof(byte)] = DbType.Byte,
+            [typeof(sbyte)] = DbType.SByte,
+            [typeof(short)] = DbType.Int16,
+            [typeof(ushort)] = DbType.UInt16,
+            [typeof(int)] = DbType.Int32,
+            [typeof(uint)] = DbType.UInt32,
+            [typeof(long)] = DbType.Int64,
+            [typeof(ulong)] = DbType.UInt64,
+            [typeof(float)] = DbType.Single,
+            [typeof(double)] = DbType.Double,
+            [typeof(decimal)] = DbType.Decimal,
+            [typeof(bool)] = DbType.Boolean,
+            [typeof(string)] = DbType.String,
+            [typeof(char)] = DbType.StringFixedLength,
+            [typeof(Guid)] = DbType.Guid,
+            [typeof(byte[])] = DbType.Binary,
+            [typeof(byte?)] = DbType.Byte,
+            [typeof(sbyte?)] = DbType.SByte,
+            [typeof(short?)] = DbType.Int16,
+            [typeof(ushort?)] = DbType.UInt16,
+            [typeof(int?)] = DbType.Int32,
+            [typeof(uint?)] = DbType.UInt32,
+            [typeof(long?)] = DbType.Int64,
+            [typeof(ulong?)] = DbType.UInt64,
+            [typeof(float?)] = DbType.Single,
+            [typeof(double?)] = DbType.Double,
+            [typeof(decimal?)] = DbType.Decimal,
+            [typeof(bool?)] = DbType.Boolean,
+            [typeof(char?)] = DbType.StringFixedLength,
+            [typeof(Guid?)] = DbType.Guid,
+            [typeof(object)] = DbType.Object
+        };
+
+        /// <summary>
+        /// Returns the DbType for given system type.
+        /// </summary>
+        /// <param name="systemType">The system type for which the DbType is to be determined.</param>
+        /// <returns>DbType for the given system tyep.</returns>
+        public static DbType? GetDbTypeFromSystemType(Type systemType)
+        {
+            if (!_systemTypeToDbTypeMap.TryGetValue(systemType, out DbType dbType))
+            {
+                return null;
+            }
+
+            return dbType;
+        }
+    }
+}

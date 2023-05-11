4da577ef4a96f940785f5b159b4b4b7005bc6cfe
Aaron Powell <me@aaron-powell.com>
2023-05-11 11:26:27 +1000

    really basic diff displayer to help debugging

--- src/Service.Tests/SnapshotExtensions.cs
+++ src/Service.Tests/SnapshotExtensions.cs
@@ -1,7 +1,13 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
 using Azure.DataApiBuilder.Config;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Snapshooter;
+using Snapshooter.Core.Serialization;
 using Snapshooter.MSTest;
 
 namespace Azure.DataApiBuilder.Service.Tests;
@@ -13,9 +19,50 @@ internal static class SnapshotExtensions
     /// in the output, such as the connection string.
     /// </summary>
     /// <param name="config"></param>
-    public static void MatchSnapshot(this RuntimeConfig config) =>
-        Snapshot.Match(
-            config,
-            options => options.ExcludeField("DataSource.ConnectionString").IgnoreField("DataSource.ConnectionString")
-        );
+    public static void MatchSnapshot(this RuntimeConfig config)
+    {
+        try
+        {
+            Snapshot.Match(
+                config,
+                options => options.ExcludeField("DataSource.ConnectionString").IgnoreField("DataSource.ConnectionString")
+            );
+        }
+        catch (AssertFailedException ex)
+        {
+            SnapshotFullName fullName = Snapshot.FullName();
+
+            string expected = File.ReadAllText(fullName.Filename);
+
+            SnapshotSerializer snapshotSerializer = new(new GlobalSnapshotSettingsResolver());
+            string actual = snapshotSerializer.SerializeObject(config);
+
+            string diff = BasicDiffDisplay(expected, actual);
+
+            throw new AssertFailedException($"Snapshot {fullName} did not match. Diff:{Environment.NewLine}{diff}", ex);
+        }
+    }
+
+    private static string BasicDiffDisplay(string expected, string actual)
+    {
+        string[] expectedLines = expected.Split(Environment.NewLine);
+        string[] actualLines = actual.Split(Environment.NewLine);
+
+        List<string> diff = new();
+
+        for(int i = 0; i < actualLines.Length; i++)
+        {
+            string line = "";
+            if (expectedLines[i] != actualLines[i])
+            {
+                line = "> ";
+            }
+
+            line += actualLines[i];
+
+            diff.Add(line);
+        }
+
+        return string.Join(Environment.NewLine, diff);
+    }
 }

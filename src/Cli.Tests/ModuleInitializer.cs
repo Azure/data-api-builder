e42c70061e97af62b9dbbb0fd795a9a96b356d3b
Aaron Powell <me@aaron-powell.com>
2023-05-17 10:18:14 +1000

    Moving snapshots into their own folder for easier PR reviews

--- src/Cli.Tests/ModuleInitializer.cs
+++ src/Cli.Tests/ModuleInitializer.cs
@@ -5,13 +5,25 @@ using System.Runtime.CompilerServices;
 
 namespace Cli.Tests;
 
+/// <summary>
+/// Setup global settings for the test project.
+/// </summary>
 static class ModuleInitializer
 {
+    /// <summary>
+    /// Initialize the Verifier settings we used for the project, such as what fields to ignore
+    /// when comparing objects and how we will name the snapshot files.
+    /// </summary>
     [ModuleInitializer]
     public static void Init()
     {
         VerifierSettings.IgnoreMember<DataSource>(dataSource => dataSource.ConnectionString);
         VerifierSettings.IgnoreMember<RuntimeConfig>(config => config.Schema);
+        VerifyBase.DerivePathInfo(
+            (sourceFile, projectDirectory, type, method) => new(
+                directory: Path.Combine(projectDirectory, "Snapshots"),
+                typeName: type.Name,
+                methodName: method.Name));
         VerifyDiffPlex.Initialize();
     }
 }
--- src/Service.Tests/ModuleInitializer.cs
+++ src/Service.Tests/ModuleInitializer.cs
@@ -1,8 +1,10 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.IO;
 using System.Runtime.CompilerServices;
 using Azure.DataApiBuilder.Config;
+using VerifyMSTest;
 using VerifyTests;
 
 namespace Azure.DataApiBuilder.Service.Tests;
@@ -14,6 +16,11 @@ static class ModuleInitializer
     {
         VerifierSettings.IgnoreMember<DataSource>(dataSource => dataSource.ConnectionString);
         VerifierSettings.IgnoreMember<RuntimeConfig>(config => config.Schema);
+        VerifyBase.DerivePathInfo(
+            (sourceFile, projectDirectory, type, method) => new(
+                directory: Path.Combine(projectDirectory, "Snapshots"),
+                typeName: type.Name,
+                methodName: method.Name));
         VerifyDiffPlex.Initialize();
     }
 }

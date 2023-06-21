f6d44838440ecb0b43b7d757906272aa5e52bfe6
Sean Leonard <sean.leonard@microsoft.com>
2023-06-21 15:25:10 -0700

    add DateTime and DateTimeOffset types to the system type to json type converter used in OpenAPI doc creation.

--- src/Core/Services/TypeHelper.cs
+++ src/Core/Services/TypeHelper.cs
@@ -96,7 +96,9 @@ namespace Azure.DataApiBuilder.Core.Services
             [typeof(bool?)] = JsonDataType.Boolean,
             [typeof(char?)] = JsonDataType.String,
             [typeof(Guid?)] = JsonDataType.String,
-            [typeof(object)] = JsonDataType.Object
+            [typeof(object)] = JsonDataType.Object,
+            [typeof(DateTime)] = JsonDataType.String,
+            [typeof(DateTimeOffset)] = JsonDataType.String
         };
 
         /// <summary>
@@ -112,6 +114,10 @@ namespace Azure.DataApiBuilder.Core.Services
             if (!_systemTypeToJsonDataTypeMap.TryGetValue(type, out JsonDataType openApiJsonTypeName))
             {
                 openApiJsonTypeName = JsonDataType.Undefined;
+            }
+            else
+            {
+                Console.Out.WriteLine("unknown type");
             }
 
             string formattedOpenApiTypeName = openApiJsonTypeName.ToString().ToLower();

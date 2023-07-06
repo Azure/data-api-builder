b636c967f34eae152ea64bfc78f9667886499c38
Aaron Powell <me@aaron-powell.com>
2023-07-06 11:27:41 +1000

    Excluding null values from the generated config (#1529)
    Enabling a setting on the serializer that will exclude `null` when it's
    serializing the runtime config file.
    
    ---------
    
    Co-authored-by: Sean Leonard <sean.leonard@microsoft.com>
    Co-authored-by: Aniruddh Munde <anmunde@microsoft.com>

--- src/Config/ObjectModel/EntityAction.cs
+++ src/Config/ObjectModel/EntityAction.cs
@@ -3,7 +3,7 @@
 
 namespace Azure.DataApiBuilder.Config.ObjectModel;
 
-public record EntityAction(EntityActionOperation Action, EntityActionFields? Fields, EntityActionPolicy Policy)
+public record EntityAction(EntityActionOperation Action, EntityActionFields? Fields, EntityActionPolicy? Policy)
 {
     public static readonly HashSet<EntityActionOperation> ValidPermissionOperations = new() { EntityActionOperation.Create, EntityActionOperation.Read, EntityActionOperation.Update, EntityActionOperation.Delete };
     public static readonly HashSet<EntityActionOperation> ValidStoredProcedurePermissionOperations = new() { EntityActionOperation.Execute };

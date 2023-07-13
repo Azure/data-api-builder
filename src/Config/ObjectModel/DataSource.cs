507df2e2b0a2c05b5a6f6e8201cd18fd2fc2b46e
Aniruddh Munde <anmunde@microsoft.com>
2023-07-13 16:52:14 -0700

    Handle possibility of options dictionary to be null

--- src/Config/ObjectModel/DataSource.cs
+++ src/Config/ObjectModel/DataSource.cs
@@ -12,40 +12,62 @@ namespace Azure.DataApiBuilder.Config.ObjectModel;
 /// </summary>
 /// <param name="DatabaseType">Type of database to use.</param>
 /// <param name="ConnectionString">Connection string to access the database.</param>
-/// <param name="Options">Custom options for the specific database. If there are no options this will be empty.</param>
-public record DataSource(DatabaseType DatabaseType, string ConnectionString, Dictionary<string, JsonElement> Options)
+/// <param name="Options">Custom options for the specific database. If there are no options, this could be null.</param>
+public record DataSource(DatabaseType DatabaseType, string ConnectionString, Dictionary<string, JsonElement>? Options)
 {
     /// <summary>
     /// Converts the <c>Options</c> dictionary into a typed options object.
+    /// May return null if the dictionary is null.
     /// </summary>
     /// <typeparam name="TOptionType">The strongly typed object for Options.</typeparam>
     /// <returns>The strongly typed representation of Options.</returns>
     /// <exception cref="NotSupportedException">Thrown when the provided <c>TOptionType</c> is not supported for parsing.</exception>
-    public TOptionType GetTypedOptions<TOptionType>() where TOptionType : IDataSourceOptions
+    public TOptionType? GetTypedOptions<TOptionType>() where TOptionType : IDataSourceOptions
     {
         HyphenatedNamingPolicy namingPolicy = new();
 
         if (typeof(TOptionType).IsAssignableFrom(typeof(CosmosDbNoSQLDataSourceOptions)))
         {
-            return (TOptionType)(object)new CosmosDbNoSQLDataSourceOptions(
-                    Database: ReadStringOption(namingPolicy.ConvertName(nameof(CosmosDbNoSQLDataSourceOptions.Database))),
-                    Container: ReadStringOption(namingPolicy.ConvertName(nameof(CosmosDbNoSQLDataSourceOptions.Container))),
-                    Schema: ReadStringOption(namingPolicy.ConvertName(nameof(CosmosDbNoSQLDataSourceOptions.Schema))),
-                    // The "raw" schema will be provided via the controller to setup config, rather than parsed from the JSON file.
-                    GraphQLSchema: ReadStringOption(namingPolicy.ConvertName(nameof(CosmosDbNoSQLDataSourceOptions.GraphQLSchema))));
+            return Options is not null ?
+                (TOptionType)(object)new CosmosDbNoSQLDataSourceOptions(
+                Database: ReadStringOption(namingPolicy.ConvertName(nameof(CosmosDbNoSQLDataSourceOptions.Database))),
+                Container: ReadStringOption(namingPolicy.ConvertName(nameof(CosmosDbNoSQLDataSourceOptions.Container))),
+                Schema: ReadStringOption(namingPolicy.ConvertName(nameof(CosmosDbNoSQLDataSourceOptions.Schema))),
+                // The "raw" schema will be provided via the controller to setup config, rather than parsed from the JSON file.
+                GraphQLSchema: ReadStringOption(namingPolicy.ConvertName(nameof(CosmosDbNoSQLDataSourceOptions.GraphQLSchema))))
+                : default;
         }
 
         if (typeof(TOptionType).IsAssignableFrom(typeof(MsSqlOptions)))
         {
-            return (TOptionType)(object)new MsSqlOptions(
-                SetSessionContext: ReadBoolOption(namingPolicy.ConvertName(nameof(MsSqlOptions.SetSessionContext))));
+            return Options is not null ?
+                (TOptionType)(object)new MsSqlOptions(
+                SetSessionContext: ReadBoolOption(namingPolicy.ConvertName(nameof(MsSqlOptions.SetSessionContext))))
+                : default;
         }
 
         throw new NotSupportedException($"The type {typeof(TOptionType).FullName} is not a supported strongly typed options object");
     }
 
-    private string? ReadStringOption(string option) => Options.ContainsKey(option) ? Options[option].GetString() : null;
-    private bool ReadBoolOption(string option) => Options.ContainsKey(option) ? Options[option].GetBoolean() : false;
+    private string? ReadStringOption(string option)
+    {
+        if (Options is not null && Options.TryGetValue(option, out JsonElement value))
+        {
+            return value.GetString();
+        }
+
+        return null;
+    }
+
+    private bool ReadBoolOption(string option)
+    {
+        if (Options is not null && Options.TryGetValue(option, out JsonElement value))
+        {
+            return value.GetBoolean();
+        }
+
+        return false;
+    }
 
     [JsonIgnore]
     public string DatabaseTypeNotSupportedMessage => $"The provided database-type value: {DatabaseType} is currently not supported. Please check the configuration file.";

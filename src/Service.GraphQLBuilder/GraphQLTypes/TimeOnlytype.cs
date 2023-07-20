9e1a93c521a69e2bbc8de8d9ba1a244719290096
Abhishek Kumar <abhishekkuma@microsoft.com>
2023-07-20 11:17:12 +0530

    fix formatting

---
+++ src/Service.GraphQLBuilder/GraphQLTypes/TimeOnlytype.cs
@@ -0,0 +1,67 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using HotChocolate.Language;
+using HotChocolate.Types;
+
+namespace Azure.DataApiBuilder.Service.GraphQLBuilder.GraphQLTypes;
+public class TimeOnlyType : ScalarType<TimeSpan>
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TimeOnlyType"/> class.
+    /// </summary>
+    public TimeOnlyType() : base("TimeOnly")
+    { }
+
+    /// <inheritdoc/>
+    public override bool IsInstanceOfType(IValueNode valueSyntax)
+    {
+        return valueSyntax is StringValueNode;
+    }
+
+    /// <inheritdoc/>
+    public override object ParseLiteral(IValueNode literal)
+    {
+        if (literal is StringValueNode stringValueNode)
+        {
+            if (TimeSpan.TryParse(stringValueNode.Value, out TimeSpan timeSpan))
+            {
+                return timeSpan;
+            }
+        }
+
+        throw new ArgumentException("Invalid TimeOnly value.");
+    }
+
+    /// <inheritdoc/>
+    public override IValueNode ParseResult(object? resultValue)
+    {
+        if (resultValue is TimeOnly timeSpan)
+        {
+            return new StringValueNode(timeSpan.ToString(@"hh\:mm\:ss\.fff"));
+        }
+
+        throw new ArgumentException("Invalid TimeOnly value.");
+    }
+
+    /// <inheritdoc/>
+    public override IValueNode ParseValue(object? value)
+    {
+        if (value is TimeOnly timeSpan)
+        {
+            return new StringValueNode(timeSpan.ToString(@"hh\:mm\:ss\.fff"));
+        }
+
+        throw new ArgumentException("Invalid TimeOnly value.");
+    }
+    /// <inheritdoc/>
+    public override object Serialize(object? value)
+    {
+        if (value is TimeSpan timeSpan)
+        {
+            return timeSpan.ToString(@"hh\:mm\:ss\.fff");
+        }
+
+        throw new ArgumentException("Invalid TimeOnly value.");
+    }
+}

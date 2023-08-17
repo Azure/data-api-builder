399b21a8d5ddd2328cabd9cc4c25dcc92d875535
Abhishek Kumar <abhishekkuma@microsoft.com>
2023-08-17 14:40:21 +0530

    adding telemetry initializer

---
+++ src/Service/Telemetry/MyTelemetryInitializer.cs
@@ -0,0 +1,29 @@
+using System;
+using Azure.DataApiBuilder.Service;
+using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using System.Collections.Generic;
+
+public class MyTelemetryInitializer : ITelemetryInitializer
+{
+    public static readonly IReadOnlyDictionary<string, string> GlobalProperties = new Dictionary<string, string>
+    {
+        { "ApplicationName", $"dab_oss_{ProductInfo.GetProductVersion()}" },
+        // Add more custom properties here
+    };
+
+    public void Initialize(ITelemetry telemetry)
+    {
+        telemetry.Context.Cloud.RoleName = "DataApiBuilder";
+        telemetry.Context.Session.Id = Guid.NewGuid().ToString();
+        telemetry.Context.Component.Version = ProductInfo.GetProductVersion();
+        telemetry.Context.Device.OperatingSystem = Environment.OSVersion.ToString();
+        telemetry.Context.Device.Type = "Container";
+        telemetry.Context.User.Id = $"{ Environment.MachineName }_{ Environment.UserName }";
+        
+        foreach (KeyValuePair<string, string> property in GlobalProperties)
+        {
+            telemetry.Context.GlobalProperties.Add(property.Key, property.Value);
+        }
+    }
+}

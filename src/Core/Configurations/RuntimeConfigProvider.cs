c6ae4a658cfb020437a8d02e57a9a83d94dfbade
Rohan Khanna <rohkhann@microsoft.com>
2023-08-18 11:44:32 -0700

    Bug fix in initialize for non-cosmos db's

--- src/Core/Configurations/RuntimeConfigProvider.cs
+++ src/Core/Configurations/RuntimeConfigProvider.cs
@@ -188,6 +188,7 @@ public class RuntimeConfigProvider
                 _ => runtimeConfig with { DataSource = runtimeConfig.DataSource with { ConnectionString = connectionString } }
             };
             ManagedIdentityAccessToken[_runtimeConfig.DefaultDBName] = accessToken;
+            _runtimeConfig.DatasourceNameToDataSource[_runtimeConfig.DefaultDBName] = runtimeConfig.DataSource;
 
             return await InvokeConfigLoadedHandlersAsync();
         }

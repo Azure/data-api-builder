0c67030db6ff916c4ae47a8a21467103bfa79970
Aniruddh Munde <anmunde@microsoft.com>
2023-08-20 14:33:07 -0700

    GetApplication Name only when replaceEnvVar=true

--- src/Config/RuntimeConfigLoader.cs
+++ src/Config/RuntimeConfigLoader.cs
@@ -74,7 +74,9 @@ public abstract class RuntimeConfigLoader
             }
 
             // Add Application Name for telemetry for MsSQL
-            if (config.DataSource.DatabaseType is DatabaseType.MSSQL)
+            // Do this only when environment variables have been replaced since it parsing
+            // the connection string will result in an exception.
+            if (config.DataSource.DatabaseType is DatabaseType.MSSQL && replaceEnvVar)
             {
                 updatedConnectionString = GetConnectionStringWithApplicationName(updatedConnectionString);
             }

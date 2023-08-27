c7e44bb51685632ca5af1705f73a2461c3b17fe3
gerson <mottajrgerson@gmail.com>
2023-08-27 14:12:22 -0300

    Move to file-scoped namespaces: Service.Tests.OpenApiIntegration

--- src/Service.Tests/OpenApiDocumentor/OpenApiDocumentorConstants.cs
+++ src/Service.Tests/OpenApiDocumentor/OpenApiDocumentorConstants.cs
@@ -1,15 +1,14 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-namespace Azure.DataApiBuilder.Service.Tests.OpenApiIntegration
+namespace Azure.DataApiBuilder.Service.Tests.OpenApiIntegration;
+
+public class OpenApiDocumentorConstants
 {
-    public class OpenApiDocumentorConstants
-    {
-        public const string TOPLEVELPROPERTY_OPENAPI = "openapi";
-        public const string TOPLEVELPROPERTY_INFO = "info";
-        public const string TOPLEVELPROPERTY_SERVERS = "servers";
-        public const string TOPLEVELPROPERTY_PATHS = "paths";
-        public const string TOPLEVELPROPERTY_COMPONENTS = "components";
-        public const string PROPERTY_SCHEMAS = "schemas";
-    }
+    public const string TOPLEVELPROPERTY_OPENAPI = "openapi";
+    public const string TOPLEVELPROPERTY_INFO = "info";
+    public const string TOPLEVELPROPERTY_SERVERS = "servers";
+    public const string TOPLEVELPROPERTY_PATHS = "paths";
+    public const string TOPLEVELPROPERTY_COMPONENTS = "components";
+    public const string PROPERTY_SCHEMAS = "schemas";
 }

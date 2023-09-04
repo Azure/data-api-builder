eef0fd8ec095645a183a088b1eab39f864f28595
Ayush Agarwal <ayusha083@gmail.com>
2023-09-04 21:39:27 +0530

    adding comment

--- src/Config/ObjectModel/RestRuntimeOptions.cs
+++ src/Config/ObjectModel/RestRuntimeOptions.cs
@@ -9,7 +9,9 @@ namespace Azure.DataApiBuilder.Config.ObjectModel;
 /// <param name="Enabled">If the REST APIs are enabled.</param>
 /// <param name="Path">The URL prefix path at which endpoints
 /// for all entities will be exposed.</param>
-/// <param name="RequestBodyStrict">Boolean property indicating whether extraneous fields are allowed in request body.</param>
+/// <param name="RequestBodyStrict">Boolean property indicating whether extraneous fields are allowed in request body.
+/// The default value is true - meaning we don't allow extraneous fields by default in the rest request body.
+/// The default value should not be changed.</param>
 public record RestRuntimeOptions(bool Enabled = true, string Path = RestRuntimeOptions.DEFAULT_PATH, bool RequestBodyStrict = true)
 {
     public const string DEFAULT_PATH = "/api";

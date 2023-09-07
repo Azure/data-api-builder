Azure/data-api-builder

commit 2e8d1bb8b7d83bbe2a90472c2ccdc8acb87fa5ec

Overhaul of config system (#1402)
## Why make this change?

The original config system has a lot of areas where it doesn't provide
concrete types for what we need, due to the flexible nature of the JSON
file, and instead forces the code to check at runtime (is the property a
bool or an object?) and then have branching logic based on that.

This results in duplication of code, as well as hidden assumptions for
what the "expanded" version of shorthand properties (by shorthand
properties I mean properties like `"rest": false` on the `runtime`).

With this change we reduce the config C# type system to only handle the
fully expanded objects, resulting in a clearer structure for what the
types are between JSON and C#, reduce the amount of type casting that
needs to be done when checking the config, and unblocking at ability to
tackle more complex config features, such as #722.

## What is this change?

- Full rewrite of the config type system and "loader" process
- This includes a set of custom `JsonConverter` classes for parts of the
config serializer/deserializer, so that we can expand the config and
apply the defaults as expected. The result of this is that if you
deserialize then serialize the resulting config can look different as we
_always_ serialize to the expanded version
- Leveraging a file system abstraction for easier testing
- Unit tests can generate a mock `RuntimeConfig` and write it to a mock
file system, meaning they are a lot more isolated from each other
- Integration tests can load the "base" `RuntimeConfig` for the DB and
then write out a config (with test-specific features) to the mock file
system, which is then given to the test runner to use, rather than
writing to disk (although some tests will still write to disk)
- Refactored engine to use the new types
- Introduced [Snapshot
Testing](https://jestjs.io/docs/en/snapshot-testing/) via
[Snapshooter](https://github.com/SwissLife-OSS/snapshooter) to make it

src/Config/RuntimeConfigLoader.cs
@@ -1,0 +1,315 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Diagnostics.CodeAnalysis;
+using System.IO.Abstractions;
+using System.Net;
+using System.Reflection;
+using System.Text.Json;
+using Azure.DataApiBuilder.Config.Converters;
+using Azure.DataApiBuilder.Config.NamingPolicies;
+using Azure.DataApiBuilder.Config.ObjectModel;
+using Azure.DataApiBuilder.Service.Exceptions;
+using Microsoft.Extensions.Logging;
+
+namespace Azure.DataApiBuilder.Config;
+
+/// <summary>
+/// This class is responsible for loading the runtime config from either a JSON string
+/// or a file located on disk, depending on how the service is being run.
+/// </summary>
+/// <remarks>
+/// This class does not maintain any internal state of the loaded config, instead it will
+/// always generate a new config when it is requested.
+///
+/// To support better testability, the <see cref="IFileSystem"/> abstraction is provided
+/// which allows for mocking of the file system in tests, providing a way to run the test
+/// in isolation of other tests or the actual file system.
+/// </remarks>
+public class RuntimeConfigLoader
+{
+    private string _baseConfigFileName;
+
+    private readonly IFileSystem _fileSystem;
+    private readonly string? _connectionString;
+
+    public const string CONFIGFILE_NAME = "dab-config";
+    public const string CONFIG_EXTENSION = ".json";
+    public const string ENVIRONMENT_PREFIX = "DAB_";
+    public const string RUNTIME_ENVIRONMENT_VAR_NAME = $"{ENVIRONMENT_PREFIX}ENVIRONMENT";
+    public const string RUNTIME_ENV_CONNECTION_STRING = $"{ENVIRONMENT_PREFIX}CONNSTRING";
+    public const string ASP_NET_CORE_ENVIRONMENT_VAR_NAME = "ASPNETCORE_ENVIRONMENT";
+    public const string SCHEMA = "dab.draft.schema.json";
+
+    public string ConfigFileName => GetFileNameForEnvironment(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT"), false);
+
+    public RuntimeConfigLoader(IFileSystem fileSystem, string baseConfigFileName = DEFAULT_CONFIG_FILE_NAME, string? connectionString = null)
+    {
+        _fileSystem = fileSystem;
+        _baseConfigFileName = baseConfigFileName;
+        _connectionString = connectionString;
+    }
+
+    /// <summary>
+    /// Load the runtime config from the specified path.
+    /// </summary>
+    /// <param name="path">The path to the dab-config.json file.</param>
+    /// <param name="config">The loaded <c>RuntimeConfig</c>, or null if none was loaded.</param>
+    /// <returns>True if the config was loaded, otherwise false.</returns>
+    public bool TryLoadConfig(string path, [NotNullWhen(true)] out RuntimeConfig? config)
+    {
+        if (_fileSystem.File.Exists(path))
+        {
+            string json = _fileSystem.File.ReadAllText(path);
+            return TryParseConfig(json, out config, connectionString: _connectionString);
+        }
+
+        config = null;
+        return false;
+    }
+
+    /// <summary>
+    /// Parses a JSON string into a <c>RuntimeConfig</c> object
+    /// </summary>
+    /// <param name="json">JSON that represents the config file.</param>
+    /// <param name="config">The parsed config, or null if it parsed unsuccessfully.</param>
+    /// <returns>True if the config was parsed, otherwise false.</returns>
+    public static bool TryParseConfig(string json, [NotNullWhen(true)] out RuntimeConfig? config, ILogger? logger = null, string? connectionString = null)
+    {
+        JsonSerializerOptions options = GetSerializationOptions();
+
+        try
+        {
+            config = JsonSerializer.Deserialize<RuntimeConfig>(json, options);
+
+            if (config is null)
+            {
+                return false;
+            }
+
+            if (!string.IsNullOrEmpty(connectionString))
+            {
+                config = config with { DataSource = config.DataSource with { ConnectionString = connectionString } };
+            }
+        }
+        catch (JsonException ex)
+        {
+            string errorMessage = $"Deserialization of the configuration file failed.\n" +
+                        $"Message:\n {ex.Message}\n" +
+                        $"Stack Trace:\n {ex.StackTrace}";
+
+            if (logger is null)
+            {
+                // logger can be null when called from CLI
+                Console.Error.WriteLine(errorMessage);
+            }
+            else
+            {
+                logger.LogError(ex, errorMessage);
+            }
+
+            config = null;
+            return false;
+        }
+
+        return true;
+    }
+
+    public static JsonSerializerOptions GetSerializationOptions()
+    {
+        JsonSerializerOptions options = new()
+        {
+            PropertyNameCaseInsensitive = false,
+            PropertyNamingPolicy = new HyphenatedNamingPolicy(),
+            ReadCommentHandling = JsonCommentHandling.Skip,
+            WriteIndented = true,
+        };
+        options.Converters.Add(new EnumMemberJsonEnumConverterFactory());
+        options.Converters.Add(new RestRuntimeOptionsConverterFactory());
+        options.Converters.Add(new GraphQLRuntimeOptionsConverterFactory());
+        options.Converters.Add(new EntitySourceConverterFactory());
+        options.Converters.Add(new EntityActionConverterFactory());
+        options.Converters.Add(new StringJsonConverterFactory());
+        return options;
+    }
+
+    /// <summary>
+    /// Tries to load the config file using the filename known to the RuntimeConfigLoader and for the default environment.
+    /// </summary>
+    /// <param name="config">The loaded <c>RuntimeConfig</c>, or null if none was loaded.</param>
+    /// <returns>True if the config was loaded, otherwise false.</returns>
+    public bool TryLoadKnownConfig([NotNullWhen(true)] out RuntimeConfig? config)
+    {
+        return TryLoadConfig(ConfigFileName, out config);
+    }
+
+    /// <summary>
+    /// Precedence of environments is
+    /// 1) Value of DAB_ENVIRONMENT.
+    /// 2) Value of ASPNETCORE_ENVIRONMENT.
+    /// 3) Default config file name.
+    /// In each case, overridden file name takes precedence.
+    /// The first file name that exists in current directory is returned.
+    /// The fall back options are dab-config.overrides.json/dab-config.json
+    /// If no file exists, this will return an empty string.
+    /// </summary>
+    /// <param name="aspnetEnvironment">Value of ASPNETCORE_ENVIRONMENT variable</param>
+    /// <param name="considerOverrides">whether to look for overrides file or not.</param>
+    /// <returns></returns>
+    public string GetFileNameForEnvironment(string? aspnetEnvironment, bool considerOverrides)
+    {
+        string configFileNameWithExtension = string.Empty;
+        string?[] environmentPrecedence = new[]
+        {
+            Environment.GetEnvironmentVariable(RUNTIME_ENVIRONMENT_VAR_NAME),
+            aspnetEnvironment,
+            string.Empty
+        };
+
+        for (short index = 0;
+            index < environmentPrecedence.Length
+            && string.IsNullOrEmpty(configFileNameWithExtension);
+            index++)
+        {
+            if (!string.IsNullOrWhiteSpace(environmentPrecedence[index])
+                // The last index is for the default case - the last fallback option
+                // where environmentPrecedence[index] is string.Empty
+                // for that case, we still need to get the file name considering overrides
+                // so need to do an OR on the last index here
+                || index == environmentPrecedence.Length - 1)
+            {
+                configFileNameWithExtension = GetFileName(environmentPrecedence[index], considerOverrides);
+            }
+        }
+
+        return configFileNameWithExtension;
+    }
+
+    /// <summary>
+    /// Returns the default config file name.
+    /// </summary>
+    public const string DEFAULT_CONFIG_FILE_NAME = $"{CONFIGFILE_NAME}{CONFIG_EXTENSION}";
+
+    /// <summary>
+    /// Generates the config file name and a corresponding overridden file name,
+    /// With precedence given to overridden file name, returns that name
+    /// if the file exists in the current directory, else an empty string.
+    /// </summary>
+    /// <param name="environmentValue">Name of the environment to
+    /// generate the config file name for.</param>
+    /// <param name="considerOverrides">whether to look for overrides file or not.</param>
+    /// <returns></returns>
+    public string GetFileName(string? environmentValue, bool considerOverrides)
+    {
+        string fileNameWithoutExtension = _fileSystem.Path.GetFileNameWithoutExtension(_baseConfigFileName);
+        string fileExtension = _fileSystem.Path.GetExtension(_baseConfigFileName);
+        string configFileName =
+            !string.IsNullOrEmpty(environmentValue)
+            ? $"{fileNameWithoutExtension}.{environmentValue}"
+            : $"{fileNameWithoutExtension}";
+        string configFileNameWithExtension = $"{configFileName}{fileExtension}";
+        string overriddenConfigFileNameWithExtension = GetOverriddenName(configFileName);
+
+        if (considerOverrides && DoesFileExistInCurrentDirectory(overriddenConfigFileNameWithExtension))
+        {
+            return overriddenConfigFileNameWithExtension;
+        }
+
+        if (DoesFileExistInCurrentDirectory(configFileNameWithExtension))
+        {
+            return configFileNameWithExtension;
+        }
+
+        return string.Empty;
+    }
+
+    private static string GetOverriddenName(string fileName)
+    {
+        return $"{fileName}.overrides{CONFIG_EXTENSION}";
+    }
+
+    public bool DoesFileExistInCurrentDirectory(string fileName)
+    {
+        string currentDir = _fileSystem.Directory.GetCurrentDirectory();
+        // Unable to use ILogger because this code is invoked before LoggerFactory
+        // is instantiated.
+        if (_fileSystem.File.Exists(_fileSystem.Path.Combine(currentDir, fileName)))
+        {
+            // This config file is logged as being found, but may not actually be used!
+            Console.WriteLine($"Found config file: {fileName}.");
+            return true;
+        }
+        else
+        {
+            // Unable to use ILogger because this code is invoked before LoggerFactory
+            // is instantiated.
+            Console.WriteLine($"Unable to find config file: {fileName} does not exist.");
+            return false;
+        }
+    }
+
+    /// <summary>
+    /// This method reads the dab.draft.schema.json which contains the link for online published
+    /// schema for dab, based on the version of dab being used to generate the runtime config.
+    /// </summary>
+    public string GetPublishedDraftSchemaLink()
+    {
+        string? assemblyDirectory = _fileSystem.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        if (assemblyDirectory is null)
+        {
+            throw new DataApiBuilderException(
+                message: "Could not get the link for DAB draft schema.",
+                statusCode: HttpStatusCode.ServiceUnavailable,
+                subStatusCode: DataApiBuilderException.SubStatusCodes.ErrorInInitialization);
+        }
+
+        string? schemaPath = _fileSystem.Path.Combine(assemblyDirectory, "dab.draft.schema.json");
+        string schemaFileContent = _fileSystem.File.ReadAllText(schemaPath);
+        Dictionary<string, object>? jsonDictionary = JsonSerializer.Deserialize<Dictionary<string, object>>(schemaFileContent, GetSerializationOptions());
+
+        if (jsonDictionary is null)
+        {
+            throw new DataApiBuilderException(
+                message: "The schema file is misconfigured. Please check the file formatting.",
+                statusCode: HttpStatusCode.ServiceUnavailable,
+                subStatusCode: DataApiBuilderException.SubStatusCodes.ErrorInInitialization);
+        }
+
+        object? additionalProperties;
+        if (!jsonDictionary.TryGetValue("additionalProperties", out additionalProperties))
+        {
+            throw new DataApiBuilderException(
+                message: "The schema file doesn't have the required field : additionalProperties",
+                statusCode: HttpStatusCode.ServiceUnavailable,
+                subStatusCode: DataApiBuilderException.SubStatusCodes.ErrorInInitialization);
+        }
+
+        // properties cannot be null since the property additionalProperties exist in the schema file.
+        Dictionary<string, string> properties = JsonSerializer.Deserialize<Dictionary<string, string>>(additionalProperties.ToString()!)!;
+
+        if (!properties.TryGetValue("version", out string? versionNum))
+        {
+            throw new DataApiBuilderException(message: "Missing required property 'version' in additionalProperties section.",
+                statusCode: HttpStatusCode.ServiceUnavailable,
+                subStatusCode: DataApiBuilderException.SubStatusCodes.ErrorInInitialization);
+        }
+
+        return versionNum;
+    }
+
+    public static string GetMergedFileNameForEnvironment(string fileName, string environmentValue)
+    {
+        return $"{fileName}.{environmentValue}.merged{CONFIG_EXTENSION}";
+    }
+
+    /// <summary>
+    /// Allows the base config file name to be updated. This is commonly done when the CLI is starting up.
+    /// </summary>
+    /// <param name="fileName"></param>
+    public void UpdateBaseConfigFileName(string fileName)
+    {
+        _baseConfigFileName = fileName;
+    }
+}
+

src/Service/Utils.cs
@@ -1,26 +1,0 @@
-// Copyright (c) Microsoft Corporation.
-// Licensed under the MIT License.
-
-using System.Diagnostics;
-using System.Reflection;
-
-namespace Azure.DataApiBuilder.Service
-{
-    public class Utils
-    {
-        public const string DEFAULT_VERSION = "1.0.0";
-
-        /// <summary>
-        /// Reads the product version from the executing assembly's file version information.
-        /// </summary>
-        /// <returns>Product version if not null, default version 1.0.0 otherwise.</returns>
-        public static string GetProductVersion()
-        {
-            Assembly assembly = Assembly.GetExecutingAssembly();
-            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
-            string? version = fileVersionInfo.ProductVersion;
-
-            return version ?? DEFAULT_VERSION;
-        }
-    }
-}

commit 1b82fb8827035734a250c792e957173f500ae93b

Avoid escaping HTML sensitive characters when writing configuration to file (#1691)
## Why make this change?

- Closes #1687 

## What is this change?

- Set the Encoder for JsonSerializationOptions to be
[`JavaScriptEncoder.UnsafeRelaxedJsonEscaping`](https://learn.microsoft.com/en-us/dotnet/api/system.text.encodings.web.javascriptencoder.unsaferelaxedjsonescaping?view=net-7.0)
instead of the default encoder.
- Although [this
article](https://learn.microsoft.com/en-us/dotnet/standard/serialization/system-text-json/character-encoding#serialize-all-characters)
mentions use this encoder with caution, since we are sure the file
contents will be deserialized as UTF-8 JSON, we should be safe to use
this encoder.
- We were using this encoder previously from the fix in #1386, but with
#1402, that original fix was essentially not being used since #1402
introduced usage of the new `GetJsonSerializerOptions()` function in
`RuntimeConfigLoader`.

## How was this tested?

- [X] Unit Tests. `TestSpecialCharactersInConnectionString` is moved to
the `ConfigGeneratorTests` since keeping it as part of `InitTests` with
the Verify framework wasn't really testing the scenario since we ignore
connection strings in the snapshots.
- This regression from #1402 was not caught because this test scenario
was effectively being skipped.
- Note, this test is modified to explicitly compare the expected and
actual json string from file since any usage of
JsonSerializer/JObject.Parse would require using the same encoder to
deserialize making the test itself run similar code which it is testing.
Parsing into JSON objects would treat `\u0027` and `'` as equal whereas
string comparison wont. Hence, comparison using string is necessary to
catch regressions here.

## Sample Request(s)

BEFORE:
`dab init --host-mode development --database-type mssql
--connection-string "@env('my-connection-string')"`

creates the configuration file as follows:

```
{

src/Cli/Utils.cs
@@ -3,9 +3,7 @@
 
 using System.Diagnostics.CodeAnalysis;
 using System.IO.Abstractions;
-using System.Text.Encodings.Web;
 using System.Text.Json;
-using System.Text.Json.Serialization;
 using Azure.DataApiBuilder.Config;
 using Azure.DataApiBuilder.Config.Converters;
 using Azure.DataApiBuilder.Config.ObjectModel;
@@ -130,30 +128,6 @@ namespace Cli
             public override string ConvertName(string name) => name.ToLower();
 
             public static string ConvertName(Enum name) => name.ToString().ToLower();
-        }
-
-        /// <summary>
-        /// Returns the Serialization option used to convert objects into JSON.
-        /// Not escaping any special unicode characters.
-        /// Ignoring properties with null values.
-        /// Keeping all the keys in lowercase.
-        /// </summary>
-        public static JsonSerializerOptions GetSerializationOptions()
-        {
-            JsonSerializerOptions? options = new()
-            {
-                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
-                WriteIndented = true,
-                DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
-                PropertyNamingPolicy = new LowerCaseNamingPolicy(),
-                // As of .NET Core 7, JsonDocument and JsonSerializer only support skipping or disallowing
-                // of comments; they do not support loading them. If we set JsonCommentHandling.Allow for either,
-                // it will throw an exception.
-                ReadCommentHandling = JsonCommentHandling.Skip
-            };
-
-            options.Converters.Add(new JsonStringEnumConverter(namingPolicy: new LowerCaseNamingPolicy()));
-            return options;
         }
 
         /// <summary>

src/Config/RuntimeConfigLoader.cs
@@ -4,6 +4,7 @@
 using System.Diagnostics.CodeAnalysis;
 using System.Net;
 using System.Runtime.CompilerServices;
+using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using Azure.DataApiBuilder.Config.Converters;
@@ -155,7 +156,8 @@ public abstract class RuntimeConfigLoader
             PropertyNamingPolicy = new HyphenatedNamingPolicy(),
             ReadCommentHandling = JsonCommentHandling.Skip,
             WriteIndented = true,
-            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
         };
         options.Converters.Add(new EnumMemberJsonEnumConverterFactory());
         options.Converters.Add(new RestRuntimeOptionsConverterFactory());

src/Cli.Tests/ConfigGeneratorTests.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Text;
+
 namespace Cli.Tests;
 
 /// <summary>
@@ -117,6 +119,77 @@ public class ConfigGeneratorTests
                 Times.Once
             );
         }
+    }
+
+    /// <summary>
+    /// Test to verify creation of initial config with special characters
+    /// such as [!,@,#,$,%,^,&,*, ,(,)] in connection-string or graphql api
+    /// </summary>
+    [TestMethod]
+    public void TestSpecialCharactersInConnectionString()
+    {
+        HandleConfigFileCreationAndDeletion(TEST_RUNTIME_CONFIG_FILE, configFilePresent: false);
+        InitOptions options = new(
+            databaseType: DatabaseType.MSSQL,
+            connectionString: "A!string@with#some$special%characters^to&check*proper(serialization).'",
+            cosmosNoSqlDatabase: null,
+            cosmosNoSqlContainer: null,
+            graphQLSchemaPath: null,
+            graphQLPath: "/An_",
+            setSessionContext: false,
+            hostMode: HostMode.Production,
+            corsOrigin: null,
+            authenticationProvider: EasyAuthType.StaticWebApps.ToString(),
+            config: TEST_RUNTIME_CONFIG_FILE);
+
+        StringBuilder expectedRuntimeConfigJson = new(
+        @"{" +
+            @"""$schema"": """ + DAB_DRAFT_SCHEMA_TEST_PATH + @"""" + "," +
+            @"""data-source"": {
+                    ""database-type"": ""mssql"",
+                    ""connection-string"": ""A!string@with#some$special%characters^to&check*proper(serialization).'"",
+                    ""options"":{
+                        ""set-session-context"": false
+                    }
+                },
+           ""runtime"": {
+                ""rest"": {
+                  ""enabled"": true,
+                  ""path"": ""/api""
+                  },
+                ""graphql"": {
+                  ""enabled"": true,
+                  ""path"": ""/An_"",
+                  ""allow-introspection"": true
+                  },
+                ""host"": {
+                  ""cors"": {
+                    ""origins"": [],
+                    ""allow-credentials"": false
+                        },
+                  ""authentication"": {
+                    ""provider"": ""StaticWebApps""
+                        },
+                  ""mode"": ""production""
+                  }
+              },
+              ""entities"": {}
+            }");
+
+        expectedRuntimeConfigJson = expectedRuntimeConfigJson.Replace(" ", string.Empty);
+        expectedRuntimeConfigJson = expectedRuntimeConfigJson.Replace("\n", string.Empty);
+        expectedRuntimeConfigJson = expectedRuntimeConfigJson.Replace("\r\n", string.Empty);
+
+        Assert.IsTrue(TryGenerateConfig(options, _runtimeConfigLoader!, _fileSystem!));
+
+        StringBuilder actualRuntimeConfigJson = new(_fileSystem!.File.ReadAllText(TEST_RUNTIME_CONFIG_FILE, Encoding.Default));
+        actualRuntimeConfigJson = actualRuntimeConfigJson.Replace(" ", string.Empty);
+        actualRuntimeConfigJson = actualRuntimeConfigJson.Replace("\r\n", string.Empty);
+        actualRuntimeConfigJson = actualRuntimeConfigJson.Replace("\n", string.Empty);
+
+        // Comparing explicit strings here since parsing these into JSON would lose
+        // the test scenario of verifying escaped chars are not written to the file system.
+        Assert.AreEqual(expectedRuntimeConfigJson.ToString(), actualRuntimeConfigJson.ToString());
     }
 
     /// <summary>

src/Cli.Tests/InitTests.cs
@@ -224,28 +224,6 @@ namespace Cli.Tests
         }
 
         /// <summary>
-        /// Test to verify creation of initial config with special characters
-        /// such as [!,@,#,$,%,^,&,*, ,(,)] in connection-string.
-        /// </summary>
-        [TestMethod]
-        public Task TestSpecialCharactersInConnectionString()
-        {
-            InitOptions options = new(
-                databaseType: DatabaseType.MSSQL,
-                connectionString: "A!string@with#some$special%characters^to&check*proper(serialization)including space.",
-                cosmosNoSqlDatabase: null,
-                cosmosNoSqlContainer: null,
-                graphQLSchemaPath: null,
-                setSessionContext: false,
-                hostMode: HostMode.Production,
-                corsOrigin: null,
-                authenticationProvider: EasyAuthType.StaticWebApps.ToString(),
-                config: TEST_RUNTIME_CONFIG_FILE);
-
-            return ExecuteVerifyTest(options);
-        }
-
-        /// <summary>
         /// Test to verify that an error is thrown when user tries to
         /// initialize a config with a file name that already exists.
         /// </summary>
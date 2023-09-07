02768d142e5330a0e5072a3007798e9d9c53e7c3
Aniruddh Munde <anmunde@microsoft.com>
2023-09-06 23:16:52 -0700

    Remove unnecessary using

--- src/Config/RuntimeConfigLoader.cs
+++ src/Config/RuntimeConfigLoader.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License.
 
 using System.Diagnostics.CodeAnalysis;
-using System.Net;
-using System.Runtime.CompilerServices;
 using System.Text.Encodings.Web;
 using System.Text.Json;
 using System.Text.Json.Serialization;

11b41ee9897bc51046fba29d5257d1c9b07ea5e9
gerson <mottajrgerson@gmail.com>
2023-09-22 18:56:30 -0300

    updating so that it move to file scoped namesspace project Core part 1

--- src/Core/AuthenticationHelpers/StaticWebAppsAuthentication.cs
+++ src/Core/AuthenticationHelpers/StaticWebAppsAuthentication.cs
@@ -10,93 +10,92 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Primitives;
 
-namespace Azure.DataApiBuilder.Core.AuthenticationHelpers
+namespace Azure.DataApiBuilder.Core.AuthenticationHelpers;
+
+/// <summary>
+/// Helper class which parses EasyAuth's injected headers into a ClaimsIdentity object.
+/// This class provides helper methods for StaticWebApps' Authentication feature: EasyAuth.
+/// </summary>
+public class StaticWebAppsAuthentication
 {
+    public const string USER_ID_CLAIM = "userId";
+    public const string USER_DETAILS_CLAIM = "userDetails";
+
     /// <summary>
-    /// Helper class which parses EasyAuth's injected headers into a ClaimsIdentity object.
-    /// This class provides helper methods for StaticWebApps' Authentication feature: EasyAuth.
+    /// Link for reference of how StaticWebAppsClientPrincipal is defined
+    /// https://docs.microsoft.com/azure/static-web-apps/user-information?tabs=csharp#client-principal-data
     /// </summary>
-    public class StaticWebAppsAuthentication
+    public class StaticWebAppsClientPrincipal
     {
-        public const string USER_ID_CLAIM = "userId";
-        public const string USER_DETAILS_CLAIM = "userDetails";
+        public string? IdentityProvider { get; set; }
+        public string? UserId { get; set; }
+        public string? UserDetails { get; set; }
+        public IEnumerable<string>? UserRoles { get; set; }
+    }
 
-        /// <summary>
-        /// Link for reference of how StaticWebAppsClientPrincipal is defined
-        /// https://docs.microsoft.com/azure/static-web-apps/user-information?tabs=csharp#client-principal-data
-        /// </summary>
-        public class StaticWebAppsClientPrincipal
+    /// <summary>
+    /// Base64 decodes and deserializes the x-ms-client-principal payload containing
+    /// SWA token metadata.
+    /// Writes SWA token metadata (roles and token claims) to .NET ClaimsIdentity object.
+    /// </summary>
+    /// <param name="context"></param>
+    /// <returns>ClaimsIdentity containing authentication metadata.</returns>
+    public static ClaimsIdentity? Parse(HttpContext context, ILogger logger)
+    {
+        ClaimsIdentity? identity = null;
+        StaticWebAppsClientPrincipal principal = new();
+        try
         {
-            public string? IdentityProvider { get; set; }
-            public string? UserId { get; set; }
-            public string? UserDetails { get; set; }
-            public IEnumerable<string>? UserRoles { get; set; }
-        }
-
-        /// <summary>
-        /// Base64 decodes and deserializes the x-ms-client-principal payload containing
-        /// SWA token metadata.
-        /// Writes SWA token metadata (roles and token claims) to .NET ClaimsIdentity object.
-        /// </summary>
-        /// <param name="context"></param>
-        /// <returns>ClaimsIdentity containing authentication metadata.</returns>
-        public static ClaimsIdentity? Parse(HttpContext context, ILogger logger)
-        {
-            ClaimsIdentity? identity = null;
-            StaticWebAppsClientPrincipal principal = new();
-            try
+            if (context.Request.Headers.TryGetValue(AuthenticationOptions.CLIENT_PRINCIPAL_HEADER, out StringValues headerPayload))
             {
-                if (context.Request.Headers.TryGetValue(AuthenticationOptions.CLIENT_PRINCIPAL_HEADER, out StringValues headerPayload))
-                {
-                    string data = headerPayload[0];
-                    byte[] decoded = Convert.FromBase64String(data);
-                    string json = Encoding.UTF8.GetString(decoded);
-                    principal = JsonSerializer.Deserialize<StaticWebAppsClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? new();
-                }
+                string data = headerPayload[0];
+                byte[] decoded = Convert.FromBase64String(data);
+                string json = Encoding.UTF8.GetString(decoded);
+                principal = JsonSerializer.Deserialize<StaticWebAppsClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true }) ?? new();
+            }
 
-                // Null UserRoles collection indicates that SWA authentication failed
-                // because all requests will at least have the 'anonymous role'
-                if (principal is null || principal.UserRoles is null || !principal.UserRoles.Any())
-                {
-                    return identity;
-                }
-
-                identity = new(authenticationType: principal.IdentityProvider, nameType: USER_ID_CLAIM, roleType: AuthenticationOptions.ROLE_CLAIM_TYPE);
-
-                if (!string.IsNullOrWhiteSpace(principal.UserId))
-                {
-                    identity.AddClaim(new Claim(USER_ID_CLAIM, principal.UserId));
-                }
-
-                if (!string.IsNullOrWhiteSpace(principal.UserDetails))
-                {
-                    identity.AddClaim(new Claim(USER_DETAILS_CLAIM, principal.UserDetails));
-                }
-
-                // output identity.Claims
-                // [0] { Type = "roles", Value = "roleName" }
-                identity.AddClaims(principal.UserRoles.Select(roleName => new Claim(AuthenticationOptions.ROLE_CLAIM_TYPE, roleName)));
-
+            // Null UserRoles collection indicates that SWA authentication failed
+            // because all requests will at least have the 'anonymous role'
+            if (principal is null || principal.UserRoles is null || !principal.UserRoles.Any())
+            {
                 return identity;
             }
-            catch (Exception error) when (
-                error is JsonException ||
-                error is ArgumentNullException ||
-                error is NotSupportedException ||
-                error is InvalidOperationException)
+
+            identity = new(authenticationType: principal.IdentityProvider, nameType: USER_ID_CLAIM, roleType: AuthenticationOptions.ROLE_CLAIM_TYPE);
+
+            if (!string.IsNullOrWhiteSpace(principal.UserId))
             {
-                // Log any SWA token processing failures in the logger.
-                // Does not raise or rethrow a DataApiBuilder exception because
-                // the authentication handler caller will return a 401 unauthorized
-                // response to the client.
-                logger.LogError(
-                    exception: error,
-                    message: "{correlationId} Failure processing the StaticWebApps EasyAuth header due to:\n{errorMessage}",
-                    HttpContextExtensions.GetLoggerCorrelationId(context),
-                    error.Message);
+                identity.AddClaim(new Claim(USER_ID_CLAIM, principal.UserId));
             }
+
+            if (!string.IsNullOrWhiteSpace(principal.UserDetails))
+            {
+                identity.AddClaim(new Claim(USER_DETAILS_CLAIM, principal.UserDetails));
+            }
+
+            // output identity.Claims
+            // [0] { Type = "roles", Value = "roleName" }
+            identity.AddClaims(principal.UserRoles.Select(roleName => new Claim(AuthenticationOptions.ROLE_CLAIM_TYPE, roleName)));
 
             return identity;
         }
+        catch (Exception error) when (
+            error is JsonException ||
+            error is ArgumentNullException ||
+            error is NotSupportedException ||
+            error is InvalidOperationException)
+        {
+            // Log any SWA token processing failures in the logger.
+            // Does not raise or rethrow a DataApiBuilder exception because
+            // the authentication handler caller will return a 401 unauthorized
+            // response to the client.
+            logger.LogError(
+                exception: error,
+                message: "{correlationId} Failure processing the StaticWebApps EasyAuth header due to:\n{errorMessage}",
+                HttpContextExtensions.GetLoggerCorrelationId(context),
+                error.Message);
+        }
+
+        return identity;
     }
 }
--- src/Core/Authorization/ClientRoleHeaderAuthorizationMiddleware.cs
+++ src/Core/Authorization/ClientRoleHeaderAuthorizationMiddleware.cs
@@ -6,74 +6,73 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Primitives;
 
-namespace Azure.DataApiBuilder.Core.Authorization
+namespace Azure.DataApiBuilder.Core.Authorization;
+
+/// <summary>
+/// This middleware to be executed prior to reaching Controllers
+/// Evaluates request and User(token) claims against developer config permissions.
+/// Authorization should do little to no request validation as that is handled
+/// in later middleware.
+/// </summary>
+public class ClientRoleHeaderAuthorizationMiddleware
 {
-    /// <summary>
-    /// This middleware to be executed prior to reaching Controllers
-    /// Evaluates request and User(token) claims against developer config permissions.
-    /// Authorization should do little to no request validation as that is handled
-    /// in later middleware.
-    /// </summary>
-    public class ClientRoleHeaderAuthorizationMiddleware
+    private readonly RequestDelegate _next;
+
+    public ClientRoleHeaderAuthorizationMiddleware(RequestDelegate next)
     {
-        private readonly RequestDelegate _next;
-
-        public ClientRoleHeaderAuthorizationMiddleware(RequestDelegate next)
-        {
-            _next = next;
-        }
-
-        public async Task Invoke(HttpContext httpContext)
-        {
-            if (!IsValidRoleContext(httpContext))
-            {
-                //Handle authorization failure and terminate the request.
-                httpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
-                return;
-            }
-
-            await _next(httpContext);
-        }
-
-        public static bool IsValidRoleContext(HttpContext httpContext)
-        {
-            StringValues clientRoleHeader = httpContext.Request.Headers[AuthorizationResolver.CLIENT_ROLE_HEADER];
-
-            if (clientRoleHeader.Count != 1)
-            {
-                // When count = 0, the clientRoleHeader is absent on requests.
-                // Consequentially, anonymous requests must specifically set
-                // the clientRoleHeader value to Anonymous.
-
-                // When count > 1, multiple header fields with the same field-name
-                // are present in a message, but are NOT supported, specifically for the client role header.
-                // Valid scenario per HTTP Spec: http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2
-                // Discussion: https://stackoverflow.com/a/3097052/18174950
-                return false;
-            }
-
-            string clientRoleHeaderValue = clientRoleHeader.ToString();
-
-            // The clientRoleHeader must have a value.
-            if (clientRoleHeaderValue.Length == 0)
-            {
-                return false;
-            }
-
-            // IsInRole looks at all the claims present in the request
-            // Reference: https://github.com/microsoft/referencesource/blob/master/mscorlib/system/security/claims/ClaimsPrincipal.cs
-            return httpContext.User.IsInRole(clientRoleHeaderValue);
-        }
+        _next = next;
     }
 
-    /// <summary>
-    /// Extension method used to add the middleware to the HTTP request pipeline.
-    /// </summary>
-    public static class ClientRoleHeaderAuthorizationMiddlewareExtensions
+    public async Task Invoke(HttpContext httpContext)
     {
-        public static IApplicationBuilder UseClientRoleHeaderAuthorizationMiddleware(this IApplicationBuilder builder)
+        if (!IsValidRoleContext(httpContext))
         {
-            return builder.UseMiddleware<ClientRoleHeaderAuthorizationMiddleware>();
+            //Handle authorization failure and terminate the request.
+            httpContext.Response.StatusCode = (int)HttpStatusCode.Forbidden;
+            return;
         }
+
+        await _next(httpContext);
+    }
+
+    public static bool IsValidRoleContext(HttpContext httpContext)
+    {
+        StringValues clientRoleHeader = httpContext.Request.Headers[AuthorizationResolver.CLIENT_ROLE_HEADER];
+
+        if (clientRoleHeader.Count != 1)
+        {
+            // When count = 0, the clientRoleHeader is absent on requests.
+            // Consequentially, anonymous requests must specifically set
+            // the clientRoleHeader value to Anonymous.
+
+            // When count > 1, multiple header fields with the same field-name
+            // are present in a message, but are NOT supported, specifically for the client role header.
+            // Valid scenario per HTTP Spec: http://www.w3.org/Protocols/rfc2616/rfc2616-sec4.html#sec4.2
+            // Discussion: https://stackoverflow.com/a/3097052/18174950
+            return false;
+        }
+
+        string clientRoleHeaderValue = clientRoleHeader.ToString();
+
+        // The clientRoleHeader must have a value.
+        if (clientRoleHeaderValue.Length == 0)
+        {
+            return false;
+        }
+
+        // IsInRole looks at all the claims present in the request
+        // Reference: https://github.com/microsoft/referencesource/blob/master/mscorlib/system/security/claims/ClaimsPrincipal.cs
+        return httpContext.User.IsInRole(clientRoleHeaderValue);
+    }
+}
+
+/// <summary>
+/// Extension method used to add the middleware to the HTTP request pipeline.
+/// </summary>
+public static class ClientRoleHeaderAuthorizationMiddlewareExtensions
+{
+    public static IApplicationBuilder UseClientRoleHeaderAuthorizationMiddleware(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<ClientRoleHeaderAuthorizationMiddleware>();
     }
 }

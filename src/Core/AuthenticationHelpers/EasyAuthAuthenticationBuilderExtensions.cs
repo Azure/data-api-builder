ca5fa1463a38b3a0b5aebf31a4bff8aa64cebe73
Gerson Jr <57489438+gmottajr@users.noreply.github.com>
2023-10-06 02:04:20 -0300

    Move to file scoped namespace project Core part 1 (#1744)
    Move to file scoped namespace project Core part 1
    
    ---------
    
    Co-authored-by: gerson <mottajrgerson@gmail.com>
    Co-authored-by: Aniruddh Munde <anmunde@microsoft.com>
    Co-authored-by: Sean Leonard <sean.leonard@microsoft.com>

--- src/Core/AuthenticationHelpers/EasyAuthAuthenticationBuilderExtensions.cs
+++ src/Core/AuthenticationHelpers/EasyAuthAuthenticationBuilderExtensions.cs
@@ -4,44 +4,43 @@
 using Azure.DataApiBuilder.Config.ObjectModel;
 using Microsoft.AspNetCore.Authentication;
 
-namespace Azure.DataApiBuilder.Core.AuthenticationHelpers
+namespace Azure.DataApiBuilder.Core.AuthenticationHelpers;
+
+/// <summary>
+/// Extension methods related to Static Web App/ App Service authentication (Easy Auth).
+/// This class allows setting up Easy Auth authentication in the startup class with
+/// a single call to .AddAuthentication(scheme).AddStaticWebAppAuthentication()
+/// </summary>
+public static class EasyAuthAuthenticationBuilderExtensions
 {
     /// <summary>
-    /// Extension methods related to Static Web App/ App Service authentication (Easy Auth).
-    /// This class allows setting up Easy Auth authentication in the startup class with
-    /// a single call to .AddAuthentication(scheme).AddStaticWebAppAuthentication()
+    /// Add authentication with Static Web Apps.
     /// </summary>
-    public static class EasyAuthAuthenticationBuilderExtensions
+    /// <param name="builder">Authentication builder.</param>
+    /// <param name="easyAuthAuthenticationProvider">EasyAuth provider type. StaticWebApps or AppService</param>
+    /// <returns>The builder, to chain commands.</returns>
+    public static AuthenticationBuilder AddEasyAuthAuthentication(
+         this AuthenticationBuilder builder, EasyAuthType easyAuthAuthenticationProvider)
     {
-        /// <summary>
-        /// Add authentication with Static Web Apps.
-        /// </summary>
-        /// <param name="builder">Authentication builder.</param>
-        /// <param name="easyAuthAuthenticationProvider">EasyAuth provider type. StaticWebApps or AppService</param>
-        /// <returns>The builder, to chain commands.</returns>
-        public static AuthenticationBuilder AddEasyAuthAuthentication(
-             this AuthenticationBuilder builder, EasyAuthType easyAuthAuthenticationProvider)
+        if (builder is null)
         {
-            if (builder is null)
-            {
-                throw new System.ArgumentNullException(nameof(builder));
-            }
-
-            builder.AddScheme<EasyAuthAuthenticationOptions, EasyAuthAuthenticationHandler>(
-                authenticationScheme: EasyAuthAuthenticationDefaults.AUTHENTICATIONSCHEME,
-                displayName: EasyAuthAuthenticationDefaults.AUTHENTICATIONSCHEME,
-                options =>
-                {
-                    if (easyAuthAuthenticationProvider is EasyAuthType.StaticWebApps)
-                    {
-                        options.EasyAuthProvider = EasyAuthType.StaticWebApps;
-                    }
-                    else if (easyAuthAuthenticationProvider is EasyAuthType.AppService)
-                    {
-                        options.EasyAuthProvider = EasyAuthType.AppService;
-                    }
-                });
-            return builder;
+            throw new System.ArgumentNullException(nameof(builder));
         }
+
+        builder.AddScheme<EasyAuthAuthenticationOptions, EasyAuthAuthenticationHandler>(
+            authenticationScheme: EasyAuthAuthenticationDefaults.AUTHENTICATIONSCHEME,
+            displayName: EasyAuthAuthenticationDefaults.AUTHENTICATIONSCHEME,
+            options =>
+            {
+                if (easyAuthAuthenticationProvider is EasyAuthType.StaticWebApps)
+                {
+                    options.EasyAuthProvider = EasyAuthType.StaticWebApps;
+                }
+                else if (easyAuthAuthenticationProvider is EasyAuthType.AppService)
+                {
+                    options.EasyAuthProvider = EasyAuthType.AppService;
+                }
+            });
+        return builder;
     }
 }

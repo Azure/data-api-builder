31a3ff5ffd6a1e85d6db95fcfe4f3fd9b72d25f8
Abhishek Kumar <abhishekkuma@microsoft.com>
2023-11-15 20:54:29 +0530

    fixing tests

--- src/Service.Tests/TestHelper.cs
+++ src/Service.Tests/TestHelper.cs
@@ -55,7 +55,7 @@ namespace Azure.DataApiBuilder.Service.Tests
             if (runtimeConfigProvider.TryGetConfig(out RuntimeConfig runtimeConfig)
                 && runtimeConfig.DataSource.DatabaseType is DatabaseType.MSSQL)
             {
-                runtimeConfigProvider.IsLateConfigured = false;
+                runtimeConfigProvider.IsLateConfigured = true;
             }
 
             return runtimeConfigProvider;

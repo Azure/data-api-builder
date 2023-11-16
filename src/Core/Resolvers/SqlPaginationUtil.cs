070e836f8534c698c780728586f44fc8e07c548a
rohkhann <124841904+rohkhann@users.noreply.github.com>
2023-11-15 17:17:36 -0800

    Apply suggestions from code review
    Co-authored-by: Aniruddh Munde <anmunde@microsoft.com>
    Co-authored-by: aaronburtle <93220300+aaronburtle@users.noreply.github.com>

--- src/Core/Resolvers/SqlPaginationUtil.cs
+++ src/Core/Resolvers/SqlPaginationUtil.cs
@@ -33,8 +33,8 @@ namespace Azure.DataApiBuilder.Core.Resolvers
             // Maintains the connection JSON object *Connection
             Dictionary<string, object> connectionJson = new();
 
-            // in dw we wrap array with "" and hence jsonValueKing is string instead of array.
-            if (root.ValueKind == JsonValueKind.String)
+            // in dw we wrap array with "" and hence jsonValueKind is string instead of array.
+            if (root.ValueKind is JsonValueKind.String)
             {
                 JsonDocument document = JsonDocument.Parse(root.GetString()!);
                 root = document.RootElement;

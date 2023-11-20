a0714d4c4008603f1646fe7db723c370a14024e9
Davide Mauri <damauri@microsoft.com>
2023-11-20 14:28:34 -0800

    added method summary

--- src/Product/ProductInfo.cs
+++ src/Product/ProductInfo.cs
@@ -28,15 +28,19 @@ public static class ProductInfo
 
     /// <summary>
     /// It retrieves the user agent for the DataApiBuilder by checking the value of
-    /// DAB_APP_NAME_ENV  environment variable. If the environment variable is not set,
+    /// DAB_APP_NAME_ENV environment variable. If the environment variable is not set,
     /// it returns a default value indicating connections from open source.
-    /// The method serves as a means of identifying the source of connections made through the DataApiBuilder.
     /// </summary>
     public static string GetDataApiBuilderUserAgent()
     {
         return Environment.GetEnvironmentVariable(DAB_APP_NAME_ENV) ?? DEFAULT_APP_NAME;
     }
 
+    /// <summary>
+    /// Returns the application name to be used for database connections for the DataApiBuilder.
+    /// It strips the hash value from the user agent string to only return the application name and the version.
+    /// The method serves as a means of identifying the source of connections made through the DataApiBuilder.
+    /// </summary>
     public static string GetDataApiBuilderApplicationName()
     {
         string dabVersion = ProductInfo.GetDataApiBuilderUserAgent();

36ab82ad068d7611d9a967cc20be25d0fb6f5e1c
Sean Leonard <sean.leonard@microsoft.com>
2023-12-05 18:17:41 -0800

    Add Cache properties to runtime config file with JSON Serialize/Deserialize (#1865)
    ## Why make this change?
    
    - first work attributed to #1618 #1617 
    
    ## What is this change?
    
    ### Global Cache Config property `cache`
    - Adds global config property `cache` with value type of boolean in the
    runtime section:
    ```json
    {
      "runtime": {
        "cache": false
      }
    }
    ```
    Behavior:
      - Nullable and defaults to false.
    - When null, will not be written to the CLI created runtime config JSON
    
    ### Entity Cache Config property/object `cache`
    - Adds entity specific config property `cache` in entity section:
    ```json
    {
      "entityName": {
        "cache": {
          "enabled": true,
          "ttl-seconds": 60
        }
      }
    }
    ```
    Behavior:
    - Nullable and defaults to `"enabled": false, "ttl-seconds": 60`
    - When an empty object -> caching disabled
    - When null -> caching disabled
    - ttl-seconds defaults to 60 regardless but due to private field state,
    tracks whether the value was user provided or set with the default value
    due to not being explicitly set in runtime config. This enables CLI
    runtime config creation to exclude the `ttl-seconds` property and value
    if not explicitly set by the user.
    
    
    ## How was this tested?
    - [X] manually tested CLI handling of writing `ttl-seconds` to file. but
    CLI specific work will come in separate pr.
    - [X] Unit Tests -> System under test is
    `RuntimeConfigLoader.TryParseConfig(string json, ...)` such that I test
    the behavior of any json parsing that goes on. JSON parsing mechanisms
    are implementation details and aren't tested directly because the
    implementation can change. I explicitly test for the desired behavior. I
    test Behavior such as providing null and empty json objects in order to
    validate how CLI merge config will work. and to avoid regressions that
    have come up in the past due to improper/lackof null handling.
    
    ## Sample Request(s)
    
    Caching is not yet enabled. Behavior can be observed in unit test class
    `CachingConfigDeserializationTests` or you can create a config using the
    CLI:
    
    1. Create a folder called `dabconfigs` at root of C:\ drive to work with
    these sample commands.
    2. run DAB cli to create a config file and one entity called Book.
    ```sh
    Microsoft.DataApiBuilder init --database-type mssql -c "C:\dabconfigs\dab-config.json"              
       
    Microsoft.DataApiBuilder add Book -c "C:\dabconfigs\dab-config.json" --source books --permissions "anonymous:create,read,update,delete" --graphql "book:books"
    ```
    
    3. Modify config file directly to add cache config to entity.
    ```json
        "Book": {
          "source": {
            "object": "books",
            "type": "table"
          },
          "graphql": {
            "enabled": true,
            "type": {
              "singular": "book",
              "plural": "books"
            }
          },
          "rest": {
            "enabled": true
          },
          "permissions": [
            {
              "role": "anonymous",
              "actions": [
                {
                  "action": "*"
                }
              ]
            }
          ],
          "cache": {
            "enabled": true
          }
        }
    ```
    
    4. add another entity, and validate that `ttl-seconds` is not added to
    the `Book` entity config because a non-explicitly set ttl should not
    result in a property/value being written to the config from CLI. Under
    the hood, the default 60 second value is used, which is validated in
    unit tests.
    ```sh
    Microsoft.DataApiBuilder add Dog -c "C:\dabconfigs\dab-config.json" --source books --permissions "anonymous:create,read,update,delete" --graphql "dog:dogs"
    ```

--- src/Config/ObjectModel/RuntimeOptions.cs
+++ src/Config/ObjectModel/RuntimeOptions.cs
@@ -1,11 +1,44 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System.Text.Json.Serialization;
+
 namespace Azure.DataApiBuilder.Config.ObjectModel;
 
-public record RuntimeOptions(
-    RestRuntimeOptions? Rest,
-    GraphQLRuntimeOptions? GraphQL,
-    HostOptions? Host,
-    string? BaseRoute = null,
-    TelemetryOptions? Telemetry = null);
+public record RuntimeOptions
+{
+    public RestRuntimeOptions? Rest { get; init; }
+    public GraphQLRuntimeOptions? GraphQL { get; init; }
+    public HostOptions? Host { get; init; }
+    public string? BaseRoute { get; init; }
+    public TelemetryOptions? Telemetry { get; init; }
+    public EntityCacheOptions? Cache { get; init; }
+
+    [JsonConstructor]
+    public RuntimeOptions(
+        RestRuntimeOptions? Rest,
+        GraphQLRuntimeOptions? GraphQL,
+        HostOptions? Host,
+        string? BaseRoute = null,
+        TelemetryOptions? Telemetry = null,
+        EntityCacheOptions? Cache = null)
+    {
+        this.Rest = Rest;
+        this.GraphQL = GraphQL;
+        this.Host = Host;
+        this.BaseRoute = BaseRoute;
+        this.Telemetry = Telemetry;
+        this.Cache = Cache;
+    }
+
+    /// <summary>
+    /// Resolves the value of the cache property if present, default is false.
+    /// Caching is enabled only when explicitly set to true.
+    /// </summary>
+    /// <returns>Whether caching is enabled globally.</returns>
+    [JsonIgnore]
+    public bool IsCachingEnabled =>
+            Cache is not null &&
+            Cache.Enabled is not null &&
+            Cache.Enabled is true;
+}

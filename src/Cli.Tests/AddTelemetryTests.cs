309debf098c67f9884dbeb0658210aea387fa832
Abhishek Kumar <abhishekkuma@microsoft.com>
2024-01-04 21:45:48 +0530

    updating test

--- src/Cli.Tests/AddTelemetryTests.cs
+++ src/Cli.Tests/AddTelemetryTests.cs
@@ -4,7 +4,7 @@
 namespace Cli.Tests
 {
     /// <summary>
-    /// Tests for Adding new Entity.
+    /// Tests for verifying the functionality of adding telemetry to the config file.
     /// </summary>
     [TestClass]
     public class AddTelemetryTests
@@ -19,11 +19,15 @@ namespace Cli.Tests
             SetCliUtilsLogger(loggerFactory.CreateLogger<Utils>());
         }
 
+        /// <summary>
+        /// Testing to check telemetry options are correctly added to the config.
+        /// Verifying scenarios such as enabling/disabling telemetry and providing a valid/empty connection string.
+        /// </summary>
         [DataTestMethod]
-        [DataRow(true, "", DisplayName = "Add Telemetry with empty connection string")]
-        [DataRow(true, "InstrumentationKey=00000000-0000-0000-0000-000000000000", DisplayName = "Add Telemetry with valid connection string")]
-        [DataRow(false, "InstrumentationKey=00000000-0000-0000-0000-000000000000", DisplayName = "Add Telemetry but disabled")]
-        public void AddTelemetryTest(bool isTelemetryEnabled, string appInsightsConnString)
+        [DataRow(true, "", false, DisplayName = "Add Telemetry with empty connection string should fail.")]
+        [DataRow(true, "InstrumentationKey=00000000-0000-0000-0000-000000000000", true, DisplayName = "Add Telemetry with valid connection string")]
+        [DataRow(false, "InstrumentationKey=00000000-0000-0000-0000-000000000000", true, DisplayName = "Add Telemetry but disabled")]
+        public void TestAddApplicationInsightsTelemetry(bool isTelemetryEnabled, string appInsightsConnString, bool expectSuccess)
         {
             MockFileSystem fileSystem = FileSystemUtils.ProvisionMockFileSystem();
             string configPath = "test-app-insights-config.json";
@@ -42,14 +46,17 @@ namespace Cli.Tests
                 fileSystem);
 
             // Assert after adding telemetry
-            Assert.IsTrue(isSuccess);
-            Assert.IsTrue(fileSystem.FileExists(configPath));
-            Assert.IsTrue(RuntimeConfigLoader.TryParseConfig(fileSystem.File.ReadAllText(configPath), out config));
-            Assert.IsNotNull(config);
-            Assert.IsNotNull(config.Runtime!.Telemetry);
-            Assert.IsNotNull(config.Runtime!.Telemetry!.ApplicationInsights);
-            Assert.AreEqual(isTelemetryEnabled, config.Runtime!.Telemetry!.ApplicationInsights!.Enabled);
-            Assert.AreEqual(appInsightsConnString, config.Runtime!.Telemetry!.ApplicationInsights!.ConnectionString);
+            Assert.AreEqual(expectSuccess, isSuccess);
+            if (expectSuccess)
+            {
+                Assert.IsTrue(fileSystem.FileExists(configPath));
+                Assert.IsTrue(RuntimeConfigLoader.TryParseConfig(fileSystem.File.ReadAllText(configPath), out config));
+                Assert.IsNotNull(config);
+                Assert.IsNotNull(config.Runtime!.Telemetry);
+                Assert.IsNotNull(config.Runtime!.Telemetry!.ApplicationInsights);
+                Assert.AreEqual(isTelemetryEnabled, config.Runtime!.Telemetry!.ApplicationInsights!.Enabled);
+                Assert.AreEqual(appInsightsConnString, config.Runtime!.Telemetry!.ApplicationInsights!.ConnectionString);
+            }
         }
     }
 }

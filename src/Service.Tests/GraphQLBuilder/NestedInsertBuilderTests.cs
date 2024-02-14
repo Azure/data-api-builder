422630da7f671acba6fd553ff7faa1776ed2d178
Ayush Agarwal <ayusha083@gmail.com>
2024-02-14 19:00:22 +0530

    initial prgress

--- src/Service.Tests/GraphQLBuilder/NestedInsertBuilderTests.cs
+++ src/Service.Tests/GraphQLBuilder/NestedInsertBuilderTests.cs
@@ -26,6 +26,7 @@ using Azure.DataApiBuilder.Config.ObjectModel;
 using System;
 using static Azure.DataApiBuilder.Service.GraphQLBuilder.GraphQLNaming;
 using System.Linq;
+using Azure.DataApiBuilder.Service.GraphQLBuilder.Directives;
 
 namespace Azure.DataApiBuilder.Service.Tests.GraphQLBuilder
 {
@@ -33,15 +34,76 @@ namespace Azure.DataApiBuilder.Service.Tests.GraphQLBuilder
     public class NestedInsertBuilderTests
     {
         private static RuntimeConfig _runtimeConfig;
-        private static GraphQLSchemaCreator _schemaCreator;
+        private static DocumentNode _documentNode;
+        //private static DocumentNode _mutationNode;
+        private static IEnumerable<IHasName> _definitions;
+        //private static Dictionary<string, InputObjectTypeDefinitionNode> _inputTypeObjects;
 
         [ClassInitialize]
         public static async Task SetUpAsync(TestContext context)
         {
-            (_schemaCreator, _runtimeConfig) = await SetUpGQLSchemaCreator("MsSql");
+            (GraphQLSchemaCreator schemaCreator, _runtimeConfig) = await SetUpGQLSchemaCreatorAndConfig("MsSql");
+            (_documentNode, Dictionary<string, InputObjectTypeDefinitionNode> inputTypes) = schemaCreator.GenerateGraphQLObjects();
+            _definitions = _documentNode.Definitions.Where(d => d is IHasName).Cast<IHasName>();
+            (_, _) = schemaCreator.GenerateQueryAndMutationNodes(_documentNode, inputTypes);
         }
 
-        private static async Task<Tuple<GraphQLSchemaCreator, RuntimeConfig>> SetUpGQLSchemaCreator(string databaseType)
+        [DataTestMethod]
+        [DataRow("Book", new string[] { "publisher_id" })]
+        [DataRow("Review", new string[] { "book_id" })]
+        [DataRow("stocks_price", new string[] { "categoryid", "pieceid" })]
+        public void ValidatePresenceOfForeignKeyDirectiveOnReferencingColumns(string referencingEntityName, string[] referencingColumns)
+        {
+            ObjectTypeDefinitionNode objectTypeDefinitionNode = GetObjectTypeDefinitionNode(
+                new NameNode(GetDefinedSingularName(
+                   entityName: referencingEntityName,
+                   configEntity: _runtimeConfig.Entities[referencingEntityName])));
+            List<FieldDefinitionNode> fieldsInObjectDefinitionNode = objectTypeDefinitionNode.Fields.ToList();
+            foreach (string referencingColumn in referencingColumns)
+            {
+                int indexOfReferencingField = fieldsInObjectDefinitionNode.FindIndex((field => field.Name.Value.Equals(referencingColumn)));
+                FieldDefinitionNode referencingFieldDefinition = fieldsInObjectDefinitionNode[indexOfReferencingField];
+                Assert.IsTrue(referencingFieldDefinition.Directives.Any(directive => directive.Name.Value == ForeignKeyDirectiveType.DirectiveName));
+            }
+        }
+
+        [DataTestMethod]
+        [DataRow("Book", "Author", DisplayName = "Validate absence of linking object for Book->Author M:N relationship")]
+        [DataRow("Author", "Book", DisplayName = "Validate absence of linking object for Author->Book M:N relationship")]
+        [DataRow("BookNF", "AuthorNF", DisplayName = "Validate absence of linking object for BookNF->AuthorNF M:N relationship")]
+        [DataRow("AuthorNF", "BookNF", DisplayName = "Validate absence of linking object for BookNF->AuthorNF M:N relationship")]
+        public void ValidateAbsenceOfLinkingObjectDefinitionsInDocumentNodeForMNRelationships(string sourceEntityName, string targetEntityName)
+        {
+            string linkingEntityName = Entity.GenerateLinkingEntityName(sourceEntityName, targetEntityName);
+
+            // Validate that the document node does not expose the object type definition for linking table.
+            ObjectTypeDefinitionNode linkingObjectTypeDefinitionNode = GetObjectTypeDefinitionNode(new NameNode(linkingEntityName));
+            Assert.IsNull(linkingObjectTypeDefinitionNode);
+        }
+
+        [DataTestMethod]
+        [DataRow("Book", "Author", DisplayName = "Validate presence of source->target linking object for Book->Author M:N relationship")]
+        [DataRow("Author", "Book", DisplayName = "Validate presence of source->target linking object for Author->Book M:N relationship")]
+        [DataRow("BookNF", "AuthorNF", DisplayName = "Validate presence of source->target linking object for BookNF->AuthorNF M:N relationship")]
+        [DataRow("AuthorNF", "BookNF", DisplayName = "Validate presence of source->target linking object for BookNF->AuthorNF M:N relationship")]
+        public void ValidatePresenceOfSourceTargetLinkingObjectDefinitionsInDocumentNodeForMNRelationships(string sourceEntityName, string targetEntityName)
+        {
+            // Validate that we have indeed inferred the object type definition for all the source->target linking objects.
+            NameNode sourceTargetLinkingNodeName = new(GenerateLinkingNodeName(
+                        GetDefinedSingularName(sourceEntityName, _runtimeConfig.Entities[sourceEntityName]),
+                        GetDefinedSingularName(targetEntityName, _runtimeConfig.Entities[targetEntityName])));
+            ObjectTypeDefinitionNode sourceTargetLinkingObjectTypeDefinitionNode = GetObjectTypeDefinitionNode(sourceTargetLinkingNodeName);
+            Assert.IsNotNull(sourceTargetLinkingObjectTypeDefinitionNode);
+            
+        }
+
+        private static ObjectTypeDefinitionNode GetObjectTypeDefinitionNode(NameNode sourceTargetLinkingNodeName)
+        {
+            IHasName definition = _definitions.FirstOrDefault(d => d.Name.Value == sourceTargetLinkingNodeName.Value);
+            return definition is ObjectTypeDefinitionNode objectTypeDefinitionNode ? objectTypeDefinitionNode : null;
+        }
+
+        private static async Task<Tuple<GraphQLSchemaCreator, RuntimeConfig>> SetUpGQLSchemaCreatorAndConfig(string databaseType)
         {
             string fileContents = await File.ReadAllTextAsync($"dab-config.{databaseType}.json");
 
@@ -77,46 +139,6 @@ namespace Azure.DataApiBuilder.Service.Tests.GraphQLBuilder
             Mock<IMutationEngineFactory> mutationEngineFactory = new();
 
             return new(new GraphQLSchemaCreator(provider, queryEngineFactory, mutationEngineFactory.Object, metadataProviderFactory, authzResolver), runtimeConfig);
-        }
-
-        [DataTestMethod]
-        [DataRow("Book", "Author", DisplayName = "Validate absence of linking object for Book->Author M:N relationship")]
-        [DataRow("Author", "Book", DisplayName = "Validate absence of linking object for Author->Book M:N relationship")]
-        [DataRow("BookNF", "AuthorNF", DisplayName = "Validate absence of linking object for BookNF->AuthorNF M:N relationship")]
-        [DataRow("AuthorNF", "BookNF", DisplayName = "Validate absence of linking object for BookNF->AuthorNF M:N relationship")]
-        public void ValidateAbsenceOfLinkingObjectDefinitionsInDocumentNodeForMNRelationships(string sourceEntityName, string targetEntityName)
-        {
-            (DocumentNode documentNode, Dictionary<string, InputObjectTypeDefinitionNode> _inputTypeObjects) = _schemaCreator.GenerateGraphQLObjects();
-            string linkingEntityName = Entity.GenerateLinkingEntityName(sourceEntityName, targetEntityName);
-
-            // Validate that the document node does not expose the object type definition for linking table.
-            IEnumerable<IHasName> definitions = documentNode.Definitions.Where(d => d is IHasName).Cast<IHasName>();
-            ObjectTypeDefinitionNode linkingObjectTypeDefinitionNode = GetObjectTypeDefinitionNode(new NameNode(linkingEntityName), definitions);
-            Assert.IsNull(linkingObjectTypeDefinitionNode);
-        }
-
-        [DataTestMethod]
-        [DataRow("Book", "Author", DisplayName = "Validate presence of source->target linking object for Book->Author M:N relationship")]
-        [DataRow("Author", "Book", DisplayName = "Validate presence of source->target linking object for Author->Book M:N relationship")]
-        [DataRow("BookNF", "AuthorNF", DisplayName = "Validate presence of source->target linking object for BookNF->AuthorNF M:N relationship")]
-        [DataRow("AuthorNF", "BookNF", DisplayName = "Validate presence of source->target linking object for BookNF->AuthorNF M:N relationship")]
-        public void ValidatePresenceOfSourceTargetLinkingObjectDefinitionsInDocumentNodeForMNRelationships(string sourceEntityName, string targetEntityName)
-        {
-            (DocumentNode documentNode, Dictionary<string, InputObjectTypeDefinitionNode> _inputTypeObjects) = _schemaCreator.GenerateGraphQLObjects();
-            // Validate that we have indeed inferred the object type definition for all the source->target linking objects.
-            IEnumerable<IHasName> definitions = documentNode.Definitions.Where(d => d is IHasName).Cast<IHasName>();
-            NameNode sourceTargetLinkingNodeName = new(GenerateLinkingNodeName(
-                        GetDefinedSingularName(sourceEntityName, _runtimeConfig.Entities[sourceEntityName]),
-                        GetDefinedSingularName(targetEntityName, _runtimeConfig.Entities[targetEntityName])));
-            ObjectTypeDefinitionNode sourceTargetLinkingObjectTypeDefinitionNode = GetObjectTypeDefinitionNode(sourceTargetLinkingNodeName, definitions);
-            Assert.IsNotNull(sourceTargetLinkingObjectTypeDefinitionNode);
-            
-        }
-
-        private static ObjectTypeDefinitionNode GetObjectTypeDefinitionNode(NameNode sourceTargetLinkingNodeName, IEnumerable<IHasName> definitions)
-        {
-            IHasName definition = definitions.FirstOrDefault(d => d.Name.Value == sourceTargetLinkingNodeName.Value);
-            return definition is ObjectTypeDefinitionNode objectTypeDefinitionNode ? objectTypeDefinitionNode : null;
         }
     }
 }

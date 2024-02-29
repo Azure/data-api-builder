2d2fc79e585ada8b04771b3061418b8cc7a23a85
Ayush Agarwal <ayusha083@gmail.com>
2024-03-01 01:19:59 +0530

    Adding unit tests for referencing entity determination based on metadata

--- src/Service.Tests/Unittests/NestedCreateOrderHelperUnitTests.cs
+++ src/Service.Tests/Unittests/NestedCreateOrderHelperUnitTests.cs
@@ -6,78 +6,82 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Azure.DataApiBuilder.Core.Resolvers;
 using HotChocolate.Resolvers;
 using Moq;
+using HotChocolate.Execution;
 
 namespace Azure.DataApiBuilder.Service.Tests.Unittests
 {
     [TestClass]
     public abstract class NestedCreateOrderHelperUnitTests : SqlTestBase
     {
-        [TestMethod]
         /// <summary>
-        /// Helper method for tests which validate that the relationship data is correctly inferred based on the info provided
-        /// in the config and the metadata collected from the database. It runs the test against various test cases verifying that
-        /// when a relationship is defined in the config between source and target entity and:
-        ///
-        /// a) An FK constraint exists in the database between the two entities: We successfully determine which is the referencing
-        /// entity based on the FK constraint. If custom source.fields/target.fields are provided, preference is given to those fields.
-        ///
-        /// b) No FK constraint exists in the database between the two entities: We ÇANNOT determine which entity is the referencing
-        /// entity and hence we keep ourselves open to the possibility of either entity acting as the referencing entity.
-        /// The actual referencing entity is determined during request execution.
+        /// Test to validate that when an FK constraint exists between the entities, we can determine which of the entity
+        /// acts as the referencing entity based on the database metadata that we collect at the startup.
+        /// The entity which holds the foreign key reference acts as the referencing entity.
         /// </summary>
+        [TestMethod]
         public void InferReferencingEntityBasedOnEntityMetadata()
         {
-            // Validate that when custom source.fields/target.fields are defined in the config for a relationship of cardinality *:1
-            // between Book - Stock but no FK constraint exists between them, we ÇANNOT successfully determine at the startup,
-            // which entity is the referencing entity and hence keep ourselves open to the possibility of either entity acting
-            // as the referencing entity. The actual referencing entity is determined during request execution.
-            //ValidateReferencingEntitiesForRelationship("Book", "Stock", "Book");
-
-            // Validate that when custom source.fields/target.fields defined in the config for a relationship of cardinality N:1
-            // between Review - Book is the same as the FK constraint from Review -> Book,
+            // Validate that for a relationship of cardinality N:1 between Review - Book where FK constraint
+            // exists from Review -> Book, irrespective of which entity is the present at higher level in nested create operation,
             // we successfully determine at the startup, that Review is the referencing entity.
-            ValidateReferencingEntitiesForRelationship("Review", "Book", "Review" );
 
-            // Validate that when custom source.fields/target.fields defined in the config for a relationship of cardinality 1:N
-            // between Book - Review is the same as the FK constraint from Review -> Book,
-            // we successfully determine at the startup, that Review is the referencing entity.
-            ValidateReferencingEntitiesForRelationship("Book", "Review", "Review" );
+            // Review is the higher level entity.
+            ValidateReferencingEntitiesForRelationship(
+                higherLevelEntityName: "Review",
+                lowerLevelEntityName: "Book",
+                expectedreferencingEntityName: "Review" );
 
-            // Validate that when custom source.fields/target.fields defined in the config for a relationship of cardinality 1:1
-            // between Stock - stocks_price is the same as the FK constraint from stocks_price -> Stock,
-            // we successfully determine at the startup, that stocks_price is the referencing entity.
-            ValidateReferencingEntitiesForRelationship("Stock", "stocks_price", "stocks_price" );
+            // Book is the higher level entity.
+            ValidateReferencingEntitiesForRelationship(
+                higherLevelEntityName: "Book",
+                lowerLevelEntityName: "Review",
+                expectedreferencingEntityName: "Review");
 
-            // Validate that when no custom source.fields/target.fields are defined in the config for a relationship of cardinality N:1
-            // between Book - Publisher and an FK constraint exists from Book->Publisher, we successfully determine at the startup,
-            // that Book is the referencing entity.
-            ValidateReferencingEntitiesForRelationship("Book", "Publisher", "Book" );
+            // Validate that for a relationship of cardinality 1:N between Book - Publisher where FK constraint
+            // exists from Book -> Publisher,irrespective of which entity is the present at higher level in nested create operation,
+            // we successfully determine at the startup, that Book is the referencing entity.
 
-            // Validate that when no custom source.fields/target.fields are defined in the config for a relationship of cardinality 1:N
-            // between Publisher - Book and an FK constraint exists from Book->Publisher, we successfully determine at the startup,
-            // that Book is the referencing entity.
-            ValidateReferencingEntitiesForRelationship("Publisher", "Book", "Book" );
+            // Book is the higher level entity.
+            ValidateReferencingEntitiesForRelationship(
+                higherLevelEntityName: "Book",
+                lowerLevelEntityName: "Publisher",
+                expectedreferencingEntityName: "Book");
 
-            // Validate that when no custom source.fields/target.fields are defined in the config for a relationship of cardinality 1:1
-            // between Book - BookWebsitePlacement and an FK constraint exists from BookWebsitePlacement->Book,
-            // we successfully determine at the startup, that BookWebsitePlacement is the referencing entity.
-            //ValidateReferencingEntitiesForRelationship("Book", "BookWebsitePlacement", new List<string>() { "BookWebsitePlacement" });
+            // Publisher is the higher level entity.
+            ValidateReferencingEntitiesForRelationship(
+                higherLevelEntityName: "Publisher",
+                lowerLevelEntityName: "Book",
+                expectedreferencingEntityName: "Book");
+
+            // Validate that for a relationship of cardinality 1:1 between Stock - stocks_price where FK constraint
+            // exists from stocks_price -> Stock, we successfully determine at the startup, that stocks_price is the
+            // referencing entity.
+            // Stock is the higher level entity.
+            ValidateReferencingEntitiesForRelationship(
+                higherLevelEntityName: "Stock",
+                lowerLevelEntityName: "stocks_price",
+                expectedreferencingEntityName: "stocks_price");
         }
 
         private static void ValidateReferencingEntitiesForRelationship(
-            string sourceEntityName,
-            string targetEntityName,
-            string expectedreferencingEntityNames)
+            string higherLevelEntityName,
+            string lowerLevelEntityName,
+            string expectedreferencingEntityName)
         {
+            // Setup mock IMiddlewareContext.
             Mock<IMiddlewareContext> context = new();
+            Mock<IVariableValueCollection> variables = new();
+            context.Setup(x => x.Variables).Returns(variables.Object);
+
+            // Get the referencing entity.
             string actualReferencingEntityName = NestedCreateOrderHelper.GetReferencingEntityName(
-                context.Object,
-                sourceEntityName,
-                targetEntityName,
-                _sqlMetadataProvider,
-                new(),
-                null);
-            Assert.AreEqual(expectedreferencingEntityNames, actualReferencingEntityName);
+                context: context.Object,
+                sourceEntityName: higherLevelEntityName,
+                targetEntityName: lowerLevelEntityName,
+                metadataProvider: _sqlMetadataProvider,
+                columnDataInSourceBody: new(),
+                targetNodeValue: null);
+            Assert.AreEqual(expectedreferencingEntityName, actualReferencingEntityName);
         }
     }
 }

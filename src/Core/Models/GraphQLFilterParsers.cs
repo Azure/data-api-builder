aff4c172ad2499a96dda6f469e6eda1a7f39e2d8
Sourabh Jain <sourabhjain@microsoft.com>
2024-03-23 07:37:55 +0530

    fix formattig issue

--- src/Core/Models/GraphQLFilterParsers.cs
+++ src/Core/Models/GraphQLFilterParsers.cs
@@ -517,7 +517,7 @@ public class GQLFilterParser
         BaseQueryStructure baseQuery,
         PredicateOperation op)
     {
-        if (fields.Count == 0 && (baseQuery is CosmosQueryStructure cosmosQueryStructure))
+        if (fields.Count == 0)
         {
             return Predicate.MakeFalsePredicate();
         }

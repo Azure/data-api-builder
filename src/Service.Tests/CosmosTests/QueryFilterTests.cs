1c1ff54ff0abb2b48243b8d233706b00689f7dff
Sourabh Jain <sourabhjain@microsoft.com>
2024-03-31 06:51:56 +0530

    Update src/Service.Tests/CosmosTests/QueryFilterTests.cs
    Co-authored-by: Aniruddh Munde <anmunde@microsoft.com>

--- src/Service.Tests/CosmosTests/QueryFilterTests.cs
+++ src/Service.Tests/CosmosTests/QueryFilterTests.cs
@@ -1179,7 +1179,7 @@ namespace Azure.DataApiBuilder.Service.Tests.CosmosTests
         }
 
         /// <summary>
-        /// Tests that the pk level query filter is working with variables
+        /// Tests that the pk level query filter is working with variables.
         /// </summary>
         [TestMethod]
         public async Task TestQueryIdFilterField_WithVariables()

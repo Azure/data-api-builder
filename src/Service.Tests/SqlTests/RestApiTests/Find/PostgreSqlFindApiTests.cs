042e12960221a7cb62506d43fc59045fa5126da3
Aaron Burtle <aaronburtle@microsoft.com>
2024-04-24 21:51:27 -0700

    adjust postgress syntax

--- src/Service.Tests/SqlTests/RestApiTests/Find/PostgreSqlFindApiTests.cs
+++ src/Service.Tests/SqlTests/RestApiTests/Find/PostgreSqlFindApiTests.cs
@@ -808,10 +808,10 @@ namespace Azure.DataApiBuilder.Service.Tests.SqlTests.RestApiTests.Find
                 @"
                     SELECT json_agg(to_jsonb(subq)) AS data
                     FROM (
-                        SELECT  ""treeId"", ""species"" AS ""fancyName"", ""region"", ""height""
+                        SELECT  treeId, species AS fancyName, region, height
                         FROM " + _integrationMappingTable + @"
-                        WHERE ""treeId"" > 'Pseudotsuga menziesii'
-                        ORDER BY species asc, ""treeId"" asc
+                        WHERE treeId > 'Pseudotsuga menziesii'
+                        ORDER BY species asc, treeId asc
                         LIMIT 101
                     ) AS subq
                 "

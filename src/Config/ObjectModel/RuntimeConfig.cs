1a196b9f2cf6360089b1d3ffdc6ad2c087efe727
Rohan Khanna <rohkhann@microsoft.com>
2024-05-02 11:35:34 -0700

    Added environment variable.

--- src/Config/ObjectModel/RuntimeConfig.cs
+++ src/Config/ObjectModel/RuntimeConfig.cs
@@ -196,7 +196,7 @@ public record RuntimeConfig
 
             foreach (string dataSourceFile in DataSourceFiles.SourceFiles)
             {
-                if (loader.TryLoadConfig(dataSourceFile, out RuntimeConfig? config, true))
+                if (loader.TryLoadConfig(dataSourceFile, out RuntimeConfig? config, replaceEnvVar: true))
                 {
                     try
                     {

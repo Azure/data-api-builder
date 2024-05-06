0e9ad76f7fd25dfbf9719b15c3f109da4d32b469
Sean Leonard <sean.leonard@microsoft.com>
2024-05-06 12:51:01 -0700

    updates the publish.ps1 script to run dotnet publish for both net6.0 and net8.0 target frameworks explicitly and then optionally zip all the output. Updated create-manifest-file to accommodate the net8 and net6 multitarget builds.

--- scripts/publish.ps1
+++ scripts/publish.ps1
@@ -10,23 +10,39 @@ param (
 )
 
 $BuildRoot = Split-Path $PSScriptRoot -Parent
-
+$dotnetTargetFrameworks = "net6.0", "net8.0"
 $RIDs = "win-x64", "linux-x64", "osx-x64"
 
+# Runs dotnet publish for each target framework and RID.
+# Example results:
+# \dotnetpublishout\publish\Release\net8.0\win-x64\dab
+# \dotnetpublishout\publish\Release\net6.0\win-x64\dab
 if ($Package)
 {
-    foreach ($RID in $RIDs) {
-        $cmd = "dotnet publish --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$RID/dab --runtime $RID --self-contained true -p:Version=$DabVersion $BuildRoot/src/Cli/Cli.csproj"
-        Write-Host $cmd
-        Invoke-Expression $cmd
-    }
+    foreach ($targetFramework in $dotnetTargetFrameworks)
+    {
+        foreach ($RID in $RIDs) {
+            $cmd = "dotnet publish --framework $targetFramework --configuration $BuildConfiguration --output $BuildOutputDir/publish/$BuildConfiguration/$targetFramework/$RID/dab --runtime $RID --self-contained true -p:Version=$DabVersion $BuildRoot/src/Cli/Cli.csproj"
+            Write-Host $cmd
+            Invoke-Expression $cmd
+        }
+    }   
 }
 
+# Zips the published output for each target framework and RID.
+# For example:
+# \dotnetpublishout\publish\Release\net8.0\win-x64\dab_net8.0_win-x64-0.14.123-rc.zip
+# \dotnetpublishout\publish\Release\net6.0\win-x64\dab_net6.0_win-x64-0.14.123-rc.zip
 if ($CreateZip)
 {
-    foreach ($RID in $RIDs) {
-        $cmd = "Compress-Archive -Force -Path $BuildOutputDir/publish/$BuildConfiguration/$RID/dab/* -DestinationPath $BuildOutputDir/publish/$BuildConfiguration/$RID/dab_$RID-$DabVersion.zip"
-        Write-Host $cmd
-        Invoke-Expression $cmd
+    foreach ($targetFramework in $dotnetTargetFrameworks)
+    {
+        foreach ($RID in $RIDs) {
+            $filesToZipPath = "$BuildOutputDir/publish/$BuildConfiguration/$targetFramework/$RID/dab/*"
+            $archiveOutputPath = "$BuildOutputDir/publish/$BuildConfiguration/$targetFramework/$RID/dab_${targetFramework}_${RID}-${DabVersion}.zip"
+            $cmd = "Compress-Archive -Force -Path $filesToZipPath -DestinationPath $archiveOutputPath"
+            Write-Host $cmd
+            Invoke-Expression $cmd
+        }
     }
 }

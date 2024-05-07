bbeb4a25b1c146b4a6e8443ee8c27cabc309cc2a
Sean Leonard <sean.leonard@microsoft.com>
2024-05-07 08:39:14 -0700

    update comment for RelationshipRole and add newline to mssql config. newline removed by dotnet format, most likely.

--- src/Config/ObjectModel/RelationshipRole.cs
+++ src/Config/ObjectModel/RelationshipRole.cs
@@ -5,8 +5,12 @@ namespace Azure.DataApiBuilder.Config.ObjectModel;
 
 /// <summary>
 /// Denotes the role of a referencing or referenced table in a relationship.
-/// Default value is None because the role is only resolved in some scenarios
-/// such as self-referencing relationships.
+/// Default value is None because the role is only resolved to help DAB
+/// create join predicates for self-referencing relationships.
+/// In non self-join relationships, DAB uses the ForeignKeyDefinition.RelationShipPair
+/// to determine the referencing/referenced entity. RelationShipPair isn't sufficient
+/// for self-join relationships because it the DatabaseObjects used to represent the pair
+/// reference the same object: e.g. both Referenced/Referencing entity would point to 'dbo.MyTable'
 /// </summary>
 public enum RelationshipRole
 {

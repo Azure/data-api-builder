135ffa034050ea05a9d24be949cb44e1d56d68fd
Sean Leonard <sean.leonard@microsoft.com>
2024-05-13 20:07:18 -0700

    Fix `dab validate` env_var parsing. (#2215)
    ## Why make this change?
    
    - Closes #2200, an issue with `dab validate -c "path_to_config.json"`
    where environment variable `@env('ENV_VAR_NAME')` references are
    unresolved during config schema validation and schema validation always
    fails. The issue specifically calls out an error when an environment
    variable is used for the property `database-type` can't resolve an
    environment variable and properly set an enum value (mssql, pgsql,
    mysql, etc) for the property.
    
    ## What is this change?
    
    - This change corrects how
    `RuntimeConfigValidator::ValidateConfigSchema(...)` resolves environment
    variables in the runtime config. The usage of `string jsonData =
    runtimeConfig.ToJson();` converts the runtime config object (which
    already has environment variables resolved) into valid json which can
    then be validated against `dab.draft.schema.json`.
    
    ## How was this tested?
    
    - [x] Integration Tests: added a test which exercises the code path that
    validates the dab runtime config against the dab json schema. By
    ensuring that no schema validation errors occur due to unresolved
    environment variables.
    
    ## Sample Request(s)
    
    Add the file `launchSettings.json` to `src/Cli/Properties` with the
    following contents:
    ```json
    {
      "profiles": {
        "start": {
          "commandName": "Project",
          "commandLineArgs": "start -c src\\Cli\\dab-config.json",
          "environmentVariables": {
            "DATABASE_TYPE": "mssql",
            "DATABASE_NAME": "<your DB name",
            "CONN_STRING": "<CONN_STRING>",
            "AUTH_METHOD": "Simulator",
            "PUB_ENT_SRC": "publishers"
          },
          "httpPort": 5002
        },
        "validate": {
          "commandName": "Project",
          "commandLineArgs": "validate -c src\\Cli\\dab-config.json",
          "environmentVariables": {
            "DATABASE_TYPE": "mssql",
            "DATABASE_NAME": "<your DB name>",
            "CONN_STRING": "<CONN_STRING>",
            "AUTH_METHOD": "Simulator",
            "PUB_ENT_SRC": "publishers"
          },
          "httpPort": 5002
        }
      }
    }
    ```
    
    and modify your dab-config.json to have the following contents (assumes
    that you've deployed the DAB test project's .sql script:
    
    ```json
    {
      "$schema": "https://github.com/Azure/data-api-builder/releases/download/v0.13.0-rc/dab.draft.schema.json",
      "data-source": {
        "database-type": "@env('DATABASE_TYPE')",
        "connection-string": "@env('CONN_STRING')"
      },
      "runtime": {
        "rest": {
          "enabled": true,
          "path": "/api"
        },
        "graphql": {
          "allow-introspection": true,
          "enabled": true,
          "path": "/graphql"
        },
        "host": {
          "mode": "development",
          "cors": {
            "origins": [
              "http://localhost:5000"
            ],
            "allow-credentials": false
          },
          "authentication": {
            "provider": "@env('AUTH_METHOD')"
          }
        }
      },
      "entities": {
        "Query": {
          "source": "books",
          "permissions": [
            {
              "role": "anonymous",
              "actions": [
                "read"
              ]
            }
          ],
          "graphql": {
            "type": {
              "singular": "ProductCatalogue",
              "plural": "ProductCatalogue"
            }
          }
        },
        "Product": {
          "source": "@env('PUB_ENT_SRC')",
          "permissions": [
            {
              "role": "authenticated",
              "actions": [
                "read",
                "delete",
                "update"
              ]
            }
          ]
        }
      }
    }
    ```

--- src/Cli.Tests/TestHelper.cs
+++ src/Cli.Tests/TestHelper.cs
@@ -160,6 +160,68 @@ namespace Cli.Tests
           ""entities"": {}";
 
         /// <summary>
+        /// Configuration with unresolved environment variable references on
+        /// properties of various data types (string, enum, bool, int).
+        /// </summary>
+        public const string CONFIG_ENV_VARS = @"
+            {
+               ""data-source"": {
+              ""database-type"": ""@env('database-type')"",
+              ""connection-string"": ""@env('connection-string')""
+            },
+          ""runtime"": {
+              ""rest"": {
+                  ""path"": ""/api"",
+                  ""enabled"": false
+              },
+              ""graphql"": {
+                  ""path"": ""/graphql"",
+                  ""enabled"": true,
+                  ""allow-introspection"": true
+              },
+              ""host"": {
+                  ""mode"": ""development"",
+                  ""cors"": {
+                      ""origins"": [],
+                      ""allow-credentials"": false
+                  },
+                  ""authentication"": {
+                      ""provider"": ""StaticWebApps""
+                  }
+              }
+          },
+              ""entities"": {
+              ""MyEntity"": {
+                ""source"": {
+                  ""type"": ""stored-procedure"",
+                  ""object"": ""s001.book"",
+                  ""parameters"": {
+                      ""param1"": ""@env('sp_param1_int')"",
+                      ""param2"": ""hello"",
+                      ""param3"": ""@env('sp_param3_bool')""
+                  }
+                },
+                ""permissions"": [
+                  {
+                    ""role"": ""anonymous"",
+                    ""actions"": [
+                      ""execute""
+                    ]
+                  }
+                ],
+                ""rest"": {
+                    ""methods"": [
+                      ""post""
+                    ]
+                  },
+                  ""graphql"": {
+                    ""operation"": ""mutation""
+                      }
+                    }
+                  }
+          }";
+
+        /// <summary>
         /// A minimal valid config json without any entities. This config string is used in unit tests.
         /// </summary>
         public const string INITIAL_CONFIG = $"{{{SAMPLE_SCHEMA_DATA_SOURCE},{RUNTIME_SECTION}}}";

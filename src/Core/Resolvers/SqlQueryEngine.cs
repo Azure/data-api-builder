f375968779e7e7c3e506edfccb3165bdcadd40f2
Abhishek Kumar <abhishekkuma@microsoft.com>
2024-07-10 12:06:39 +0530

    fix formatting

--- src/Core/Resolvers/SqlQueryEngine.cs
+++ src/Core/Resolvers/SqlQueryEngine.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.Text;
 using System.Text.Json;
 using System.Text.Json.Nodes;
 using Azure.DataApiBuilder.Auth;

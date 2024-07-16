335066880e885318280f4cb98f6fb75c97dd55a2
Sourabh Jain <sourabhjain@microsoft.com>
2024-07-16 17:20:00 +0530

    fix code

--- src/Core/Generator/Sampler/TopNSampler.cs
+++ src/Core/Generator/Sampler/TopNSampler.cs
@@ -8,21 +8,44 @@ namespace Azure.DataApiBuilder.Core.Generator.Sampler
 {
     internal class TopNSampler : ISchemaGeneratorSampler
     {
+        // Default Configuration
+        public const int NUMBER_OF_RECORDS = 10;
+        public const int MAX_DAYS = 0;
+
+        // Query
+        public const string SELECT_QUERY = "SELECT TOP {0} * FROM c {1} ORDER BY c._ts desc";
+
         private int _numberOfRecords;
-        private QueryExecutor _queryExecutor;
+        private int _maxDays;
 
-        public TopNSampler(Container container, int? numberOfRecords)
+        private CosmosExecutor _cosmosExecutor;
+
+        public TopNSampler(Container container, int? numberOfRecords, int? maxDays)
         {
-            this._numberOfRecords = numberOfRecords ?? 10;
+            this._numberOfRecords = numberOfRecords ?? NUMBER_OF_RECORDS;
+            this._maxDays = maxDays ?? MAX_DAYS;
 
-            this._queryExecutor = new QueryExecutor(container);
+            this._cosmosExecutor = new CosmosExecutor(container);
         }
 
+        /// <summary>
+        /// This Function return TOP N records, in the order of time, from the Cosmos DB
+        /// </summary>
+        /// <returns></returns>
         public async Task<List<JObject>> GetSampleAsync()
         {
-            string query = $"SELECT TOP {_numberOfRecords} * FROM c ORDER BY c._ts desc";
+            string daysFilterClause = string.Empty;
 
-            return await _queryExecutor.ExecuteQueryAsync<JObject>(query);
+            if (_maxDays > 0)
+            {
+                long timestampThreshold = new DateTimeOffset(DateTime.UtcNow.AddDays(_maxDays)).ToUnixTimeSeconds();
+
+                daysFilterClause = $"WHERE c._ts <= {timestampThreshold}";
+            }
+            
+            string query = string.Format(SELECT_QUERY, _numberOfRecords, daysFilterClause);
+
+            return await _cosmosExecutor.ExecuteQueryAsync<JObject>(query);
         }
     }
 }
--- src/Core/Generator/SchemaGeneratorFactory.cs
+++ src/Core/Generator/SchemaGeneratorFactory.cs
@@ -1,19 +1,44 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using Azure.DataApiBuilder.Config.ObjectModel;
 using Azure.DataApiBuilder.Core.Generator.Sampler;
 using Microsoft.Azure.Cosmos;
 using Newtonsoft.Json.Linq;
 
 namespace Azure.DataApiBuilder.Core.Generator
 {
-    internal static class SchemaGeneratorFactory
+    public static class SchemaGeneratorFactory
     {
-        public static async Task<string> Create(Container container, SamplingMode mode, int? sampleCount, string? partitionKeyPath, int? days, int? groupCount)
+        public static async Task<string> Create(RuntimeConfig config, string mode, int? sampleCount, string? partitionKeyPath, int? days, int? groupCount)
         {
-            ISchemaGeneratorSampler schemaGeneratorSampler = mode switch
+            if (config.DataSource == null)
             {
-                SamplingMode.TopNSampler => new TopNSampler(container, sampleCount),
+                throw new ArgumentException("Data Source not found");
+            }
+
+            if (config.DataSource.DatabaseType != DatabaseType.CosmosDB_NoSQL)
+            {
+                throw new ArgumentException("Config file passed is not compatible with this feature. Please make sure datasource type is configured as 'Cosmos DB'");
+            }
+
+            string? connectionString = config?.DataSource?.ConnectionString;
+            string? databaseName = config?.DataSource?.Options?["database"]?.ToString();
+            string? containerName = config?.DataSource?.Options?["container"]?.ToString();
+
+            if (connectionString == null || databaseName == null || containerName == null)
+            {
+                throw new ArgumentException("Connection String, Database and container name must be provided in the config file");
+            }
+
+            Container container = ConnectToCosmosDB(connectionString, databaseName, containerName);
+            ;
+
+            SamplingMode samplingMode = (SamplingMode)Enum.Parse(typeof(SamplingMode), mode);
+
+            ISchemaGeneratorSampler schemaGeneratorSampler = samplingMode switch
+            {
+                SamplingMode.TopNSampler => new TopNSampler(container, sampleCount, days),
                 SamplingMode.PartitionBasedSampler => new PartitionBasedSampler(container, partitionKeyPath, sampleCount, days),
                 SamplingMode.TimeBasedSampler => new TimeBasedSampler(container, groupCount, sampleCount, days),
                 _ => throw new ArgumentException($"Invalid sampling mode: {mode}")
@@ -24,6 +49,13 @@ namespace Azure.DataApiBuilder.Core.Generator
 
             // Generate GQL Schema
             return SchemaGenerator.Generate(dataArray, container.Id);
+        }
+
+        private static Container ConnectToCosmosDB(string connectionString, string database, string container)
+        {
+            CosmosClient cosmosClient = new (connectionString);
+
+            return cosmosClient.GetContainer(database, container);
         }
     }
 }

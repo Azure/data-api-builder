fb8bb6228df9801942e44ef80c0394fbfea87277
aaron burtle <aaronburtle@microsoft.com>
2024-09-09 23:51:37 -0700

    save branch

--- src/Config/FileSystemRuntimeConfigLoader.cs
+++ src/Config/FileSystemRuntimeConfigLoader.cs
@@ -53,8 +53,8 @@ public class FileSystemRuntimeConfigLoader : RuntimeConfigLoader
     /// </summary>
     public string ConfigFilePath { get; internal set; }
 
-    public FileSystemRuntimeConfigLoader(IFileSystem fileSystem, string baseConfigFilePath = DEFAULT_CONFIG_FILE_NAME, string? connectionString = null)
-        : base(connectionString)
+    public FileSystemRuntimeConfigLoader(IFileSystem fileSystem, HotReloadEventHandler<CustomEventArgs> handler, string baseConfigFilePath = DEFAULT_CONFIG_FILE_NAME, string? connectionString = null)
+        : base(handler, connectionString)
     {
         _fileSystem = fileSystem;
         _baseConfigFilePath = baseConfigFilePath;

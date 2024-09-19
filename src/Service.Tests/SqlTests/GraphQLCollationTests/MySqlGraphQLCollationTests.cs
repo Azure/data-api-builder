5ff7b6a554846031badbeb49f5e504ce3da85f9a
Ruben Cerna <rcernaserna@microsoft.com>
2024-09-19 15:35:45 -0700

    Changes for Testing

--- src/Service.Tests/SqlTests/GraphQLCollationTests/MySqlGraphQLCollationTests.cs
+++ src/Service.Tests/SqlTests/GraphQLCollationTests/MySqlGraphQLCollationTests.cs
@@ -26,7 +26,7 @@ namespace Azure.DataApiBuilder.Service.Tests.SqlTests.GraphQLCollationTests
         /// </summary>
         [DataTestMethod]
         [DataRow("books", "title", @"SELECT COALESCE(JSON_ARRAYAGG(JSON_OBJECT('title', `subq1`.`title`)), '[]') AS `data` FROM ( SELECT `table0`.`title` AS `TITLE` FROM `books` AS `table0` WHERE 1 = 1 ORDER BY `table0`.`title` ASC LIMIT 100 ) AS `subq1`")]
-        [DataRow("books", "title", @"SELECT COALESCE(JSON_ARRAYAGG(JSON_OBJECT('title', `subq1`.`TITLE`)), '[]') AS `DATA` FROM ( SELECT `table0`.`TITLE` AS `TITLE` FROM `BOOKS` AS `table0` WHERE 1 = 1 ORDER BY `table0`.`TITLE` ASC LIMIT 100 ) AS `subq1`")]
+        [DataRow("books", "title", @"SELECT COALESCE(JSON_ARRAYAGG(JSON_OBJECT('title', `subq1`.`TITLE`)), '[]') AS `DATA` FROM ( SELECT `table0`.`TITLE` AS `TITLE` FROM `books` AS `table0` WHERE 1 = 1 ORDER BY `table0`.`TITLE` ASC LIMIT 100 ) AS `subq1`")]
         [DataRow("books", "title", @"SELECT COALESCE(JSON_ARRAYAGG(JSON_OBJECT('title', `subq1`.`Title`)), '[]') AS `Data` FROM ( SELECT `table0`.`Title` AS `Title` FROM `Books` AS `table0` WHERE 1 = 1 ORDER BY `table0`.`Title` ASC LIMIT 100 ) AS `subq1`")]
 
         public async Task MySqlCapitalizationResultQuery(string type, string item, string dbQuery)

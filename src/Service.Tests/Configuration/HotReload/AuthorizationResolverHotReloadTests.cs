2b4163e3daf9bae8a6d9d3d83b29296ad29989c5
Abhishek Kumar <abhishekkuma@microsoft.com>
2024-10-30 15:43:54 +0530

    fix formatting

--- src/Service.Tests/Configuration/HotReload/AuthorizationResolverHotReloadTests.cs
+++ src/Service.Tests/Configuration/HotReload/AuthorizationResolverHotReloadTests.cs
@@ -213,7 +213,7 @@ public class AuthorizationResolverHotReloadTests
         {
             File.Delete(AUTHZ_HR_FILENAME);
         }
-        
+
         _testServer.Dispose();
         _testClient.Dispose();
     }

bc34455b933fd669ad7a4450728b36ae146478b5
Abhishek  Kumar <102276754+abhishekkumams@users.noreply.github.com>
2024-10-30 15:20:40 +0530

    using enum type instead of string for Database Type

--- src/Service.Tests/Configuration/HotReload/ConfigurationHotReloadTests.cs
+++ src/Service.Tests/Configuration/HotReload/ConfigurationHotReloadTests.cs
@@ -54,7 +54,7 @@ public class ConfigurationHotReloadTests
         "]";
 
     private static void GenerateConfigFile(
-        string databaseType = "mssql",
+        DatabaseType databaseType = DatabaseType.MSSQL,
         string connectionString = "",
         string restPath = "/rest",
         string restEnabled = "true",

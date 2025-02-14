52e6f2a0faf87244de4afb8275c6adf2f0d24506
Jerry Nixon <jerry.nixon@microsoft.com>
2025-02-14 11:39:18 -0700

    Enhance VersionChecker documentation: add XML comments for class and method

--- src/Core/VersionChecker.cs
+++ src/Core/VersionChecker.cs
@@ -1,14 +1,28 @@
-using System.Diagnostics;
 using System.Net.Http.Json;
 using System.Text.Json.Serialization;
 using Azure.DataApiBuilder.Product;
 
 namespace Azure.DataApiBuilder.Core;
 
+/// <summary>
+/// A class to test the local version of the engine against the NuGet version.
+/// </summary>
+/// <remarks>
+/// This is used in startup to suggest upgrading the engine.
+/// </remarks>
 public static class VersionChecker
 {
     private const string NUGETURL = "https://api.nuget.org/v3-flatcontainer/microsoft.dataapibuilder/index.json";
 
+    /// <summary>
+    /// Checks if the current local version of the product matches the latest version available on NuGet.
+    /// </summary>
+    /// <param name="nugetVersion">Outputs the latest version available on NuGet.</param>
+    /// <param name="localVersion">Outputs the current local version of the product.</param>
+    /// <returns>
+    /// Returns <c>true</c> if the local version matches the latest NuGet version or if the NuGet version is not available;
+    /// otherwise, returns <c>false</c>.
+    /// </returns>
     public static bool IsCurrentVersion(out string? nugetVersion, out string? localVersion)
     {
         nugetVersion = FetchLatestNuGetVersion();

dcf260a11efbe10df3595b784bbd09f8db079849
Sezal Chug <30494467+sezal98@users.noreply.github.com>
2025-03-17 18:07:35 +0530

    Modify Config Files according to health check endpoint (#2606)
    ## Why make this change?
    Closes #2605 
    
    ## What is this change?
    
    1. This change includes creating the new JSON format for health check
    report and the related files for the same.
    2. It also includes the changes required in the runtime, datasource and
    entities section of the config file to support the newly updated
    properties for health checks.
    3. Further I have created an enhanced response writer for comprehensive
    endpoint which would take up the new config changes and return the
    health report and mapped it to the `/health` endpoint.
    
    ## How was this tested?
    
    This is currently tested by making sure the basic health checks are
    running as expected.
    Condition 1: Health is enabled at runtime config
    Here, we would get the serialized version of the basic health check.
    **Note: This would be improved further.**
    
    Condition 2: Health is disabled or null in runtime config
    Here, we would get a 404 Not Found response at the page.
    
    ## Sample Request(s)
    Not Found Page
    <img width="821" alt="image"
    src="https://github.com/user-attachments/assets/330e584c-cd96-4b61-afac-7225e5d8895d"
    />
    
    Basic Health Check as expected
    <img width="215" alt="image"
    src="https://github.com/user-attachments/assets/aaa77479-28e6-4955-ae7f-72ec1ef8ff0b"
    />
    
    Comprehensive Health Check (To Be improved further)
    <img width="253" alt="image"
    src="https://github.com/user-attachments/assets/cfe8e8bb-6741-4879-80a2-eee399d0654f"
    />
    
    ---------
    
    Co-authored-by: sezalchug <sezalchug@microsoft.com>
    Co-authored-by: Aniruddh Munde <anmunde@microsoft.com>

---
+++ src/Config/HealthCheck/ConfigurationDetails.cs
@@ -0,0 +1,17 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Azure.DataApiBuilder.Config.ObjectModel
+{
+    /// <summary>
+    /// The configuration details of the DAB Engine.
+    /// </summary>
+    public record ConfigurationDetails
+    {
+        public bool Rest { get; init; }
+        public bool GraphQL { get; init; }
+        public bool Caching { get; init; }
+        public bool Telemetry { get; init; }
+        public HostMode Mode { get; init; }
+    }
+}

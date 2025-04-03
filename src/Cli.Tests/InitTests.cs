9c2fe9012bfba84e6dc10057c5c5890abe5babd3
Sander ten Brinke <s.tenbrinke2@gmail.com>
2025-04-03 09:05:02 +0200

    Apply suggestions from code review
    Co-authored-by: RubenCerna2079 <32799214+RubenCerna2079@users.noreply.github.com>

--- src/Cli.Tests/InitTests.cs
+++ src/Cli.Tests/InitTests.cs
@@ -304,8 +304,8 @@ namespace Cli.Tests
         [DataRow("AppService", null, null, DisplayName = "AppService with no audience and no issuer specified.")]
         [DataRow("Simulator", null, null, DisplayName = "Simulator with no audience and no issuer specified.")]
         [DataRow("AzureAD", "aud-xxx", "issuer-xxx", DisplayName = "AzureAD with both audience and issuer specified.")]
-        [DataRow("EntraId", "aud -xxx", "issuer-xxx", DisplayName = "EntraId with both audience and issuer specified.")]
-        [DataRow("OAuth", "aud -xxx", "issuer-xxx", DisplayName = "OAuth with both audience and issuer specified.")]
+        [DataRow("EntraId", "aud-xxx", "issuer-xxx", DisplayName = "EntraId with both audience and issuer specified.")]
+        [DataRow("OAuth", "aud-xxx", "issuer-xxx", DisplayName = "OAuth with both audience and issuer specified.")]
         public Task EnsureCorrectConfigGenerationWithDifferentAuthenticationProviders(
             string authenticationProvider,
             string? audience,

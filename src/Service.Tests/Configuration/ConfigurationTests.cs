832d5d52d3621cc5813e4a37701880c99a716b50
sezalchug <sezalchug@microsoft.com>
2025-05-09 14:44:59 +0530

    ignore tests

--- src/Service.Tests/Configuration/ConfigurationTests.cs
+++ src/Service.Tests/Configuration/ConfigurationTests.cs
@@ -3726,6 +3726,7 @@ type Planet @model(name:""PlanetAlias"") {
         /// <summary>
         /// Tests different log level filters that are valid and check that they are deserialized correctly
         /// </summary>
+        [Ignore]
         [DataTestMethod]
         [TestCategory(TestCategory.MSSQL)]
         [DataRow(LogLevel.Trace, typeof(RuntimeConfigValidator))]

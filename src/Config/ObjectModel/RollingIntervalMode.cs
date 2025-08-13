dc2a98131d1371b4645a4ed7e8f4b2a1d06383ca
Ruben Cerna <rcernaserna@microsoft.com>
2025-08-13 15:12:53 -0700

    Fix Rolling Interval Mode

--- src/Config/ObjectModel/RollingIntervalMode.cs
+++ src/Config/ObjectModel/RollingIntervalMode.cs
@@ -12,7 +12,33 @@ namespace Azure.DataApiBuilder.Config.ObjectModel;
 [JsonConverter(typeof(JsonStringEnumConverter))]
 public enum RollingIntervalMode
 {
-    Hour,
+    /// <summary>
+    /// The log file will never roll; no time period information will be appended to the log file name.
+    /// </summary>
+    Infinite,
+
+    /// <summary>
+    /// Roll every year. Filenames will have a four-digit year appended in the pattern <code>yyyy</code>.
+    /// </summary>
+    Year,
+
+    /// <summary>
+    /// Roll every calendar month. Filenames will have <code>yyyyMM</code> appended.
+    /// </summary>
+    Month,
+
+    /// <summary>
+    /// Roll every day. Filenames will have <code>yyyyMMdd</code> appended.
+    /// </summary>
     Day,
-    Week
+
+    /// <summary>
+    /// Roll every hour. Filenames will have <code>yyyyMMddHH</code> appended.
+    /// </summary>
+    Hour,
+
+    /// <summary>
+    /// Roll every minute. Filenames will have <code>yyyyMMddHHmm</code> appended.
+    /// </summary>
+    Minute
 }

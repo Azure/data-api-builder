67dd3a00d5a0281ea5e2c10df3248bf2b09fd716
Jerry Nixon <jerry.nixon@microsoft.com>
2025-08-28 12:02:43 -0600

    basic working

--- src/Azure.DataApiBuilder.Mcp/Extensions.cs
+++ src/Azure.DataApiBuilder.Mcp/Extensions.cs
@@ -1,35 +1,32 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
 using System.Diagnostics.CodeAnalysis;
-using Azure.DataApiBuilder.Config.ObjectModel;
+using System.Text.Json;
 using Azure.DataApiBuilder.Core.Configurations;
+using Google.Protobuf.WellKnownTypes;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Extensions.DependencyInjection;
-
-using Azure.DataApiBuilder.Mcp.Health;
-using Azure.DataApiBuilder.Mcp.Tools;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Server;
 
 namespace Azure.DataApiBuilder.Mcp
 {
     public static class Extensions
     {
-        private static McpOptions _mcpOptions = default!;
-
         public static IServiceCollection AddDabMcpServer(this IServiceCollection services, RuntimeConfigProvider runtimeConfigProvider)
         {
-            if (runtimeConfigProvider.TryGetConfig(out RuntimeConfig? runtimeConfig))
-            {
-                _mcpOptions = runtimeConfig?.Ai?.Mcp ?? throw new NullReferenceException("Configuration is required.");
-            }
-
-            services.AddDmlTools(_mcpOptions);
-
             services
                 .AddMcpServer()
+                .WithToolsFromAssembly()
                 .WithHttpTransport();
-
             return services;
         }
        
@@ -37,8 +34,55 @@ namespace Azure.DataApiBuilder.Mcp
         public static IEndpointRouteBuilder MapDabMcp(this IEndpointRouteBuilder endpoints, [StringSyntax("Route")] string pattern = "")
         {
             endpoints.MapMcp();
-            endpoints.MapDabHealthChecks("/jerry");
+            endpoints.MapHealthChecks("/jerry", new HealthCheckOptions
+            {
+                ResponseWriter = WriteHealthCheckResponse
+            });
             return endpoints;
         }
+
+        private static async Task WriteHealthCheckResponse(HttpContext context, HealthReport report)
+        {
+            // Get the MCP server endpoint
+            IServiceProvider serviceProvider = context.RequestServices;
+            HttpRequest? request = serviceProvider.GetService<IHttpContextAccessor>()?.HttpContext?.Request;
+            if (request == null)
+            {
+                throw new Exception();
+            }
+
+            string scheme = request.Scheme;
+            string host = request.Host.Value;
+            string endpoint = $"{scheme}://{host}";
+
+            IMcpClient mcpClient = await McpClientFactory.CreateAsync(
+                new SseClientTransport(new()
+                {
+                    Endpoint = new Uri(endpoint),
+                    Name = "HealthCheck"
+                }),
+                clientOptions: new McpClientOptions()
+                {
+                    Capabilities = new() { }
+                },
+                loggerFactory: null);
+
+            IList<McpClientTool> mcpTools = await mcpClient.ListToolsAsync();
+            string[] toolNames = mcpTools.Select(t => t.Name).OrderBy(t => t).ToArray();
+
+            context.Response.ContentType = "application/json";
+            await context.Response.WriteAsync(JsonSerializer.Serialize(toolNames, new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                WriteIndented = true
+            }));
+        }
+    }
+
+    [McpServerToolType]
+    public static class DabMcpTools
+    {
+        [McpServerTool]
+        public static string Echo(string message) => message;
     }
 }

66bba27e1dfc5e04de4e79476c75142b4aeafeb2
souvikghosh04 <souvikofficial04@gmail.com>
2025-09-09 21:23:08 +0530

    Revert "Updated mege"
    This reverts commit 04b19f9e5667d52471702b0fb81b07179ef0a38e, reversing
    changes made to 3948822118f182e4b5ed00f77b11cadab841354a.

--- src/Azure.DataApiBuilder.Mcp/Extensions.cs
+++ src/Azure.DataApiBuilder.Mcp/Extensions.cs
@@ -2,7 +2,6 @@
 // Licensed under the MIT License.
 
 using System.Diagnostics.CodeAnalysis;
-using System.Text.Json;
 using Azure.DataApiBuilder.Config.ObjectModel;
 using Azure.DataApiBuilder.Core.Configurations;
 using Azure.DataApiBuilder.Mcp.Health;
@@ -10,9 +9,6 @@ using Azure.DataApiBuilder.Mcp.Tools;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using ModelContextProtocol;
-using ModelContextProtocol.Protocol;
-using ModelContextProtocol.Server;
 
 namespace Azure.DataApiBuilder.Mcp
 {
@@ -38,8 +34,8 @@ namespace Azure.DataApiBuilder.Mcp
 
         public static IEndpointRouteBuilder MapDabMcp(this IEndpointRouteBuilder endpoints, [StringSyntax("Route")] string? pattern = null)
         {
-            endpoints.MapMcp(pattern);
-            endpoints.MapMcpHealthEndpoint(pattern & "/health");
+            endpoints.MapMcp();
+            endpoints.MapMcpHealthEndpoint(pattern);
             return endpoints;
         }
     }

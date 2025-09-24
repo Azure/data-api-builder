5a1574366bb50f52892ad1b4ca9824c280187625
Ruben Cerna <rcernaserna@microsoft.com>
2025-09-24 12:08:08 -0700

    Fix merge conflict

--- src/Config/ObjectModel/McpRuntimeOptions.cs
+++ src/Config/ObjectModel/McpRuntimeOptions.cs
@@ -5,7 +5,9 @@ using System.Diagnostics.CodeAnalysis;
 using System.Text.Json.Serialization;
 using Azure.DataApiBuilder.Config.Converters;
 
-namespace Azure.DataApiBuilder.Config.ObjectModel
+namespace Azure.DataApiBuilder.Config.ObjectModel;
+
+public record McpRuntimeOptions
 {
     public const string DEFAULT_PATH = "/mcp";
 

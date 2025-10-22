4206af682bd1f12ff49b91956833d2ac03e9ecb8
souvikghosh04 <souvikofficial04@gmail.com>
2025-10-22 20:06:51 +0530

    TEST ONLY- temporary do not set TrustServerCertificate to false explicitly

--- src/Core/Resolvers/MsSqlQueryExecutor.cs
+++ src/Core/Resolvers/MsSqlQueryExecutor.cs
@@ -149,7 +149,7 @@ namespace Azure.DataApiBuilder.Core.Resolvers
                 if (_runtimeConfigProvider.IsLateConfigured)
                 {
                     builder.Encrypt = SqlConnectionEncryptOption.Mandatory;
-                    builder.TrustServerCertificate = false;
+                    // builder.TrustServerCertificate = false; // TODO: THIS IS COMMENTED OUT FOR TESTING PURPOSES ONLY. UNCOMMENT BEFORE SHIPPING.
                 }
 
                 ConnectionStringBuilders.TryAdd(dataSourceName, builder);

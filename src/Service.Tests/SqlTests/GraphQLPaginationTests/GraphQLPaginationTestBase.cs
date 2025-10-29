ae05b1e96d4c0698f5a27a99875dbcd3e162017a
Giovanna Ribeiro <gribeiro@microsoft.com>
2025-10-29 10:11:49 -0700

    fixes formatting

--- src/Service.Tests/SqlTests/GraphQLPaginationTests/GraphQLPaginationTestBase.cs
+++ src/Service.Tests/SqlTests/GraphQLPaginationTests/GraphQLPaginationTestBase.cs
@@ -3,7 +3,6 @@
 
 using System.Text.Json;
 using System.Threading.Tasks;
-using Azure.DataApiBuilder.Config.ObjectModel;
 using Azure.DataApiBuilder.Core.Resolvers;
 using Azure.DataApiBuilder.Service.Exceptions;
 using Azure.DataApiBuilder.Service.GraphQLBuilder.Queries;

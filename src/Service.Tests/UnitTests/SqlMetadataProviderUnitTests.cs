aa3116a2c0b4e098ffa8a8279100aff6f280f729
Ruben Cerna <rcernaserna@microsoft.com>
2026-01-29 17:48:50 -0800

    Fix formatting issues

--- src/Service.Tests/UnitTests/SqlMetadataProviderUnitTests.cs
+++ src/Service.Tests/UnitTests/SqlMetadataProviderUnitTests.cs
@@ -616,7 +616,7 @@ namespace Azure.DataApiBuilder.Service.Tests.UnitTests
             };
             RuntimeConfigProvider runtimeConfigProvider = TestHelper.GenerateInMemoryRuntimeConfigProvider(configWithAutoentity);
             SetUpSQLMetadataProvider(runtimeConfigProvider);
-            
+
             await _sqlMetadataProvider.InitializeAsync();
 
             // Act

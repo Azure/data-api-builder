27657f25de58822d39771e1a14149789e6345e0d
Ruben Cerna <rcernaserna@microsoft.com>
2026-02-13 14:37:06 -0800

    Fix unit tests

--- src/Cli.Tests/AutoentitiesConfigureTests.cs
+++ src/Cli.Tests/AutoentitiesConfigureTests.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
-using System.IO.Abstractions;
 using System.Text;
 using System.Text.Json;
 using Azure.DataApiBuilder.Config;

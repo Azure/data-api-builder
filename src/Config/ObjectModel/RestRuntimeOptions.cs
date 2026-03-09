e7cb923c071fc03f80ca0bad43ff7b061f053f37
Aniruddh Munde <anmunde@microsoft.com>
2026-03-08 17:17:23 -0700

    Apply suggestions from code review

--- src/Config/ObjectModel/RestRuntimeOptions.cs
+++ src/Config/ObjectModel/RestRuntimeOptions.cs
@@ -10,7 +10,7 @@ namespace Azure.DataApiBuilder.Config.ObjectModel;
 /// <param name="Path">The URL prefix path at which endpoints
 /// for all entities will be exposed.</param>
 /// <param name="RequestBodyStrict">Boolean property indicating whether extraneous fields are allowed in request body.
-/// The default value for existing configs (without this property set) is true - meaning we don't allow extraneous fields by default.
+/// The default value for existing configs (without this property set) is false - meaning we allow extraneous fields by default.
 /// When dab init generates a new config, the default is false - allowing extraneous fields by default for a smoother development experience.</param>
 public record RestRuntimeOptions(bool Enabled = true, string Path = RestRuntimeOptions.DEFAULT_PATH, bool RequestBodyStrict = true)
 {

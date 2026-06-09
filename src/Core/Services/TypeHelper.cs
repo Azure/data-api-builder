3cc1c0b4cbf4af82c3b210350422457e9d4ed4d6
Ruben Cerna <rcernaserna@microsoft.com>
2026-06-09 16:16:35 -0700

    First additions to vector data type

--- src/Core/Services/TypeHelper.cs
+++ src/Core/Services/TypeHelper.cs
@@ -8,6 +8,7 @@ using System.Net;
 using Azure.DataApiBuilder.Core.Services.OpenAPI;
 using Azure.DataApiBuilder.Service.Exceptions;
 using HotChocolate.Language;
+using Microsoft.Data;
 using Microsoft.OData.Edm;
 
 namespace Azure.DataApiBuilder.Core.Services
@@ -77,7 +78,8 @@ namespace Azure.DataApiBuilder.Core.Services
             [typeof(TimeOnly)] = JsonDataType.String,
             [typeof(object)] = JsonDataType.Object,
             [typeof(DateTime)] = JsonDataType.String,
-            [typeof(DateTimeOffset)] = JsonDataType.String
+            [typeof(DateTimeOffset)] = JsonDataType.String,
+            [typeof(float[])] = JsonDataType.Array
         };
 
         /// <summary>
@@ -111,7 +113,8 @@ namespace Azure.DataApiBuilder.Core.Services
             [SqlDbType.TinyInt] = typeof(byte),
             [SqlDbType.UniqueIdentifier] = typeof(Guid),
             [SqlDbType.VarBinary] = typeof(byte[]),
-            [SqlDbType.VarChar] = typeof(string)
+            [SqlDbType.VarChar] = typeof(string),
+            [SqlDbTypeExtensions.Vector] = typeof(float[])
         };
 
         private static Dictionary<SqlDbType, DbType> _sqlDbDateTimeTypeToDbType = new()
